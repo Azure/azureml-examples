478a52ba57699fdb9990c0d36268221e2ad4281a
Jordan Edwards <jordane@microsoft.com>
2020-08-21 11:42:07 -0700

    Create azureml-train.py

---
+++ code/models/sklearn-diabetes/azureml-train.py
@@ -0,0 +1,11 @@
+from azureml.core import Workspace, Environment, ScriptRunConfig, Experiment
+
+ws = Workspace.from_config()
+
+env = Environment.from_conda_specification(name = "myenv", file_path = "environment.yml")
+
+src = ScriptRunConfig(source_directory=".", script='train.py')
+src.run_config.environment = env
+src.run_config.target = 'cpu-cluster'
+
+run = Experiment(ws, "sklearn-diabetes").submit(src)
