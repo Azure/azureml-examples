39d4a6c9917bc73544c2d776b913f2c8d99d2ec4
Cody Peterson <cody@dkdcair.local>
2020-08-24 09:19:31 -0700

    update test training script to run locally

--- code/models/sklearn-diabetes/azureml-train.py
+++ code/models/sklearn-diabetes/azureml-train.py
@@ -2,11 +2,11 @@ from azureml.core import Workspace, Environment, ScriptRunConfig, Experiment
 
 ws = Workspace.from_config()
 
-env = Environment.from_conda_specification(name = "myenv", file_path = "environment.yml")
+env = Environment.from_conda_specification(name = 'myenv', file_path = 'environment.yml')
 
-src = ScriptRunConfig(source_directory=".", script='train.py')
+src = ScriptRunConfig(source_directory='.', script='train.py')
 src.run_config.environment = env
-src.run_config.target = 'cpu-cluster'
+src.run_config.target = 'local'
 
 run = Experiment(ws, "sklearn-diabetes").submit(src)
 run.wait_for_completion(show_output=True)
