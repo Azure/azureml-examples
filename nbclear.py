0d76e550518e368c34e6c183ab9c92f6915eace1
Cody Peterson <cody@dkdcair.local>
2020-08-31 14:55:24 -0700

    update nblcear

--- nbclear.py
+++ nbclear.py
@@ -1,12 +1,7 @@
 import os 
 import glob
 
-nbs = glob.glob('notebooks/**/*.ipynb', recursive=True)
-
-for nb in nbs:
-    os.system(f'jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {nb}')
-
-nbs = glob.glob('concepts/**/*.ipynb', recursive=True)
+nbs = glob.glob('**/*.ipynb', recursive=True)
 
 for nb in nbs:
     os.system(f'jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {nb}')
