12e523630002cf41c4abe21819fab61e031b0f1f
Cody Peterson <cody@dkdcair.local>
2020-09-10 11:46:41 -0700

    checkpoint

--- nbclear.py
+++ nbclear.py
@@ -4,4 +4,4 @@ import glob
 nbs = glob.glob('**/*.ipynb', recursive=True)
 
 for nb in nbs:
-    os.system(f'jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {nb}')
+    os.system(f'jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace {nb} &')
--- readme.py
+++ readme.py
@@ -31,29 +31,77 @@ suffix='''
 We welcome contributions and suggestions! Please see the [Contributing Guidelines](CONTRIBUTING.md) for details.
 '''
 
-nbs = glob.glob('notebooks/**/*.ipynb', recursive=True)
+def write_workflows(ws='default', rg='azureml-examples', creds='${{secrets.AZ_AE_CREDS}}'):
+    nbs = glob.glob('notebooks/**/*.ipynb', recursive=True)
 
-for nb in nbs:
-    print()
-    print(nb)
-    with open(nb, 'r') as f:
-        data = json.load(f)
-    try:
-        index_data = data['metadata']['index']
-        print(index_data)
+    for nb in nbs:
+        print()
+        print(nb)
 
-        scenario = index_data['scenario']
-        compute = index_data['compute']
-        frameworks = index_data['frameworks']
-        dataset = index_data['dataset']
-        environment = index_data['environment']
-        distribution = index_data['distribution']
-        other = index_data['other']
+        name = nb.split('/')[-1].split('.')[0]
 
-        row = f'[{nb}]({nb})|{scenario}|{compute}|{frameworks}|{dataset}|{environment}|{distribution}|{other}\n'
-        prefix += row 
-    except:
-        print(f'Notebook: {nb} is missing metadata for building the index.')
+        workflow = f'''name: {name}
+on: [push]
+env:
+  ml-ws: {ws}
+  ml-rg: {rg}
+jobs:
+  build:
+    runs-on: ubuntu-latest 
+    steps:
+    - name: check out repo
+      uses: actions/checkout@v2
+    - name: setup python
+      uses: actions/setup-python@v2
+    - name: pip install
+      run: pip install -r requirements.txt
+    - name: azure login
+      uses: azure/login@v1
+      with:
+        creds: {creds}
+    - name: install azmlcli
+      run: az extension add -n azure-cli-ml
+    - name: attach to workspace
+      run: az ml folder attach -w ${{env.ml-ws}} -g ${{env.ml-rg}}
+    - name: run notebook
+      run: papermill {nb} out.ipynb -k python
+    - name: upload notebook artifact
+      uses: actions/upload-artifact@v2
+      with:
+        path: out.ipynb
+'''
 
-with open('README.md', 'w') as f:
-    f.write(prefix+suffix)
+        with open(f'.github/workflows/{name}.yml', 'w') as f:
+            f.write(workflow)
+
+
+def write_readme():
+    nbs = glob.glob('notebooks/**/*.ipynb', recursive=True)
+
+    for nb in nbs:
+        print()
+        print(nb)
+        with open(nb, 'r') as f:
+            data = json.load(f)
+        try:
+            index_data = data['metadata']['index']
+            print(index_data)
+
+            scenario = index_data['scenario']
+            compute = index_data['compute']
+            frameworks = index_data['frameworks']
+            dataset = index_data['dataset']
+            environment = index_data['environment']
+            distribution = index_data['distribution']
+            other = index_data['other']
+
+            row = f'[{nb}]({nb})|{scenario}|{compute}|{frameworks}|{dataset}|{environment}|{distribution}|{other}\n'
+            prefix += row 
+        except:
+            print(f'Notebook: {nb} is missing metadata for building the index.')
+
+    with open('README.md', 'w') as f:
+        f.write(prefix+suffix)
+
+#write_readme()
+write_workflows()
