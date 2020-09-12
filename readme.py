cb84288e0ae536ac97e23809bd333140629adbd5
Cody Peterson <cody@dkdcpro.local>
2020-09-11 20:48:16 -0700

    update readmes

--- readme.py
+++ readme.py
@@ -6,12 +6,12 @@ import glob
 prefix = """# Azure ML Examples
 
 [![run-notebooks-badge](https://github.com/Azure/azureml-examples/workflows/run-notebooks/badge.svg)](https://github.com/Azure/azureml-examples/actions?query=workflow%3Arun-notebooks)
-[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 [![License: MIT](https://img.shields.io/badge/License-MIT-purple.svg)](LICENSE)
 
 Welcome to the Azure ML examples!
 
-## Getting started
+## Installation
 
 Clone this repository and install required packages:
 
@@ -42,6 +42,10 @@ suffix = """
 ## Contributing
 
 We welcome contributions and suggestions! Please see the [contributing guidelines](CONTRIBUTING.md) for details.
+
+## Code of Conduct 
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). Please see the [code of conduct](CODE_OF_CONDUCT.md) for details. 
 """
 
 ws = "default"
