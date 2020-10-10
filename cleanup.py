68db8d29b293ae144ed7e76bb2d45c3c80b4b7e2
lostmygithubaccount <cody.dkdc2@gmail.com>
2020-10-10 09:31:18 -0700

    fix dask tutorial

--- cleanup.py
+++ cleanup.py
@@ -11,7 +11,12 @@ for webservice in ws.webservices:
     ws.webservices[webservice].delete()
 
 for compute_target in ws.compute_targets:
-    if "concept" in compute_target or "dask-ct" in compute_target:
+    if "concept" in compute_target:
+        ws.compute_targets[compute_target].delete()
+    elif (
+        "dask-ct" in compute_target
+        and len(ws.compute_targets[compute_target].list_nodes()) == 0
+    ):
         ws.compute_targets[compute_target].delete()
 
 workspaces = Workspace.list(ws.subscription_id, resource_group=ws.resource_group)
