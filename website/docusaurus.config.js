2112b458d21a9aba17b0432b456788d2105470c4
amin <amin.saied@gmail.com>
2020-10-20 10:13:44 -0700

    edit url points to azure.github.com/azureml-examples

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -98,7 +98,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/Azure/azureml-examples/tree/website/website/docs',
         },
         cookbook: {
           sidebarPath: require.resolve('./sidebars.js'),
@@ -107,7 +107,7 @@ module.exports = {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/Azure/azureml-examples/tree/website/website/blog',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
