81a6e90316de7c9f6e45bad12942170af066a7b0
lostmygithubaccount <cody.dkdc2@gmail.com>
2020-10-20 16:48:43 -0700

    comment the cleanup script; get ready to remove concepts

--- cleanup.py
+++ cleanup.py
@@ -1,27 +1,25 @@
+# imports
 import argparse
 from azureml.core import Workspace
 
+# setup argparse 
 parser = argparse.ArgumentParser()
 parser.add_argument("--config", type=str, default="")
 args = parser.parse_args()
 
+# get workspace
 ws = Workspace.from_config(args.config)
 
+# delete all webservices
 for webservice in ws.webservices:
     ws.webservices[webservice].delete()
 
+# delete some compute targets
 for compute_target in ws.compute_targets:
     if ws.compute_targets[compute_target].get_status() in ["Failed", "Canceled"]:
-        ws.compute_targets[compute_target].delete()
-    elif "concept" in compute_target:
         ws.compute_targets[compute_target].delete()
     elif (
         "dask-ct" in compute_target
         and len(ws.compute_targets[compute_target].list_nodes()) == 0
     ):
         ws.compute_targets[compute_target].delete()
-
-workspaces = Workspace.list(ws.subscription_id, resource_group=ws.resource_group)
-for workspace in workspaces:
-    if "concept" in workspace:
-        workspaces[workspace][0].delete(delete_dependent_resources=True, no_wait=True)
