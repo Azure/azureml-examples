e4407d7b93fa220c63d2a00113c81c8bfa83433c
amin saied <aminsaied@users.noreply.github.com>
2020-10-22 10:16:31 -0700

    clean up installation guide (#163)

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -59,8 +59,8 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="A user guide to Azure ML <head />">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
