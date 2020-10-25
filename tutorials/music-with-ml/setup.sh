0152c5e24f222173d001b2c95e4f552066ebc689
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2020-10-25 01:46:16 +0000

    remove setup.sh

--- tutorials/music-with-ml/setup.sh
+++
@@ -1,4 +0,0 @@
-apt-get update -qq && apt-get install -qq libfluidsynth1 fluid-soundfont-gm build-essential libasound2-dev libjack-dev
-pip install -qU setuptools
-pip install -qU pyfluidsynth pretty_midi
-pip install -qU magenta
