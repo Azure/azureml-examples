f201e95bfa421cdda02ddd02a0c04eef9803d251
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-01-31 15:45:26 -0800

    saving

--- readme.py
+++ readme.py
@@ -4,299 +4,302 @@ import json
 import glob
 import argparse
 
-# issue #146
-if "posix" not in os.name:
-    print(
-        "windows is not supported, see issue #146 (https://github.com/Azure/azureml-examples/issues/146)"
-    )
-    exit(1)
-
-# setup argparse
-parser = argparse.ArgumentParser()
-parser.add_argument("--check-readme", type=bool, default=False)
-args = parser.parse_args()
-
-# constants, variables, parameters, etc.
-with open("prefix.md", "r") as f:
-    prefix = f.read()
-with open("suffix.md", "r") as f:
-    suffix = f.read()
-
-tutorial_table = """
-**Tutorials**
-
-path|status|notebooks|description
--|-|-|-
-"""
-
-notebook_table = """
-**Notebooks**
-
-path|description
--|-
-"""
-
-train_table = """
-**Train**
-
-path|compute|environment|description
--|-|-|-
-"""
-
-deploy_table = """
-**Deploy**
-
-path|compute|description
--|-|-
-"""
-
-ws = "default"
-rg = "azureml-examples"
-mn = "${{matrix.notebook}}"
-mw = "${{matrix.workflow}}"
-cr = "${{secrets.AZ_AE_CREDS}}"
-
-kernelspec = {"display_name": "Python 3.8", "language": "python", "name": "python3.8"}
-
-# process tutorials/*
-tutorials = sorted(glob.glob("tutorials/*"))
-
-for tutorial in tutorials:
+# define functions
+def main(args):
+    # get list of tutorials
+    tutorials = sorted(glob.glob("tutorials/*", recursive=False))
 
     # get list of notebooks
-    nbs = sorted(
-        [nb.split("/")[-1] for nb in glob.glob(f"{tutorial}/*.ipynb")]
-    )  # TODO: fix for Windows
-    nbs = [f"[{nb}]({tutorial}/{nb})" for nb in nbs]  # TODO: fix for Windows
-    nbs = "<br>".join(nbs)
+    notebooks = sorted(glob.glob("notebooks/**/*.ipynb", recursive=True))
 
-    # get the tutorial name and initials
-    name = tutorial.split("/")[-1]  # TODO: fix for Windows
-    initials = "".join([word[0][0] for word in name.split("-")])
+    # get list of workflows
+    workflows = sorted(glob.glob("workflows/**/*job*.py", recursive=True))
 
-    # build entries for tutorial table
-    status = f"[![{name}](https://github.com/Azure/azureml-examples/workflows/run-tutorial-{initials}/badge.svg)](https://github.com/Azure/azureml-examples/actions?query=workflow%3Arun-tutorial-{initials})"
-    desc = "*no description*"
-    try:
-        with open(f"{tutorial}/README.md", "r") as f:
-            for line in f.readlines():
-                if "description: " in str(line):
-                    desc = line.split(": ")[-1].strip()
-                    break
-    except:
-        pass
+    # get a list of ALL notebooks, including tutorials
+    all_notebooks = sorted(glob.glob("**/*.ipynb", recursive=True))
 
-    # add row to tutorial table
-    tutorial_table += f"[{name}]({tutorial})|{status}|{nbs}|{desc}\n"
+    # read existing README.md
+    with open("README.md", "r") as f:
+        readme_before = f.read()
 
-# process notebooks/*
-notebooks = sorted(glob.glob("notebooks/*.ipynb"))
+    # make all notebooks consistent
+    modify_notebooks(all_notebooks)
 
-# create `run-workflows` workflow yaml file
-workflow = f"""name: run-notebooks
-on:
-  schedule:
-      - cron: "0 0/2 * * *"
-  push: 
-    branches:
-      - main
-    paths:
-      - "notebooks/**"
-  pull_request:
-    branches:
-      - main
-    paths:
-      - "notebooks/**"
-jobs:
-  build:
-    runs-on: ubuntu-latest 
-    strategy:
-      matrix:
-        notebook: {notebooks}
-    steps:
-    - name: check out repo
-      uses: actions/checkout@v2
-    - name: setup python
-      uses: actions/setup-python@v2
-      with: 
-        python-version: "3.8"
-    - name: pip install
-      run: pip install -r requirements.txt
-    - name: azure login
-      uses: azure/login@v1
-      with:
-        creds: {cr}
-    - name: install azmlcli
-      run: az extension add -s https://azurecliext.blob.core.windows.net/release/azure_cli_ml-1.15.0-py3-none-any.whl -y
-    - name: attach to workspace
-      run: az ml folder attach -w {ws} -g {rg}
-    - name: run notebook
-      run: papermill {mn} out.ipynb -k python
-"""
+    # format code
+    format_code()
 
-# write `run-notebooks` workflow yaml file
-print("writing workflow file...")
-with open(f".github/workflows/run-notebooks.yml", "w") as f:
-    f.write(workflow)
+    # write workflows
+    write_workflows(notebooks, workflows)
 
-# create notebook_table
-for nb in notebooks:
+    # write README.md
+    write_readme(tutorials, notebooks, workflows)
 
-    # read in notebook
-    with open(nb, "r") as f:
-        data = json.load(f)
+    # read modified README.md
+    with open("README.md", "r") as f:
+        readme_after = f.read()
 
-    # read in the description
-    try:
-        if "description: " in str(data["cells"][0]["source"]):
-            desc = (
-                str(data["cells"][0]["source"])
-                .split("description: ")[-1]
-                .replace("']", "")
-                .strip()
-            )
-    except:
-        desc = "*no description*"
+    # check if readme matches
+    if args.check_readme:
+        if not check_readme(readme_before, readme_after):
+            print("README.md file did not match...")
+            exit(2)
 
-    # build tables
-    notebook_table += f"[{nb}]({nb})|{desc}\n"
 
-# process code/azureml/*
-workflows = sorted(glob.glob("workflows/**/*/*job*.py", recursive=True))
+def write_readme(tutorials, notebooks, workflows):
+    # read in prefix.md and suffix.md
+    with open("prefix.md", "r") as f:
+        prefix = f.read()
+    with open("suffix.md", "r") as f:
+        suffix = f.read()
 
-# create `run-workflows` workflow yaml file
-workflow = f"""name: run-workflows
-on:
-  schedule:
-      - cron: "0 0/2 * * *"
-  push: 
-    branches:
-      - main
-    paths:
-      - "workflows/**"
-  pull_request:
-    branches:
-      - main
-    paths:
-      - "workflows/**"
-jobs:
-  build:
-    runs-on: ubuntu-latest 
-    strategy:
-      matrix:
-        workflow: {workflows}
-    steps:
-    - name: check out repo
-      uses: actions/checkout@v2
-    - name: setup python
-      uses: actions/setup-python@v2
-      with: 
-        python-version: "3.8"
-    - name: pip install
-      run: pip install -r requirements.txt
-    - name: azure login
-      uses: azure/login@v1
-      with:
-        creds: {cr}
-    - name: install azmlcli
-      run: az extension add -s https://azurecliext.blob.core.windows.net/release/azure_cli_ml-1.15.0-py3-none-any.whl -y
-    - name: attach to workspace
-      run: az ml folder attach -w {ws} -g {rg}
-    - name: run workflow 
-      run: python {mw}
-"""
+    # define markdown tables
+    tutorial_table = "\n**Tutorials**\n\npath|status|notebooks|description\n-|-|-|-\n"
+    notebook_table = "\n**Notebooks**\n\npath|status|description\n-|-|-\n"
+    train_table = "\n**Train**\n\npath|status|description\n-|-|-\n"
+    deploy_table = "\n**Deploy**\n\npath|status|description\n-|-|-\n"
 
-# write `run-workflows` workflow yaml file
-print("writing workflow file...")
-with open(f".github/workflows/run-workflows.yml", "w") as f:
-    f.write(workflow)
+    # process tutorials
+    for tutorial in tutorials:
+        # get list of notebooks
+        nbs = sorted([nb.split("/")[-1] for nb in glob.glob(f"{tutorial}/*.ipynb")])
+        nbs = [f"[{nb}]({tutorial}/{nb})" for nb in nbs]
+        nbs = "<br>".join(nbs)
 
-# create example tables
-for wf in workflows:
+        # get tutorial name and initials
+        name = tutorial.split("/")[-1]
+        initials = "".join(word[0][0] for word in name.split("-"))
 
-    # read in example
-    with open(wf, "r") as f:
-        data = f.read()
-
-        # read in the description
+        # build entries for tutorial table
+        status = f"[![{name}](https://github.com/Azure/azureml-examples/workflows/tutorial-{initials}/badge.svg)](https://github.com/Azure/azureml-examples/actions?query=workflow%3Atutorial-{initials})"
+        description = "*no description*"
         try:
-            desc = data.split("\n")[0].split(": ")[-1].strip()
+            with open(f"{tutorial}/README.md", "r") as f:
+                for line in f.readlines():
+                    if "description: " in str(line):
+                        description = line.split(": ")[-1].strip()
+                        break
         except:
-            desc = "*no description*"
+            pass
 
-        # build tables
-        if "train" in wf:
-            # parse for compute target
-            if "cpu-cluster" in data:
-                compute = "AML - CPU"
-            elif "gpu-cluster" in data or "gpu-K80" in data or "gpu-V100" in data:
-                compute = "AML - GPU"
-            else:
-                compute = "unknown"
-            # parse for environment type
-            if "Environment.from_pip_requirements" in data:
-                environment = "pip"
-            elif "Environment.from_conda_specification" in data:
-                environment = "conda"
-            elif "env.docker.base_dockerfile" in data:
-                environment = "docker"
-            elif "mlproject" in wf:
-                environment = "mlproject"
-            else:
-                environment = "unknown"
-            train_table += f"[{wf}]({wf})|{compute}|{environment}|{desc}\n"
-        elif "deploy" in wf:
-            if "aci-cpu" in wf:
-                compute = "ACI - CPU"
-            elif "aks-cpu" in wf:
-                compute = "AKS - CPU"
-            elif "aks-gpu" in wf:
-                compute = "AKS - GPU"
-            elif "local" in wf:
-                compute = "local"
-            else:
-                compute = "unknown"
-            deploy_table += f"[{wf}]({wf})|{compute}|{desc}\n"
+        # add row to tutorial table
+        tutorial_table += f"[{name}]({tutorial})|{status}|{nbs}|{description}\n"
 
-# glob all notebooks
-notebooks = sorted(glob.glob("**/**/*.ipynb", recursive=True))
+    # process notebooks
+    for notebook in notebooks:
+        # get notebook name and initials
+        name = notebook.split("/")[-1].replace(".ipynb", "")
+        initials = "".join(word[0][0] for word in name.split("-"))
 
-# process all notebooks and rewrite
-for nb in notebooks:
+        # read in notebook
+        with open(notebook, "r") as f:
+            data = json.load(f)
 
-    # read in notebook
-    with open(nb, "r") as f:
-        data = json.load(f)
+        # build entries for notebook table
+        status = f"[![{name}](https://github.com/Azure/azureml-examples/workflows/notebook-{initials}/badge.svg)](https://github.com/Azure/azureml-examples/actions?query=workflow%3Anotebook-{initials})"
+        description = "*no description*"
+        try:
+            if "description: " in str(data["cells"][0]["source"]):
+                description = (
+                    str(data["cells"][0]["source"])
+                    .split("description: ")[-1]
+                    .replace("']", "")
+                    .strip()
+                )
+        except:
+            pass
 
-    # update metadata
-    data["metadata"]["kernelspec"] = kernelspec
+        # add row to notebook table
+        notebook_table += f"[{notebook}]({notebook})|{status}|{description}\n"
 
-    # write notebook
-    with open(nb, "w") as f:
-        json.dump(data, f, indent=1)
+    # process workflows
+    for workflow in workflows:
+        # get the workflow scenario, tool, project, and name
+        scenario = workflow.split("/")[1]
+        tool = workflow.split("/")[2]
+        project = workflow.split("/")[3]
+        name = workflow.split("/")[4].replace(".py", "")
 
-# run code formatter on .py files
-os.system("black .")
+        # read in workflow
+        with open(workflow, "r") as f:
+            data = f.read()
 
-# run code formatter on .ipynb files
-os.system("black-nb --clear-output .")
+        # build entires for workflow tables
+        status = f"[![{scenario}-{tool}-{project}-{name}](https://github.com/Azure/azureml-examples/workflows/{scenario}-{tool}-{project}-{name}/badge.svg)](https://github.com/Azure/azureml-examples/actions?query=workflow%3A{scenario}-{tool}-{project}-{name})"
+        description = "*no description*"
+        try:
+            description = data.split("\n")[0].split(": ")[-1].strip()
+        except:
+            pass
 
-# read in README.md for comparison
-with open("README.md", "r") as f:
-    before = f.read()
+        # add row to workflow table
+        row = f"[{workflow}]({workflow})|{status}|{description}\n"
+        if scenario == "train":
+            train_table += row
+        elif scenario == "deploy":
+            deploy_table += row
+        else:
+            print("new scenario! modifications needed...")
+            exit(3)
 
-# write README.md file
-print("writing README.md...")
-with open("README.md", "w") as f:
-    f.write(
-        prefix + tutorial_table + notebook_table + train_table + deploy_table + suffix
-    )
+    # write README.md
+    print("writing README.md...")
+    with open("README.md", "w") as f:
+        f.write(
+            prefix
+            + tutorial_table
+            + notebook_table
+            + train_table
+            + deploy_table
+            + suffix
+        )
 
-# read in README.md for comparison
-with open("README.md", "r") as f:
-    after = f.read()
 
-# check if README.md file matches before and after
-if args.check_readme and before != after:
-    print("README.md file did not match...")
-    exit(2)
+def write_workflows(notebooks, workflows):
+    # process notebooks
+    for notebook in notebooks:
+        # get notebook name and initials
+        name = notebook.split("/")[-1].replace(".ipynb", "")
+        initials = "".join(word[0][0] for word in name.split("-"))
+
+        # write workflow file
+        write_notebook_workflow(notebook, name, initials)
+
+    # process workflows
+    for workflow in workflows:
+        # get the workflow scenario, tool, project, and name
+        scenario = workflow.split("/")[1]
+        tool = workflow.split("/")[2]
+        project = workflow.split("/")[3]
+        name = workflow.split("/")[4].replace(".py", "")
+
+        # write workflow file
+        write_python_workflow(workflow, scenario, tool, project, name)
+
+
+def check_readme(before, after):
+    return before == after
+
+
+def modify_notebooks(notebooks):
+    # setup variables
+    kernelspec = {
+        "display_name": "Python 3.8",
+        "language": "python",
+        "name": "python3.8",
+    }
+
+    # for each notebooks
+    for notebook in notebooks:
+
+        # read in notebook
+        with open(notebook, "r") as f:
+            data = json.load(f)
+
+        # update metadata
+        data["metadata"]["kernelspec"] = kernelspec
+
+        # write notebook
+        with open(notebook, "w") as f:
+            json.dump(data, f, indent=1)
+
+
+def format_code():
+    # run code formatter on .py files
+    os.system("black .")
+
+    # run code formatter on .ipynb files
+    os.system("black-nb --clear-output .")
+
+
+def write_notebook_workflow(notebook, name, initials):
+    workflow_yaml = f"""name: notebook-{initials}
+on:
+  schedule:
+    - cron: "0 0/2 * * *"
+  pull_request:
+    branches:
+      - main
+    paths:
+      - {notebook}
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+    - name: check out repo
+      uses: actions/checkout@v2
+    - name: setup python
+      uses: actions/setup-python@v2
+      with: 
+        python-version: "3.8"
+    - name: pip install
+      run: pip install -r requirements.txt
+    - name: azure login
+      uses: azure/login@v1
+      with:
+        creds: ${{secrets.AZ_AE_CREDS}}
+    - name: install azmlcli
+      run: az extension add -n azure-cli-ml -y
+    - name: attach to workspace
+      run: az ml folder attach -w ${{secrets.WORKSPACE}} -g ${{secrets.RESOURCE_GROUP}}
+    - name: run notebook
+      run: papermill {notebook} out.ipynb -k python\n"""
+
+    # write workflow
+    with open(f".github/workflows/notebook-{initials}.yml", "w") as f:
+        f.write(workflow_yaml)
+
+
+def write_python_workflow(workflow, scenario, tool, project, name):
+    workflow_yaml = f"""name: {scenario}-{tool}-{project}-{name}
+on:
+  schedule:
+    - cron: "0 0/2 * * *"
+  pull_request:
+    branches:
+      - main
+    paths:
+      - {workflow}
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+    - name: check out repo
+      uses: actions/checkout@v2
+    - name: setup python
+      uses: actions/setup-python@v2
+      with: 
+        python-version: "3.8"
+    - name: pip install
+      run: pip install -r requirements.txt
+    - name: azure login
+      uses: azure/login@v1
+      with:
+        creds: ${{secrets.AZ_AE_CREDS}}
+    - name: install azmlcli
+      run: az extension add -n azure-cli-ml -y
+    - name: attach to workspace
+      run: az ml folder attach -w ${{secrets.WORKSPACE}} -g ${{secrets.RESOURCE_GROUP}}
+    - name: run workflow
+      run: python {workflow}\n"""
+
+    # write workflow
+    with open(f".github/workflows/{scenario}-{tool}-{project}-{name}.yml", "w") as f:
+        f.write(workflow_yaml)
+
+
+# run functions
+if __name__ == "__main__":
+    # issue #146
+    if "posix" not in os.name:
+        print(
+            "windows is not supported, see issue #146 (https://github.com/Azure/azureml-examples/issues/146)"
+        )
+        exit(1)
+
+    # setup argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--check-readme", type=bool, default=False)
+    args = parser.parse_args()
+
+    # call main
+    main(args)
