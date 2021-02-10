c65ae99925758173c692774030ae5dbd8b2d7a7f
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-02-10 11:09:32 -0800

    Update train.py (#371)

--- workflows/train/scikit-learn/diabetes-mlproject/src/train.py
+++ workflows/train/scikit-learn/diabetes-mlproject/src/train.py
@@ -6,6 +6,7 @@ from sklearn.datasets import load_diabetes
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+import sys
 import mlflow
 import mlflow.sklearn
 
