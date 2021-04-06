7d9f87e7873b92a46c854bd50d383a13ee998bc2
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-04-06 11:56:02 -0700

    minor edit to scikit-learn example

--- workflows/train/scikit-learn/diabetes/src/train.py
+++ workflows/train/scikit-learn/diabetes/src/train.py
@@ -12,7 +12,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 data = {"train": {"X": X_train, "y": y_train}, "test": {"X": X_test, "y": y_test}}
 
 # Enable auto logging
-mlflow.sklearn.autolog()
+mlflow.autolog()
 
 # Create, fit, and test the scikit-learn Ridge regression model
 regression_model = Ridge(alpha=0.03)
