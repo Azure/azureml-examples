df71fba43db1db22d564fa6669e2400fc8775426
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-04-09 14:26:42 -0700

    update manage-assets and test it

--- experimental/using-cli/manage-assets.sh
+++ experimental/using-cli/manage-assets.sh
@@ -1,28 +1,6 @@
 ## IMPORTANT: this file and accompanying assets are the source for snippets in https://docs.microsoft.com/azure/machine-learning! 
 ## Please reach out to the Azure ML docs & samples team before before editing for the first time.
 
-# <install>
-#az extension add -n ml
-# </install>
-
-# <update>
-#az extension update -n ml
-# </update>
-
-# <create_resource_group>
-az group create -n azureml-examples -l eastus
-# </create_resource_group>
-
-# <create_workspace>
-az workspace create --name main -l eastus
-# </create_workspace>
-
-# <configure_defaults>
-az configure --defaults workspace="main"
-az configure --defaults location="eastus"
-az configure --defaults group="azureml-examples"
-# </configure_defaults>
-
 # <create_data>
 az ml data create -f assets/data/iris-url.yml
 # </create_data>
