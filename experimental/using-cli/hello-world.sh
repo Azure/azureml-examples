b565c367a83869f431a97a4be46673b1430023f8
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-04-12 09:24:18 -0700

    fix hello world

--- experimental/using-cli/hello-world.sh
+++ experimental/using-cli/hello-world.sh
@@ -2,5 +2,13 @@
 ## Please reach out to the Azure ML docs & samples team before before editing for the first time.
 
 # <hello_world>
-az ml job create -f hello-world.yml
+job_id=`az ml job create -f hello-world.yml -o tsv | cut -f11`
 # </hello_world>
+
+# <show_job_in_studio>
+az ml job show -n $job_id --web
+# </show_job_in_studio>
+
+# <stream_job_logs_to_console>
+az ml job stream -n $job_id
+# </stream_job_logs_to_console>
