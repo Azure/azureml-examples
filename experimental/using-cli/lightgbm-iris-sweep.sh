942b98944fdc4de90fcec730ad388de72e962545
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-04-12 14:38:16 -0700

    saving - run sweep job

---
+++ experimental/using-cli/lightgbm-iris-sweep.sh
@@ -0,0 +1,33 @@
+## IMPORTANT: this file and accompanying assets are the source for snippets in https://docs.microsoft.com/azure/machine-learning! 
+## Please reach out to the Azure ML docs & samples team before before editing for the first time.
+
+# <create_compute>
+az ml compute create -n cpu-cluster --min-instances 0 --max-instances 2 --type AmlCompute
+# </create_compute>
+
+# <create_sweep_job>
+job_id=`az ml job create -f jobs/train/lightgbm/iris/sweep.yml -o tsv | cut -f11`
+# </create_sweep_job>
+
+echo $job_id
+
+# <show_job_in_studio>
+az ml job show -n $job_id --web
+# </show_job_in_studio>
+
+# <stream_job_logs_to_console>
+#az ml job stream -n $job_id
+# </stream_job_logs_to_console>
+
+# <download_outputs>
+#az ml job download -n $job_id --outputs
+# </download_outputs>
+
+# <create_endpoint>
+#az ml endpoint create
+# </create_endpoint>
+
+# <score_endpoint>
+#curl something | something
+# </score_endpoint>
+
---
+++ experimental/using-cli/pytorch-cifar-distributed.sh
@@ -0,0 +1,28 @@
+## IMPORTANT: this file and accompanying assets are the source for snippets in https://docs.microsoft.com/azure/machine-learning! 
+## Please reach out to the Azure ML docs & samples team before before editing for the first time.
+
+# <create_compute>
+az ml compute create -n gpu-cluster --min-instances 0 --max-instances 2 --type AmlCompute --size Standard_NC12
+# </create_compute>
+
+# <download_cifar>
+wget "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
+# </download_cifar>
+
+# <untar_cifar>
+tar -xvzf cifar-10-python.tar.gz
+# </untar_cifar>
+
+# <remove_tar>
+rm cifar-10-python.tar.gz
+# </remove_tar>
+
+# <create_data>
+# az ml data create ?? --local-path cifar-10-batches-py
+# </create_data>
+
+# <create_basic_job>
+#job_id=`az ml job create -f jobs/train/pytorch/cifar-distributed/basic.yml -o tsv | cut -f11`
+# </create a basic job>
+
+ 
