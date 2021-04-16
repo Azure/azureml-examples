5361a1ceb52e2839ca6119e9b1f7d9ed3d772878
rsethur <rsethur@gmail.com>
2021-04-16 14:40:57 -0700

    online endpoints

---
+++ cli/how-to-deploy-declarative-safe-rollout-online-endpoints.sh
@@ -0,0 +1,33 @@
+## IMPORTANT: this file and accompanying assets are the source for snippets in https://docs.microsoft.com/azure/machine-learning! 
+## Please reach out to the Azure ML docs & samples team before before editing for the first time.
+
+# delete endpoint if it already exists
+az ml endpoint delete -n my-new-endpoint --yes
+
+# <scale_blue>
+az ml endpoint update -n my-new-endpoint -f endpoints/online/managed/canary-declarative-flow/2-scale-blue.yaml
+# </scale_blue>
+
+# <create_green>
+az ml endpoint update -n my-new-endpoint -f endpoints/online/managed/canary-declarative-flow/3-create-green.yaml
+# </create_green>
+
+# <test_green>
+az ml endpoint update -n my-new-endpoint --f endpoints/online/managed/canary-declarative-flow/4-flight-green.yaml
+# </test_green>
+
+# <green_10pct_traffic>
+az ml endpoint update -n my-new-endpoint -f endpoints/online/managed/canary-declarative-flow/4-flight-green.yaml
+# </green_10pct_traffic>
+
+# <green_100pct_traffic>
+az ml endpoint update -n my-new-endpoint -f endpoints/online/managed/canary-declarative-flow/5-full-green.yaml
+# </green_100pct_traffic>
+
+# <delete_blue>
+az ml endpoint update -n my-new-endpoint -f endpoints/online/managed/canary-declarative-flow/6-delete-blue.yaml
+# </delete_blue>
+
+# <delete_endpoint>
+az ml endpoint delete -n my-new-endpoint
+# </delete_endpoint>
---
+++ cli/how-to-deploy-managed-online-endpoint.sh
@@ -0,0 +1,37 @@
+## IMPORTANT: this file and accompanying assets are the source for snippets in https://docs.microsoft.com/azure/machine-learning! 
+## Please reach out to the Azure ML docs & samples team before before editing for the first time.
+
+# This is needed temporarily
+export WS=mir-mir-msdocs-ws
+export RG=mir-msdocs
+export LOC=westus2
+az configure --defaults group=$RG
+az configure --defaults location=$LOC
+az configure --defaults workspace=$WS
+
+# delete endpoint if it already exists
+az ml endpoint delete -n my-endpoint --yes
+
+# <deploy>
+az ml endpoint create --name my-endpoint -f endpoints/online/managed/simple-flow/1-create-endpoint-with-blue.yaml
+# </deploy>
+
+# <get_status>
+az ml endpoint show -n my-endpoint
+# </get_status>
+
+# <test_endpoint>
+az ml endpoint invoke -n my-endpoint --request-file endpoints/online/model-1/sample-request.json
+# </test_endpoint>
+
+# <get_scoring_uri>
+az ml endpoint show -n my-endpoint --query "scoring_uri"
+# </get_scoring_uri>
+
+# <get_access_token>
+az ml endpoint list-keys -n my-endpoint
+# </get_access_token>
+
+# <delete_endpoint>
+az ml endpoint delete -n my-endpoint --yes
+# </delete_endpoint>
