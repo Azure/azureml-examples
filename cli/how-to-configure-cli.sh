b31a75e0e09fd652eeb5a2768a3cf04be52fb353
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-04-19 18:39:10 -0700

    docs edits (#440)
    * docs edits
    
    * save
    
    * remove upgrade
    
    * fix
    
    * reorder

--- cli/how-to-configure-cli.sh
+++ cli/how-to-configure-cli.sh
@@ -1,45 +1,6 @@
 ## IMPORTANT: this file and accompanying assets are the source for snippets in https://docs.microsoft.com/azure/machine-learning! 
 ## Please reach out to the Azure ML docs & samples team before before editing for the first time.
 
-# <az_account_set>
-az account set -s "<YOUR_SUBSCRIPTION_NAME>"
-# </az_account_set>
-
-# <az_extension_remove>
-az extension remove -n azure-cli-ml
-az extension remove -n ml
-# </az_extension_remove>
-
-# <az_version>
-az version 
-# </az_version>
-
-# <az_upgrade>
-az upgrade
-# </az_upgrade>
-
-# <az_extension_list>
-az extension list 
-# <check_extension_list>
-
-# <az_ml_install>
-az extension add -n ml
-# </az_ml_install>
-
-# <az_ml_update>
-az extension update -n ml
-# </az_ml_update>
-
-# <az_ml_verify>
-az ml -h
-# </az_ml_verify>
-
-# <export_variables_placeholders>
-export WS="<YOUR_WORKSPACE_NAME>"
-export RG="<YOUR_RESOURCE_GROUP_NAME>"
-export LOC="<YOUR_AZURE_LOCATION>"
-# </export_variables_placeholders>
-
 # <export_variables>
 export WS="main"
 export RG="azureml-examples"
@@ -59,10 +20,6 @@ az configure --defaults group=$RG
 az configure --defaults location=$LOC
 az configure --defaults workspace=$WS
 # </az_configure_defaults>
-
-# <hello_world>
-az ml job create -f jobs/hello-world.yml
-# </hello_world>
 
 # <hello_world_output>
 job_id=`az ml job create -f jobs/hello-world.yml --query name -o tsv`
@@ -95,3 +52,38 @@ else
   exit 2
 fi
 # </check_job_status>
+
+# <az_version>
+az version 
+# </az_version>
+
+# <az_extension_list>
+az extension list 
+# <check_extension_list>
+
+# <az_ml_install>
+az extension add -n ml
+# </az_ml_install>
+
+# <az_ml_update>
+az extension update -n ml
+# </az_ml_update>
+
+# <az_ml_verify>
+az ml -h
+# </az_ml_verify>
+
+# <export_variables_placeholders>
+export WS="<YOUR_WORKSPACE_NAME>"
+export RG="<YOUR_RESOURCE_GROUP_NAME>"
+export LOC="<YOUR_AZURE_LOCATION>"
+# </export_variables_placeholders>
+
+# <az_account_set>
+az account set -s "<YOUR_SUBSCRIPTION_NAME>"
+# </az_account_set>
+
+# <az_extension_remove>
+az extension remove -n azure-cli-ml
+az extension remove -n ml
+# </az_extension_remove>
