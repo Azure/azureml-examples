423e96893f28950de7abd64e430b59157860ae77
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-04-20 09:52:53 -0700

    edits

--- cli/how-to-deploy-cli-simple.sh
+++ cli/how-to-deploy-cli-simple.sh
@@ -3,10 +3,10 @@
 
 endpoint_id=`az ml endpoint create -f endpoints/online/model-1/simple-flow/1-create-endpoint-with-blue.yml --query name -o tsv`
 
-az ml endpoint show -n endpoint_id
+az ml endpoint show -n $endpoint_id
 
-az ml endpoint invoke -n endpoint_id --request-file endpoints/online/model-1/sample-request.json
+az ml endpoint invoke -n $endpoint_id --request-file endpoints/online/model-1/sample-request.json
 
-az ml endpoint log -n endpoint_id --deployment blue --tail 100
+az ml endpoint log -n $endpoint_id --deployment blue --tail 100
 
-az ml endpoint delete -n endpoint_id -y
+az ml endpoint delete -n $endpoint_id -y
