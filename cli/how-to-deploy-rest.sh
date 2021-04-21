3fe2b460f393ac0f9ace826a667864ba846e7f39
trent <tjmcc01@gmail.com>
2021-04-21 08:40:02 -0700

    Add REST examples (#439)
    * Add scripts without blob cli
    
    * Everything working for endpoint except discrimnator issue with online deployment
    
    * Working simple job flow
    
    * Use default container for data
    
    * Rename scripts, make wait_for_completion a function, fix online deployment
    
    * Add scoring to endpoint
    
    * Change structure of folders
    
    * Remove todo
    
    * Move rest to cli folder
    
    * Switch to lightgbm from xgboost
    
    * Fix wait_for_completion
    
    * Get AZURE_STORAGE_KEY from az cli
    
    * Change MIR region to westus
    
    * Remove exports from variables
    
    * Fix scoring the endpoint
    
    Co-authored-by: Trent McCormick <trmccorm@microsoft.com>

---
+++ cli/how-to-deploy-rest.sh
@@ -0,0 +1,183 @@
+#!/bin/bash
+
+## IMPORTANT: this file and accompanying assets are the source for snippets in https://docs.microsoft.com/azure/machine-learning! 
+## Please reach out to the Azure ML docs & samples team before before editing for the first time.
+
+# <installation>
+# az extension add -n ml
+# </installation>
+
+# <create environment variables>
+SUBSCRIPTION_ID="7ab7d5bc-5d9e-47ef-80e6-2dffa8ca83a1"
+RESOURCE_GROUP="trmccorm-centraluseuap"
+WORKSPACE="trmccorm-centraluseuap"
+API_VERSION="2021-03-01-preview"
+
+TOKEN=$(az account get-access-token | jq -r ".accessToken")
+
+AZURE_STORAGE_ACCOUNT="trmccormcentra1277620275"
+AZURE_STORAGE_KEY=$(az storage account keys list --account-name $AZURE_STORAGE_ACCOUNT | jq '.[0].value')
+AZUREML_DEFAULT_CONTAINER="azureml-blobstore-2e2e441d-f57b-41fa-bd88-49136cef6140"
+#</create environment variables>
+
+# <create resource group>
+# az group create -n azureml-examples-cli -l eastus
+# </create resource group>
+
+# <create workspace>
+# az ml workspace create --name main -g azureml-examples-cli
+# </create workspace>
+
+# <configure-defaults>
+az configure --defaults workspace=$WORKSPACE
+az configure --defaults location="centraluseuap"
+az configure --defaults group=$RESOURCE_GROUP
+# </configure-defaults>
+
+# define how to wait
+wait_for_completion () {
+    operationid=$(echo $1 | grep -Fi Azure-AsyncOperation | sed "s/azure-asyncoperation: //" | tr -d '\r')
+    # TODO error handling here
+    operation_status="unknown"
+
+    while [[ $operation_status != "Succeeded" && $operation_status != "Failed" ]]
+    do
+        $echo "Getting operation status from: $operationid"
+        operation_result=$(curl --location --request GET $operationid --header "Authorization: Bearer $TOKEN")
+        # TODO error handling here
+        operation_status=$(echo $operation_result | jq -r ".status")
+        echo "Current operation status: $operation_status"
+        sleep 5
+    done
+}
+
+# delete endpoint
+curl --location --request DELETE "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/onlineEndpoints/my-endpoint?api-version=$API_VERSION" \
+--header "Content-Type: application/json" \
+--header "Authorization: Bearer $TOKEN"
+
+# TODO: we can get the default container from listing datastores
+# TODO using the latter two as env vars shouldn't be necessary
+az storage blob upload-batch -d $AZUREML_DEFAULT_CONTAINER/score \
+ -s endpoints/online/model-1/onlinescoring --account-name $AZURE_STORAGE_ACCOUNT --account-key $AZURE_STORAGE_KEY
+
+# <create code>
+curl --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/codes/score-sklearn/versions/1?api-version=$API_VERSION" \
+--header "Authorization: Bearer $TOKEN" \
+--header "Content-Type: application/json" \
+--data-raw "{
+  \"properties\": {
+    \"description\": \"Score code\",
+    \"datastoreId\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/datastores/workspaceblobstore\",
+    \"path\": \"score\"
+  }
+}"
+# </create code>
+
+# upload model
+az storage blob upload-batch -d $AZUREML_DEFAULT_CONTAINER/model \
+ -s endpoints/online/model-1/model --account-name $AZURE_STORAGE_ACCOUNT --account-key $AZURE_STORAGE_KEY
+
+# <create model>
+curl --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/models/sklearn/versions/1?api-version=$API_VERSION" \
+--header "Authorization: Bearer $TOKEN" \
+--header "Content-Type: application/json" \
+--data-raw "{
+    \"properties\": {
+        \"datastoreId\":\"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/datastores/workspaceblobstore\",
+        \"path\": \"model/sklearn_regression_model.pkl\",
+    }
+}"
+# </create model>
+
+# <create environment>
+curl --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/environments/sklearn-env/versions/1?api-version=$API_VERSION" \
+--header "Authorization: Bearer $TOKEN" \
+--header "Content-Type: application/json" \
+--data-raw "{
+    \"properties\":{
+        \"condaFile\": \"channels:\n  - conda-forge\ndependencies:\n  - python=3.6.1\n  - numpy\n  - pip\n  - scikit-learn==0.19.1\n  - scipy\n  - pip:\n    - azureml-defaults\n    - inference-schema[numpy-support]\n    - joblib\n    - numpy\n    - scikit-learn==0.19.1\n    - scipy\",
+        \"Docker\": {
+            \"DockerSpecificationType\": \"Image\",
+            \"DockerImageUri\": \"mcr.microsoft.com/azureml/intelmpi2018.3-ubuntu16.04:20210301.v1\"
+        }
+    }
+}"
+# </create environment>
+
+# TODO: had to change syntax to get headers
+#<create endpoint>
+headers=$(curl -i -H --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/onlineEndpoints/my-endpoint?api-version=$API_VERSION" \
+--header "Content-Type: application/json" \
+--header "Authorization: Bearer $TOKEN" \
+--data-raw "{
+    \"identity\": {
+       \"type\": \"systemAssigned\"
+    },
+    \"properties\": {
+        \"authMode\": \"AMLToken\",
+        \"traffic\": { \"blue\": 100 }
+    },
+    \"location\": \"westus\"
+}")
+#</create endpoint>
+
+echo $headers
+wait_for_completion $headers
+
+# <create deployment>
+headers=$(curl -i -H --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/onlineEndpoints/my-endpoint/deployments/blue?api-version=$API_VERSION" \
+--header "Content-Type: application/json" \
+--header "Authorization: Bearer $TOKEN" \
+--data-raw "{
+    \"location\": \"westus\",
+    \"properties\": {
+        \"endpointComputeType\": \"Managed\",
+        \"scaleSettings\": {
+            \"scaleType\": \"Manual\",
+            \"instanceCount\": 1,
+            \"minInstances\": 1,
+            \"maxInstances\": 2
+        },
+        \"model\": {
+            \"referenceType\": \"Id\",
+            \"assetId\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/models/sklearn/versions/1\"
+        },
+        \"codeConfiguration\": {
+            \"codeId\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/codes/score-sklearn/versions/1\",
+            \"scoringScript\": \"score.py\"
+        },
+        \"environmentId\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/environments/sklearn-env\",
+        \"InstanceType\": \"Standard_F2s_v2\"
+    }
+}")
+#</create deployment>
+
+echo $headers
+wait_for_completion $headers
+
+# <get endpoint>
+response=$(curl --location --request GET "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/onlineEndpoints/my-endpoint?api-version=$API_VERSION" \
+--header "Content-Type: application/json" \
+--header "Authorization: Bearer $TOKEN")
+
+scoringUri=$(echo $response | jq -r ".properties" | jq -r ".scoringUri")
+# </get endpoint>
+
+# <get access token>
+response=$(curl -H "Content-Length: 0" --location --request POST "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/onlineendpoints/my-endpoint/token?api-version=$API_VERSION" \
+--header "Authorization: Bearer $TOKEN")
+accessToken=$(echo $response | jq -r ".accessToken")
+# </get access token>
+
+# <score endpoint>
+curl --location --request POST $scoringUri \
+--header "Authorization: Bearer $accessToken" \
+--header "Content-Type: application/json" \
+--data-raw @endpoints/online/model-1/sample-request.json
+# </score endpoint>
+
+# delete endpoint
+curl --location --request DELETE "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/onlineEndpoints/my-endpoint?api-version=$API_VERSION" \
+--header "Content-Type: application/json" \
+--header "Authorization: Bearer $TOKEN"
---
+++ cli/how-to-train-rest.sh
@@ -0,0 +1,189 @@
+#!/bin/bash
+
+## IMPORTANT: this file and accompanying assets are the source for snippets in https://docs.microsoft.com/azure/machine-learning! 
+## Please reach out to the Azure ML docs & samples team before before editing for the first time.
+
+# <installation>
+# az extension add -n ml
+# </installation>
+
+# <create environment variables>
+SUBSCRIPTION_ID="7ab7d5bc-5d9e-47ef-80e6-2dffa8ca83a1"
+RESOURCE_GROUP="trmccorm-centraluseuap"
+WORKSPACE="trmccorm-centraluseuap"
+API_VERSION="2021-03-01-preview"
+COMPUTE_NAME="e2ecpucluster"
+
+TOKEN=$(az account get-access-token | jq -r ".accessToken")
+
+AZURE_STORAGE_ACCOUNT="trmccormcentra1277620275"
+AZURE_STORAGE_KEY=$(az storage account keys list --account-name $AZURE_STORAGE_ACCOUNT | jq '.[0].value')
+AZUREML_DEFAULT_CONTAINER="azureml-blobstore-2e2e441d-f57b-41fa-bd88-49136cef6140"
+#</create environment variables>
+
+# <create resource group>
+# az group create -n azureml-examples-cli -l eastus
+# </create resource group>
+
+# <create workspace>
+# az ml workspace create --name main -g azureml-examples-cli
+# </create workspace>
+
+# <configure-defaults>
+az configure --defaults workspace=$WORKSPACE
+az configure --defaults location="centraluseuap"
+az configure --defaults group=$RESOURCE_GROUP
+# </configure-defaults>
+
+wait_for_completion () {
+    # TODO error handling here
+    job_status="unknown"
+
+    while [[ $job_status != "Completed" && $job_status != "Failed" ]]
+    do
+        $echo "Getting job status from: $1"
+        job=$(curl --location --request GET "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/jobs/$1?api-version=$API_VERSION" \
+            --header "Authorization: Bearer $TOKEN")
+        # TODO error handling here
+        job_status=$(echo $job | jq -r ".properties" | jq -r ".status")
+        echo "Current job status: $job_status"
+        sleep 5
+    done
+}
+
+# <create environment>
+curl --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/environments/lightgbm-environment/versions/1?api-version=$API_VERSION" \
+--header "Authorization: Bearer $TOKEN" \
+--header "Content-Type: application/json" \
+--data-raw "{
+    \"properties\":{
+        \"condaFile\": \"name: python-ml-basic-cpu\nchannels:\n  - conda-forge\ndependencies:\n  - python=3.8\n  - pip\n  - pip:\n    - numpy\n    - pandas\n    - scipy\n    - scikit-learn\n    - matplotlib\n    - xgboost\n    - lightgbm\n    - dask\n    - distributed\n    - dask-ml\n    - adlfs\n    - fastparquet\n    - pyarrow\n    - mlflow\n    - azureml-mlflow\n    \n\",
+        \"Docker\": {
+            \"DockerSpecificationType\": \"Image\",
+            \"DockerImageUri\": \"mcr.microsoft.com/azureml/openmpi3.1.2-ubuntu18.04\"
+        }
+    }
+}"
+# </create environment>
+
+# TODO decide whether create datastore
+
+# <create datastore>
+# curl --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/datastores/localuploads?api-version=$API_VERSION" \
+# --header "Authorization: Bearer $TOKEN" \
+# --header "Content-Type: application/json" \
+# --data-raw "{
+#     \"properties\": {
+#         \"contents\": {
+#             \"contentsType\": \"AzureBlob\",
+#             \"accountName\": \"$AZURE_STORAGE_ACCOUNT\",
+#             \"containerName\": \"$AZUREML_DEFAULT_CONTAINER\",
+#             \"endpoint\": \"core.windows.net\",
+#             \"protocol\": \"https\",
+#             \"credentials\": {
+#                 \"credentialsType\": \"AccountKey\",
+#                 \"secrets\": {
+#                     \"key\": \"$AZURE_STORAGE_KEY\"
+#                 }
+#             }
+#         },
+#         \"hasBeenValidated\": false,
+#         \"isDefault\": false,
+#         \"description\": \"My local uploads\",
+#     }
+# }"
+#</create datastore>
+
+#<create data>
+curl --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/data/iris-data/versions/1?api-version=$API_VERSION" \
+--header "Authorization: Bearer $TOKEN" \
+--header "Content-Type: application/json" \
+--data-raw "{
+  \"properties\": {
+    \"description\": \"Iris datset\",
+    \"datasetType\": \"Simple\",
+    \"path\": \"https://azuremlexamples.blob.core.windows.net/datasets/iris.csv\"
+  }
+}"
+#</create data>
+
+# TODO: we can get the default container from listing datastores
+# TODO using the latter two as env vars shouldn't be necessary
+az storage blob upload-batch -d $AZUREML_DEFAULT_CONTAINER/src \
+ -s jobs/train/lightgbm/iris/src --account-name $AZURE_STORAGE_ACCOUNT --account-key $AZURE_STORAGE_KEY
+
+#<create code>
+curl --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/codes/train-lightgbm/versions/1?api-version=$API_VERSION" \
+--header "Authorization: Bearer $TOKEN" \
+--header "Content-Type: application/json" \
+--data-raw "{
+  \"properties\": {
+    \"description\": \"Train code\",
+    \"datastoreId\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/datastores/workspaceblobstore\",
+    \"path\": \"src\"
+  }
+}"
+#</create code>
+
+# <create job>
+jobid=$(uuidgen)
+curl --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/jobs/$jobid?api-version=$API_VERSION" \
+--header "Authorization: Bearer $TOKEN" \
+--header "Content-Type: application/json" \
+--data-raw "{
+    \"properties\": {
+        \"jobType\": \"Command\",
+        \"codeId\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/codes/train-lightgbm/versions/1\",
+        \"command\": \"python main.py --iris-csv \$AZURE_ML_INPUT_iris\",
+        \"environmentId\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/environments/lightgbm-environment/versions/1\",
+        \"inputDataBindings\": {
+            \"iris\": {
+                \"dataId\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/data/iris-data/versions/1\"
+            }
+        },
+        \"experimentName\": \"lightgbm-iris\",
+        \"compute\": {
+            \"target\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/computes/$COMPUTE_NAME\",
+            \"instanceCount\": 1
+        }
+    }
+}"
+# </create job>
+
+wait_for_completion $jobid
+
+# <create a sweep job>
+jobid=$(uuidgen)
+curl --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/jobs/$jobid?api-version=$API_VERSION" \
+--header "Authorization: Bearer $TOKEN" \
+--header "Content-Type: application/json" \
+--data-raw "{
+    \"properties\": {
+        \"algorithm\": \"Random\",
+        \"jobType\": \"Sweep\",
+        \"trial\":{
+            \"codeId\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/codes/train-lightgbm/versions/1\",
+            \"command\": \"python main.py --iris-csv \$AZURE_ML_INPUT_iris\",
+            \"environmentId\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/environments/lightgbm-environment/versions/1\"
+        },
+        \"experimentName\": \"lightgbm-iris-sweep\",
+        \"compute\": {
+            \"target\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/computes/$COMPUTE_NAME\",
+            \"instanceCount\": 1
+        },
+        \"objective\": {
+            \"primaryMetric\": \"test-multi_logloss\",
+            \"goal\": \"minimize\"
+        },
+        \"searchSpace\": {
+            \"--learning_rate\": [\"uniform\", [0.01, 0.9]],
+            \"--boosting\":[\"choice\",[[\"gbdt\",\"dart\"]]]
+        },
+        \"maxTotalTrials\": 20,
+        \"maxConcurrentTrials\": 10,
+        \"timeout\": \"PT120M\"
+    }
+}"
+# </create a sweep job>
+
+wait_for_completion $jobid
