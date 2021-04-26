38d92e2bdd521677b1a6b26d85f28982014e1b4d
tmccrmck <tjmcc01@gmail.com>
2021-04-26 16:22:00 -0700

    Fix typo

--- cli/how-to-deploy-rest.sh
+++ cli/how-to-deploy-rest.sh
@@ -165,7 +165,7 @@ scoringUri=$(echo $response | jq -r ".properties" | jq -r ".scoringUri")
 # </get endpoint>
 
 # <get access token>
-response=$(curl -H "Content-Length: 0" --location --request POST "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/onlinendpoints/my-endpoint/token?api-version=$API_VERSION" \
+response=$(curl -H "Content-Length: 0" --location --request POST "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/onlineEndpoints/my-endpoint/token?api-version=$API_VERSION" \
 --header "Authorization: Bearer $TOKEN")
 accessToken=$(echo $response | jq -r ".accessToken")
 # </get access token>
@@ -178,7 +178,7 @@ curl --location --request POST $scoringUri \
 # </score endpoint>
 
 # <get deployment logs>
-curl --location --request POST "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/onlineendpoints/my-endpoint/deployments/blue/getLogs?api-version=$API_VERSION" \
+curl --location --request POST "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/onlineEndpoints/my-endpoint/deployments/blue/getLogs?api-version=$API_VERSION" \
 --header "Authorization: Bearer $TOKEN" \
 --header "Content-Type: application/json" \
 --data-raw "{ \"tail\": 100 }"
