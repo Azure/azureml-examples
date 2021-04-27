abeb15026b3d1b0f55c1debd121926aa243e6667
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-04-27 01:01:18 +0000

    Revert "run readme.py; add pipelines to requirements.txt"
    This reverts commit 77ee59a752773765b816cecf15c9447dd68fb26b.

--- notebooks/machine-learning-pipelines/intro-to-pipelines/keras-mnist-fashion/train.py
+++ notebooks/machine-learning-pipelines/intro-to-pipelines/keras-mnist-fashion/train.py
@@ -15,7 +15,7 @@ from azureml.core import Run
 
 # dataset object from the run
 run = Run.get_context()
-dataset = run.input_datasets["prepared_fashion_ds"]
+dataset = run.input_datasets['prepared_fashion_ds']
 
 # split dataset into train and test set
 (train_dataset, test_dataset) = dataset.random_split(percentage=0.8, seed=111)
@@ -38,11 +38,9 @@ X_test = np.array(data_test.iloc[:, 1:])
 y_test = to_categorical(np.array(data_test.iloc[:, 0]))
 
 
-X_train = (
-    X_train.reshape(X_train.shape[0], img_rows, img_cols, 1).astype("float32") / 255
-)
-X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1).astype("float32") / 255
-X_val = X_val.reshape(X_val.shape[0], img_rows, img_cols, 1).astype("float32") / 255
+X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1).astype('float32') / 255
+X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1).astype('float32') / 255
+X_val = X_val.reshape(X_val.shape[0], img_rows, img_cols, 1).astype('float32') / 255
 
 batch_size = 256
 num_classes = 10
@@ -50,32 +48,25 @@ epochs = 10
 
 # construct neuron network
 model = Sequential()
-model.add(
-    Conv2D(
-        32,
-        kernel_size=(3, 3),
-        activation="relu",
-        kernel_initializer="he_normal",
-        input_shape=input_shape,
-    )
-)
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 kernel_initializer='he_normal',
+                 input_shape=input_shape))
 model.add(MaxPooling2D((2, 2)))
 model.add(Dropout(0.25))
-model.add(Conv2D(64, (3, 3), activation="relu"))
+model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
-model.add(Conv2D(128, (3, 3), activation="relu"))
+model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(Dropout(0.4))
 model.add(Flatten())
-model.add(Dense(128, activation="relu"))
+model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.3))
-model.add(Dense(num_classes, activation="softmax"))
+model.add(Dense(num_classes, activation='softmax'))
 
-model.compile(
-    loss=keras.losses.categorical_crossentropy,
-    optimizer=keras.optimizers.Adam(),
-    metrics=["accuracy"],
-)
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adam(),
+              metrics=['accuracy'])
 
 # start an Azure ML run
 run = Run.get_context()
@@ -85,48 +76,45 @@ class LogRunMetrics(Callback):
     # callback at the end of every epoch
     def on_epoch_end(self, epoch, log):
         # log a value repeated which creates a list
-        run.log("Loss", log["loss"])
-        run.log("Accuracy", log["accuracy"])
+        run.log('Loss', log['loss'])
+        run.log('Accuracy', log['accuracy'])
 
 
-history = model.fit(
-    X_train,
-    y_train,
-    batch_size=batch_size,
-    epochs=epochs,
-    verbose=1,
-    validation_data=(X_val, y_val),
-    callbacks=[LogRunMetrics()],
-)
+history = model.fit(X_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(X_val, y_val),
+                    callbacks=[LogRunMetrics()])
 
 score = model.evaluate(X_test, y_test, verbose=0)
 
 # log a single value
 run.log("Final test loss", score[0])
-print("Test loss:", score[0])
+print('Test loss:', score[0])
 
-run.log("Final test accuracy", score[1])
-print("Test accuracy:", score[1])
+run.log('Final test accuracy', score[1])
+print('Test accuracy:', score[1])
 
 plt.figure(figsize=(6, 3))
-plt.title("Fashion MNIST with Keras ({} epochs)".format(epochs), fontsize=14)
-plt.plot(history.history["accuracy"], "b-", label="Accuracy", lw=4, alpha=0.5)
-plt.plot(history.history["loss"], "r--", label="Loss", lw=4, alpha=0.5)
+plt.title('Fashion MNIST with Keras ({} epochs)'.format(epochs), fontsize=14)
+plt.plot(history.history['accuracy'], 'b-', label='Accuracy', lw=4, alpha=0.5)
+plt.plot(history.history['loss'], 'r--', label='Loss', lw=4, alpha=0.5)
 plt.legend(fontsize=12)
 plt.grid(True)
 
 # log an image
-run.log_image("Loss v.s. Accuracy", plot=plt)
+run.log_image('Loss v.s. Accuracy', plot=plt)
 
 # create a ./outputs/model folder in the compute target
 # files saved in the "./outputs" folder are automatically uploaded into run history
-os.makedirs("./outputs/model", exist_ok=True)
+os.makedirs('./outputs/model', exist_ok=True)
 
 # serialize NN architecture to JSON
 model_json = model.to_json()
 # save model JSON
-with open("./outputs/model/model.json", "w") as f:
+with open('./outputs/model/model.json', 'w') as f:
     f.write(model_json)
 # save model weights
-model.save_weights("./outputs/model/model.h5")
+model.save_weights('./outputs/model/model.h5')
 print("model saved in ./outputs/model folder")
