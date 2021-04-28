a421ad8362587673db96d4c1a23f39c1731282c9
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-04-28 12:51:41 -0700

    Update setup.sh

--- cli/setup.sh
+++ cli/setup.sh
@@ -2,7 +2,7 @@
 ## Please reach out to the Azure ML docs & samples team before before editing for the first time.
 
 # <az_ml_install>
-#az extension add -n ml
+az extension add -n ml
 # </az_ml_install>
 
 # <az_group_create>
