bb1dfe4814a2c66672f62635fe49760ad6b7d2cb
Gopal K. Vashishtha <gopalv@microsoft.com>
2021-04-29 16:09:03 -0400

    local testing works

--- cli/endpoints/online/custom-container/tfserving.dockerfile
+++ cli/endpoints/online/custom-container/tfserving.dockerfile
@@ -1,6 +1,1 @@
 FROM tensorflow/serving
-
-# ENV MODEL_BASE_PATH=/var/azureml-app/azureml-models/tfserving-mounted/1
-# ENV MODEL_NAME=half_plus_two2
-# CMD ["tensorflow_model_server", "--port=5000", "--rest_api_port=5001"]
-# COPY ./half_plus_two/ /models/model
--- cli/how-to-deploy-custom-container.sh
+++ cli/how-to-deploy-custom-container.sh
@@ -1,13 +1,40 @@
 BASE_PATH=endpoints/online/custom-container
+MODEL_BASE_PATH=/var/azureml-app/azureml-models/tfserving-mounted/1
+MODEL_NAME=half_plus_two
+
+# Download and unzip model
 wget https://aka.ms/half_plus_two-model -O $BASE_PATH/half_plus_two.tar.gz
 tar -xvf $BASE_PATH/half_plus_two.tar.gz -C $BASE_PATH
 
-# GET ACR
+# Get name of workspace ACR
 ACR_NAME=$(az ml workspace show -n gopalv-test -g gopalv-arm-centraluseuap --query container_registry | cut -d'/' -f9-)
-ACR_NAME=${test%\"}
-docker build $BASE_PATH -f ./tfserving.dockerfile -t $ACR_NAME.azurecr.io/tf-serving:8501-env-variables-mount
-docker push $ACR_NAME.azurecr.io/tf-serving:8501-env-variables-mount
-az ml endpoint create -f TFServing-endpoint.yaml
+ACR_NAME=${ACR_NAME%\"}
+IMAGE_TAG=${ACR_NAME}.azurecr.io/tf-serving:8501-env-variables-mount
+docker build $BASE_PATH -f $BASE_PATH/tfserving.dockerfile -t $IMAGE_TAG
+docker push $IMAGE_TAG
 
+# Run image locally for testing
+docker run -d -v $PWD/$BASE_PATH:$MODEL_BASE_PATH -p 8501:8501 \
+    -e MODEL_BASE_PATH=$MODEL_BASE_PATH -e MODEL_NAME=$MODEL_NAME $IMAGE_TAG 
+
+# Check liveness
+curl -v http://localhost:8501/v1/models/$MODEL_NAME
+
+# Check scoring
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data @$BASE_PATH/sample_request.json \
+  http://localhost:8501/v1/models/$MODEL_NAME:predict
+
+# Fill in name of ACR in deployment YAML
+sed -i 's/{{acr_name}}/'$ACR_NAME'/' $BASE_PATH/TFServing-endpoint.yml
+sed -i 's/{{model_base_path}}/'$MODEL_BASE_PATH'/' $BASE_PATH/TFServing-endpoint.yml
+sed -i 's/{{model_name}}/'$MODEL_NAME'/g' $BASE_PATH/TFServing-endpoint.yml
+
+az ml endpoint create -f $BASE_PATH/TFServing-endpoint.yml
+
+# Test remotely
+
+# Remove local model file
 rm $BASE_PATH/half_plus_two.tar.gz
 rm -r $BASE_PATH/half_plus_two
