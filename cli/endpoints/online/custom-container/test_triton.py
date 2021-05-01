addd16f773d602a59b618a369d4debeb765ef42c
Gopal K. Vashishtha <gopalv@microsoft.com>
2021-05-01 16:12:52 -0400

    works until MFE error

---
+++ cli/endpoints/online/custom-container/test_triton.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+import argparse
+import gevent.ssl
+import numpy as np
+import requests
+import io
+
+import tritonclient.http as tritonhttpclient
+
+if (__name__=='__main__'):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--base_url')
+    parser.add_argument('--token')
+    args = parser.parse_args()
+
+    headers = {"Content-Type": "application/octet-stream"}
+    headers['Authorization'] = f'Bearer {args.token}'
+
+    test_sample = requests.get("https://aka.ms/peacock-pic", allow_redirects=True).content
+
+    test=np.array([test_sample], dtype=bytes)
+    test = np.stack(test, axis=0)
+    input = tritonhttpclient.InferInput('img_in_bytes', test.shape, 'BYTES')
+    input.set_data_from_numpy(test)
+    inputs = [input]
+    if args.token:
+        client = tritonhttpclient.InferenceServerClient(
+            args.base_url,
+            ssl=True,
+            ssl_context_factory=gevent.ssl._create_default_https_context)
+    else:
+        client = tritonhttpclient.InferenceServerClient(args.base_url)
+    outputs = [tritonhttpclient.InferRequestedOutput('label')]
+    print(f'liveness check: {client.is_server_live()}')
+    result = client.infer(model_name='ensemble', inputs=inputs, request_id='1', outputs=outputs)
+    print(f"scoring check: {result.as_numpy('label')}")
---
+++ cli/how-to-deploy-triton.sh
@@ -0,0 +1,56 @@
+BASE_PATH=endpoints/online/custom-container
+AML_MODEL_NAME=triton-ensemble-model
+AZUREML_MODEL_DIR=azureml-models/$AML_MODEL_NAME/1
+MODEL_BASE_PATH=/var/azureml-app/$AZUREML_MODEL_DIR
+ENDPOINT_NAME=triton-endpoint
+DEPLOYMENT_NAME=triton
+
+# Download and unzip model
+wget https://aka.ms/triton_ensemble-model -O $BASE_PATH/triton_ensemble.tar.gz
+tar -xvf $BASE_PATH/triton_ensemble.tar.gz -C $BASE_PATH
+
+# Get name of workspace ACR, build image
+WORKSPACE=$(az config get --query "defaults[?name == 'workspace'].value" -o tsv)
+ACR_NAME=$(az ml workspace show -n $WORKSPACE --query container_registry -o tsv | cut -d'/' -f9-)
+az acr login -n $ACR_NAME
+IMAGE_TAG=${ACR_NAME}.azurecr.io/triton:8000
+az acr build $BASE_PATH -f $BASE_PATH/triton.dockerfile -t $IMAGE_TAG -r $ACR_NAME
+
+# Run image locally for testing
+docker run -d -v $PWD/$BASE_PATH/triton:$MODEL_BASE_PATH/triton -p 8000:8000 \
+    -e MODEL_BASE_PATH=$MODEL_BASE_PATH -e AZUREML_MODEL_DIR=$AZUREML_MODEL_DIR \
+    $IMAGE_TAG
+    
+sleep 10
+
+# Check scoring and liveness locally
+$BASE_PATH/test_triton.py --base_url=localhost:8000
+
+# Fill in placeholders in deployment YAML
+cp $BASE_PATH/base-triton-endpoint.yml $BASE_PATH/$ENDPOINT_NAME.yml
+sed -i 's/{{acr_name}}/'$ACR_NAME'/' $BASE_PATH/$ENDPOINT_NAME.yml
+sed -i 's|{{model_base_path}}|'$MODEL_BASE_PATH'|' $BASE_PATH/$ENDPOINT_NAME.yml
+sed -i 's/{{model_name}}/'$MODEL_NAME'/g' $BASE_PATH/$ENDPOINT_NAME.yml
+sed -i 's/{{aml_model_name}}/'$AML_MODEL_NAME'/g' $BASE_PATH/$ENDPOINT_NAME.yml
+sed -i 's|{{azureml_model_dir}}|'$AZUREML_MODEL_DIR'|g' $BASE_PATH/$ENDPOINT_NAME.yml
+
+# Create endpoint, failing gracefully if there's an issue
+az ml endpoint create -f $BASE_PATH/$ENDPOINT_NAME.yml -n $ENDPOINT_NAME
+STATE=$(az ml endpoint show -n $ENDPOINT_NAME --query deployments[0].provisioning_state -o tsv)
+if [[ $STATE != "Succeeded" ]]
+then
+  az ml endpoint log -n $ENDPOINT_NAME --deployment $DEPLOYMENT_NAME
+  az ml endpoint log -n $ENDPOINT_NAME --deployment $DEPLOYMENT_NAME --container storage-initializer
+  az ml endpoint delete -n $ENDPOINT_NAME -y
+  exit 1
+fi
+
+# Test remotely
+KEY=$(az ml endpoint list-keys -n $ENDPOINT_NAME --query accessToken -o tsv)
+$BASE_PATH/test_triton.py --url=localhost:8000 --token=$KEY
+
+# Clean up
+rm $BASE_PATH/triton_ensemble.tar.gz
+rm -r $BASE_PATH/triton_ensemble
+az ml endpoint delete -n $ENDPOINT_NAME -y
+az ml model delete -n $MODEL_NAME -y
