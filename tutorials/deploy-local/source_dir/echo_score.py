6f93cde15e3b5bdf4fc5e046cf7f3ee894f58947
HiteshTetarwal <imhitesh007@gmail.com>
2021-05-04 12:57:35 +0530

    python readme run

--- tutorials/deploy-local/source_dir/echo_score.py
+++ tutorials/deploy-local/source_dir/echo_score.py
@@ -1,9 +1,11 @@
 import json
 
+
 def init():
-    print('This is init')
+    print("This is init")
+
 
 def run(data):
     test = json.loads(data)
-    print(f'received data {test}')
-    return(f'test is {test}')
+    print(f"received data {test}")
+    return f"test is {test}"
--- tutorials/deploy-local/source_dir/score.py
+++ tutorials/deploy-local/source_dir/score.py
@@ -5,24 +5,32 @@ import onnxruntime
 from nltk import word_tokenize
 import nltk
 
+
 def init():
-    nltk.download('punkt')
+    nltk.download("punkt")
     global sess
-    sess = onnxruntime.InferenceSession(os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.onnx'))
+    sess = onnxruntime.InferenceSession(
+        os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model.onnx")
+    )
+
 
 def run(request):
     print(request)
     text = json.loads(request)
-    qw, qc = preprocess(text['query'])
-    cw, cc = preprocess(text['context'])
+    qw, qc = preprocess(text["query"])
+    cw, cc = preprocess(text["context"])
 
     # Run inference
-    test = sess.run(None, {'query_word': qw, 'query_char': qc, 'context_word': cw, 'context_char': cc})
+    test = sess.run(
+        None,
+        {"query_word": qw, "query_char": qc, "context_word": cw, "context_char": cc},
+    )
     start = np.asscalar(test[0])
     end = np.asscalar(test[1])
-    ans = [w for w in cw[start:end+1].reshape(-1)]
+    ans = [w for w in cw[start : end + 1].reshape(-1)]
     print(ans)
     return ans
+
 
 def preprocess(word):
     tokens = word_tokenize(word)
@@ -32,6 +40,6 @@ def preprocess(word):
 
     # split words into chars, in numpy array with shape of (seq, 1, 1, 16)
     chars = [[c for c in t][:16] for t in tokens]
-    chars = [cs+['']*(16-len(cs)) for cs in chars]
+    chars = [cs + [""] * (16 - len(cs)) for cs in chars]
     chars = np.asarray(chars).reshape(-1, 1, 1, 16)
     return words, chars
