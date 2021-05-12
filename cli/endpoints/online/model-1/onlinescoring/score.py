218d0e0f6e28b0ed5d6ba88cd60a81aaebad9c15
rsethur <rsethur@gmail.com>
2021-05-12 12:40:43 -0700

    update the min max count and score.py  comments

--- cli/endpoints/online/model-1/onlinescoring/score.py
+++ cli/endpoints/online/model-1/onlinescoring/score.py
@@ -7,10 +7,13 @@ from sklearn.externals import joblib
 
 
 def init():
+    """
+    This function is called when the container is initialized/started, typically after create/update of the deployment.
+    You can write the logic here to perform init operations like caching the model in memory
+    """
     global model
     # AZUREML_MODEL_DIR is an environment variable created during deployment.
     # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
-    # For multiple models, it points to the folder containing all deployed models (./azureml-models)
     model_path = os.path.join(
         os.getenv("AZUREML_MODEL_DIR"), "sklearn_regression_model.pkl"
     )
@@ -19,8 +22,12 @@ def init():
     logging.info("Init complete")
 
 
-# note you can pass in multiple rows for scoring
 def run(raw_data):
+    """
+    This function is called for every invocation of the endpoint to perform the actual scoring/prediction. 
+    In the example we extract the data from the json input and call the scikit-learn model's predict() 
+    method and return the result back
+    """
     logging.info("Request received")
     data = json.loads(raw_data)["data"]
     data = numpy.array(data)
