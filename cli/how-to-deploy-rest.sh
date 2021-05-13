2932d443a4a16e962fd25982d00caad8a3c4a4e3
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-05-12 18:26:13 -0700

    minor edit to rest scripts

--- cli/how-to-deploy-rest.sh
+++ cli/how-to-deploy-rest.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-set -e
-
 ## IMPORTANT: this file and accompanying assets are the source for snippets in https://docs.microsoft.com/azure/machine-learning! 
 ## Please reach out to the Azure ML docs & samples team before before editing for the first time.
 
