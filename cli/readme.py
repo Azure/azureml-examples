d3ad2776e42603366198b096a481893c15c59eb6
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-05-21 15:23:03 -0700

    add back storage key

--- cli/readme.py
+++ cli/readme.py
@@ -373,7 +373,9 @@ jobs:
       run: sudo apt-get upgrade -y && sudo apt-get install uuid-runtime jq -y
     - name: test doc script
       run: set -e; bash -x {doc}.sh
-      working-directory: cli\n"""
+      working-directory: cli
+      env:
+        AZURE_STORAGE_KEY: ${{ secrets.AzureStorageKey }}\n"""
 
     # write workflow
     with open(f"../.github/workflows/cli-docs-{hyphenated}.yml", "w") as f:
