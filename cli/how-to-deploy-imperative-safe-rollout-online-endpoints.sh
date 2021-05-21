1a0329db59e2349595966ecd00756d3d4c629e95
rsethur <rsethur@gmail.com>
2021-05-20 19:19:22 -0700

    update

--- cli/how-to-deploy-imperative-safe-rollout-online-endpoints.sh
+++ cli/how-to-deploy-imperative-safe-rollout-online-endpoints.sh
@@ -1,5 +1,5 @@
 ## IMPORTANT: this file and accompanying assets are the source for snippets in https://docs.microsoft.com/azure/machine-learning! 
-##  Please reach out to the Azure ML docs & samples team before before editing for the first time.
+## Please reach out to the Azure ML docs & samples team before before editing for the first time.
 set -e
 
 # <set_endpoint_name>
