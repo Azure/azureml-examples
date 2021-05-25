0a09bc029b574a2f6a375515a7a801b82590c98f
Sethu Raman <rsethur@users.noreply.github.com>
2021-05-24 21:20:38 -0700

    Update how-to-deploy-rest.sh

--- cli/how-to-deploy-rest.sh
+++ cli/how-to-deploy-rest.sh
@@ -41,11 +41,11 @@ wait_for_completion () {
     fi
 }
 
+
+# <get_storage_details>
 # Get values for storage account
 response=$(curl --location --request GET "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/datastores?api-version=$API_VERSION&isDefault=true" \
 --header "Authorization: Bearer $TOKEN")
-
-# <get_storage_details>
 AZUREML_DEFAULT_DATASTORE=$(echo $response | jq -r '.value[0].name')
 AZUREML_DEFAULT_CONTAINER=$(echo $response | jq -r '.value[0].properties.contents.containerName')
 export AZURE_STORAGE_ACCOUNT=$(echo $response | jq -r '.value[0].properties.contents.accountName')
