3bfc4668e2461379789e9520f0d07c0d5c889768
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-06-02 22:59:18 +0000

    readme edit

--- cli/readme.py
+++ cli/readme.py
@@ -20,6 +20,7 @@ EXCLUDED_DOCS = ["setup", "cleanup"]
 def main(args):
     # get list of jobs
     jobs = sorted(glob.glob("jobs/**/*job*.yml", recursive=True))
+    jobs += sorted(glob.glob("jobs/*.yml", recursive=False))
     jobs = [
         job.replace(".yml", "")
         for job in jobs
