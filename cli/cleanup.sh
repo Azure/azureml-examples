3f89c1d50729a1ab65638f24a76d0ee8a469acf7
Joe Zhou <jiazho@microsoft.com>
2021-06-14 10:32:34 -0700

    updated cleanup script

--- cli/cleanup.sh
+++ cli/cleanup.sh
@@ -4,3 +4,11 @@ for val in $ENDPOINT_LIST; do
 	echo deleting $val
     `az ml endpoint delete -n "$val" --yes --no-wait`
 done
+STORAGE_ACCOUNT_LIST=$(az storage account list --query "[*].[name]" -o tsv)
+echo $STORAGE_ACCOUNT_LIST
+for val in $STORAGE_ACCOUNT_LIST; do
+    if [[ $val == *"oepstorage"* ]]; then
+        echo deleting $val
+        `az storage account delete -n "$val" --yes`
+    fi
+done
