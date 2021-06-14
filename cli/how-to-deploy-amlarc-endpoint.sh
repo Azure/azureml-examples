2384862c4f72425a5dc5238b72473b80483d0209
Jeremy Kramer <jkramerdesigns@hotmail.com>
2021-06-14 13:20:36 -0400

    add amlarc examples (#564)
    * add amlarc examples
    
    * reformat yamls
    
    * simplify compute string
    
    * add sh test files
    
    * run black
    
    * move files out of managed
    
    * update readme
    
    Co-authored-by: Jeremy Kramer <jekrame@microsoft.com>

---
+++ cli/how-to-deploy-amlarc-endpoint.sh
@@ -0,0 +1,60 @@
+## IMPORTANT: this file and accompanying assets are the source for snippets in https://docs.microsoft.com/azure/machine-learning! 
+## Please reach out to the Azure ML docs & samples team before before editing for the first time.
+
+set -e
+
+# <set_endpoint_name>
+export ENDPOINT_NAME="<YOUR_ENDPOINT_NAME>"
+# </set_endpoint_name>
+
+export COMPUTE_ID = "<FULLY_QUALIFIED_COMPUTE_ID>"
+export ENDPOINT_NAME=endpt-`echo $RANDOM`
+
+$file = "endpoints/amlarc/managed/simple-flow/1-create-endpoint-with-blue.yml"
+$compute_string = "{{fullyQualifiedComputeId}}"
+sed -i.bak 's|${compute_string}|${COMPUTE_ID}|g' $file
+
+# <deploy>
+az ml endpoint create -n $ENDPOINT_NAME -f $file
+# </deploy>
+
+# <get_status>
+az ml endpoint show -n $ENDPOINT_NAME
+# </get_status>
+
+#   check if create was successful
+endpoint_status=`az ml endpoint show --name $ENDPOINT_NAME --query "provisioning_state" -o tsv`
+echo $endpoint_status
+if [[ $endpoint_status == "Succeeded" ]]
+then
+  echo "Endpoint created successfully"
+else
+  echo "Endpoint creation failed"
+  exit 1
+fi
+
+deploy_status=`az ml endpoint show --name $ENDPOINT_NAME --query "deployments[?name=='blue'].provisioning_state" -o tsv`
+echo $deploy_status
+if [[ $deploy_status == "Succeeded" ]]
+then
+  echo "Deployment completed successfully"
+else
+  echo "Deployment failed"
+  exit 1
+fi
+
+# <test_endpoint>
+az ml endpoint invoke -n $ENDPOINT_NAME --request-file endpoints/online/model-1/sample-request.json
+# </test_endpoint>
+
+# <get_logs>
+az ml endpoint get-logs -n $ENDPOINT_NAME --deployment blue
+# </get_logs>
+
+# <get_scoring_uri>
+az ml endpoint show -n $ENDPOINT_NAME --query "scoring_uri"
+# </get_scoring_uri>
+
+# <delete_endpoint>
+az ml endpoint delete -n $ENDPOINT_NAME --yes --no-wait
+# </delete_endpoint>
