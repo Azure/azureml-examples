1e0a2c058d811ad80e90e556644ac96ac29a7ad0
Gopal K. Vashishtha <gopalv@microsoft.com>
2021-06-18 16:34:46 -0700

    adding r sample

---
+++ cli/endpoints/online/custom-container/R/scripts/plumber.R
@@ -0,0 +1,21 @@
+# plumber.R
+
+#* Liveness check
+#* @get /live
+function() {
+  "alive"
+}
+
+#* Readiness check
+#* @get /ready
+function() {
+  "ready"
+}
+
+#* Return the sum of two numbers
+#* @param a The first number to add
+#* @param b The second number to add
+#* @post /sum
+function(a, b) {
+  as.numeric(a) + as.numeric(b)
+}
