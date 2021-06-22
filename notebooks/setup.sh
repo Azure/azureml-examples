3a43850ca8b9dfb5add093974b07e79b34dc076d
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-06-21 17:46:02 -0700

    ?

--- notebooks/setup.sh
+++ notebooks/setup.sh
@@ -9,6 +9,6 @@ RG=$(az group show --query name -o tsv)
 WS=$(az ml workspace show --query name -o tsv)
 LOC=$(az ml workspace show --query location -o tsv)
 
-export AZUREML_MLFLOW_URI="azureml://$LOC.experiments.azureml.net/mlflow/v1.0/subscriptions/$ID/resourceGroups/$RG/providers/Microsoft.MachineLearningServices/workspaces/$WS"
+export AZUREML_MLFLOW_URI="azureml://$LOC.experiments.azureml.net/mlflow/v1.0/subscriptions/$ID/resourceGroups/$RG/providers/Microsoft.MachineLearningServices/workspaces/$WS?"
 echo $AZUREML_MLFLOW_URI
 
