e2d8b900e4e2261f09ad0f585a87d28ca322d90c
Jeremy Kramer <jekrame@microsoft.com>
2021-07-13 11:09:30 -0400

    black

--- cli/endpoints/amlarc/model-1/onlinescoring/score.py
+++ cli/endpoints/amlarc/model-1/onlinescoring/score.py
@@ -8,9 +8,7 @@ def init():
     # AZUREML_MODEL_DIR is an environment variable created during deployment.
     # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
     # For multiple models, it points to the folder containing all deployed models (./azureml-models)
-    model_path = os.path.join(
-        os.getenv("AZUREML_MODEL_DIR"), "helloworld.txt"
-    )
+    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "helloworld.txt")
     # deserialize the model file back into a sklearn model
     with open(model_path) as f:
         model = f.readline()
