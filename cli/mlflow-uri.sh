4aa56b89b870261090ac85526556ff5bcd848022
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-08-12 09:23:55 -0700

    update mlflow-uri.sh to use new workspace field (#667)

--- cli/mlflow-uri.sh
+++ cli/mlflow-uri.sh
@@ -2,10 +2,6 @@
 ## Please reach out to the Azure ML docs & samples team before before editing for the first time.
 
 # <set_mlflow_uri>
-location=$(az ml workspace show --query location -o tsv)
-subscription=$(az account show --query id -o tsv)
-group=$(az group show --query name -o tsv)
-workspace=$(az ml workspace show --query name -o tsv)
-
-export AZUREML_MLFLOW_URI="azureml://$location.experiments.azureml.net/mlflow/v1.0/subscriptions/$subscription/resourceGroups/$group/providers/Microsoft.MachineLearningServices/workspaces/$workspace?"
+AZUREML_MLFLOW_URI=$(az ml workspace show --query mlflow_tracking_uri -o tsv)
+export AZUREML_MLFLOW_URI
 # </set_mlflow_uri>
