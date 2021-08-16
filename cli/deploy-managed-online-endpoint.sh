9ccd441bb205f262c6e21358cd7b8e9ef564e77c
rsethur <rsethur@gmail.com>
2021-08-16 13:13:22 -0700

    pup refresh

--- cli/deploy-managed-online-endpoint.sh
+++ cli/deploy-managed-online-endpoint.sh
@@ -9,16 +9,20 @@ export ENDPOINT_NAME="<YOUR_ENDPOINT_NAME>"
 
 export ENDPOINT_NAME=endpt-`echo $RANDOM`
 
-# <deploy>
-az ml endpoint create -n $ENDPOINT_NAME -f endpoints/online/managed/simple-flow/1-create-endpoint-with-blue.yml
-# </deploy>
+# <create_endpoint>
+az ml online-endpoint create -n $ENDPOINT_NAME -f endpoints/online/managed/saferollout/endpoint.yml
+# </create_endpoint>
+
+# <create_deployment>
+az ml online-deployment create -n $ENDPOINT_NAME -f endpoints/online/managed/saferollout/blue-deployment.yml
+# </create_endpoint>
 
 # <get_status>
-az ml endpoint show -n $ENDPOINT_NAME
+az ml online-endpoint show -n $ENDPOINT_NAME
 # </get_status>
 
 #   check if create was successful
-endpoint_status=`az ml endpoint show --name $ENDPOINT_NAME --query "provisioning_state" -o tsv`
+endpoint_status=`az ml online-endpoint show --name $ENDPOINT_NAME --query "provisioning_state" -o tsv`
 echo $endpoint_status
 if [[ $endpoint_status == "Succeeded" ]]
 then
@@ -28,7 +32,7 @@ else
   exit 1
 fi
 
-deploy_status=`az ml endpoint show --name $ENDPOINT_NAME --query "deployments[?name=='blue'].provisioning_state" -o tsv`
+deploy_status=`az ml online-deployment show --name blue --endpoint $ENDPOINT_NAME --query "provisioning_state" -o tsv`
 echo $deploy_status
 if [[ $deploy_status == "Succeeded" ]]
 then
@@ -39,17 +43,17 @@ else
 fi
 
 # <test_endpoint>
-az ml endpoint invoke -n $ENDPOINT_NAME --request-file endpoints/online/model-1/sample-request.json
+az ml online-endpoint invoke -n $ENDPOINT_NAME --request-file endpoints/online/model-1/sample-request.json
 # </test_endpoint>
 
 # <get_logs>
-az ml endpoint get-logs -n $ENDPOINT_NAME --deployment blue
+az ml online-endpoint get-logs -n $ENDPOINT_NAME --deployment blue
 # </get_logs>
 
 # <get_scoring_uri>
-az ml endpoint show -n $ENDPOINT_NAME --query "scoring_uri"
+az ml online-endpoint show -n $ENDPOINT_NAME --query "scoring_uri"
 # </get_scoring_uri>
 
 # <delete_endpoint>
-az ml endpoint delete -n $ENDPOINT_NAME --yes --no-wait
+az ml online-endpoint delete -n $ENDPOINT_NAME --yes --no-wait
 # </delete_endpoint>
