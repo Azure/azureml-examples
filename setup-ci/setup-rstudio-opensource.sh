271858b12c538f5a0d9be2fa8791e70c1c3efc5f
vijetajo <40418529+vijetajo@users.noreply.github.com>
2021-08-25 02:02:13 -0700

    Create setup-rstudio-opensource.sh

---
+++ setup-ci/setup-rstudio-opensource.sh
@@ -0,0 +1,21 @@
+#This script installs latest version of RStudio open source on compute instance
+
+#!/bin/bash
+dpkg --purge rstudio-server # in case open source version is installed.
+
+echo "
+[Unit]
+Description=RStudio Server
+Requires=docker.service
+After=docker.service
+
+[Service]
+Restart=always
+ExecStartPre=-/usr/bin/docker stop rstudio-server
+ExecStartPre=-/usr/bin/docker rm rstudio-server
+ExecStart=/usr/bin/docker run --privileged --rm -p 8787:8787 -e DISABLE_AUTH=true -v /home/azureuser/cloudfiles/code:/home/azureuser/code -e USER=azureuser --name rstudio-server rocker/rstudio:latest 
+ExecStop=/usr/bin/docker stop rstudio-server
+[Install]
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/rstudio-server.service
+
+sudo systemctl start rstudio-server
