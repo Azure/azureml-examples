0aa07c80129be5458e429275ab47f41725d1b1c3
vijetajo <40418529+vijetajo@users.noreply.github.com>
2021-08-25 02:08:26 -0700

    Create setup-container.sh

---
+++ setup-ci/setup-container.sh
@@ -0,0 +1,23 @@
+echo $(date -u) "Starting user setup."
+RUNAS=azureuser
+
+sudo CUR_PATH=`pwd` -u $RUNAS -i <<'EOF'
+
+ENV_FOLDER=~/customenv
+KERNEL_FOLDER=~/.local/share/jupyter/kernels/custom_env
+
+mkdir $ENV_FOLDER
+cp $CUR_PATH/Dockerfile $ENV_FOLDER
+cd $ENV_FOLDER
+echo $(date -u) "Building docker image..."
+docker build --tag my-custom-env .
+echo $(date -u) "Docker image built."
+
+mkdir -p $KERNEL_FOLDER
+cp $CUR_PATH/kernel.json $KERNEL_FOLDER
+echo $(date -u) "Installed kernel."
+
+EOF
+
+systemctl restart jupyter
+echo $(date -u) "Restarted jupyter."
