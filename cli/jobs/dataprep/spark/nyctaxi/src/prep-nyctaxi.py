139661fa05b08c3df7e8fda39de3b42e9b27ed4c
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-09-07 22:02:48 +0000

    format code

--- cli/jobs/dataprep/spark/nyctaxi/src/prep-nyctaxi.py
+++ cli/jobs/dataprep/spark/nyctaxi/src/prep-nyctaxi.py
@@ -26,15 +26,14 @@ print(f"dataset location: {dataset}")
 os.system(f"find {dataset}")
 
 spark = (
-    SparkSession
-    .builder
-    .appName("AML Dataprep")
-    .config("spark.executor.cores",1)
+    SparkSession.builder.appName("AML Dataprep")
+    .config("spark.executor.cores", 1)
     .config("spark.executor.instances", 1)
-    .config("spark.executor.memory","1g")
-    .config("spark.executor.cores",1)
-    .config("spark.executor.instances", 1 )
-    .getOrCreate())
+    .config("spark.executor.memory", "1g")
+    .config("spark.executor.cores", 1)
+    .config("spark.executor.instances", 1)
+    .getOrCreate()
+)
 
 # # Azure storage access info
 # blob_account_name = "azureopendatastorage"
@@ -52,7 +51,7 @@ spark = (
 # SPARK read parquet, note that it won't load any data yet by now
 # df = spark.read.parquet(wasbs_path)
 df = spark.read.parquet(dataset)
-#df.show()
+# df.show()
 
 print(df.head())
 mlflow.log_text(str(df.head()), "df.head")
@@ -85,6 +84,7 @@ query_frags = [
 ]
 query = " AND ".join(query_frags)
 
+
 def clean(df_part, must_haves, query):
     df_part = df_part.filter(query)
 
@@ -105,30 +105,31 @@ def clean(df_part, must_haves, query):
             "pickup_datetime",
             "dropoff_datetime",
         ]:
-            df_part.withColumn(col,df_part[col].cast('timestamp'))
+            df_part.withColumn(col, df_part[col].cast("timestamp"))
             continue
 
         # if column was read as a string, recast as float
         if df_part.select(col).dtypes[0][1] is "string":
-            df_part.na.fill(value=-1,subset=[col])
-            df_part.withColumn(col,df_part[col].cast('double'))
+            df_part.na.fill(value=-1, subset=[col])
+            df_part.withColumn(col, df_part[col].cast("double"))
         else:
             # save some memory by using 32 bit floats
             if "int" in str(df_part[col].dtype):
-                df_part.withColumn(col,df_part[col].cast('int'))
+                df_part.withColumn(col, df_part[col].cast("int"))
             if "double" in str(df_part[col].dtype):
-                df_part.withColumn(col,df_part[col].cast('double'))
-            df_part.na.fill(value=-1,subset=[col])
+                df_part.withColumn(col, df_part[col].cast("double"))
+            df_part.na.fill(value=-1, subset=[col])
 
     return df_part
 
 
 import math
-from math import pi,cos,sin,sqrt
+from math import pi, cos, sin, sqrt
 import numpy as np
 from pyspark.sql.functions import datediff, to_date, lit
 from pyspark.sql import functions as F
 from pyspark.sql.functions import *
+
 
 def haversine_distance(
     pickup_latitude, pickup_longitude, dropoff_latitude, dropoff_longitude
@@ -147,23 +148,34 @@ def haversine_distance(
 
     return float(c) * r
 
+
 def add_features(df):
-    df = df.withColumn("hour",hour(df["pickupDatetime"]).cast('int'))
-    df = df.withColumn("year",year(df["pickupDatetime"]).cast('int'))
-    df = df.withColumn("month",month(df["pickupDatetime"]).cast('int'))
-    df = df.withColumn("day",dayofmonth(df["pickupDatetime"]).cast('int'))
-    df = df.withColumn("day_of_week",dayofweek(df["pickupDatetime"]).cast('int'))
+    df = df.withColumn("hour", hour(df["pickupDatetime"]).cast("int"))
+    df = df.withColumn("year", year(df["pickupDatetime"]).cast("int"))
+    df = df.withColumn("month", month(df["pickupDatetime"]).cast("int"))
+    df = df.withColumn("day", dayofmonth(df["pickupDatetime"]).cast("int"))
+    df = df.withColumn("day_of_week", dayofweek(df["pickupDatetime"]).cast("int"))
 
-    df = df.withColumn("diff",datediff(df["dropoffDatetime"], df["pickupDatetime"]).cast('int'))
+    df = df.withColumn(
+        "diff", datediff(df["dropoffDatetime"], df["pickupDatetime"]).cast("int")
+    )
 
-    df = df.withColumn("startLatr",(F.floor(df["startLat"] / (0.01))* 0.01).cast('double'))
-    df = df.withColumn("startLonr",(F.floor(df["startLon"] / (0.01))* 0.01).cast('double'))
-    df = df.withColumn("endLatr",(F.floor(df["endLat"] / (0.01))* 0.01).cast('double'))
-    df = df.withColumn("endLonr",(F.floor(df["endLon"] / (0.01))* 0.01).cast('double'))
+    df = df.withColumn(
+        "startLatr", (F.floor(df["startLat"] / (0.01)) * 0.01).cast("double")
+    )
+    df = df.withColumn(
+        "startLonr", (F.floor(df["startLon"] / (0.01)) * 0.01).cast("double")
+    )
+    df = df.withColumn(
+        "endLatr", (F.floor(df["endLat"] / (0.01)) * 0.01).cast("double")
+    )
+    df = df.withColumn(
+        "endLonr", (F.floor(df["endLon"] / (0.01)) * 0.01).cast("double")
+    )
 
     # df = df.drop('pickup_datetime', axis=1)
     # df = df.drop('dropoff_datetime', axis=1)
-    
+
     import numpy
 
     # df.withColumn("h_distance",haversine_distance(
@@ -173,12 +185,15 @@ def add_features(df):
     #     df.select("endLon"),
     # ).cast('double'))
 
-    df = df.withColumn("is_weekend",(df["day_of_week"] > 5).cast('int'))
+    df = df.withColumn("is_weekend", (df["day_of_week"] > 5).cast("int"))
     return df
 
 
-
-df = df.withColumnRenamed("tpepPickupDateTime","pickupDatetime").withColumnRenamed("tpepDropoffDateTime","dropoffDatetime").withColumnRenamed("rateCodeId","rateCode")
+df = (
+    df.withColumnRenamed("tpepPickupDateTime", "pickupDatetime")
+    .withColumnRenamed("tpepDropoffDateTime", "dropoffDatetime")
+    .withColumnRenamed("rateCodeId", "rateCode")
+)
 taxi_df = clean(df, must_haves, query)
 taxi_df = add_features(taxi_df)
 
@@ -194,6 +209,7 @@ mlflow.log_text(str(taxi_df.head()), "taxi_df.head")
 def list_output():
     return os.listdir(output_path)
 
+
 elapsed_time = time.time() - start_time
 mlflow.log_metric("time_saving_seconds", elapsed_time)
 
@@ -208,7 +224,4 @@ print("done")
 
 # make sure that the output_path exists on all nodes of the cluster.
 # See above for how to create it on all cluster nodes.
-taxi_df.coalesce(1).write\
-.option("header", "true")\
-.mode("append")\
-.parquet(output_path)
+taxi_df.coalesce(1).write.option("header", "true").mode("append").parquet(output_path)
