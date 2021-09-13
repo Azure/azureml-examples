79a38d29ef9975a0ac63f50fcb9013701e8bf694
max <public.max@outlook.com>
2021-09-13 09:29:46 -0700

    remove set x

--- cli/deploy-managed-online-endpoint-access-resource-uai.sh
+++ cli/deploy-managed-online-endpoint-access-resource-uai.sh
@@ -1,7 +1,6 @@
 ## IMPORTANT: this file and accompanying assets are the source for snippets in https://docs.microsoft.com/azure/machine-learning! 
 ## Please reach out to the Azure ML docs & samples team before before editing for the first time.
 set -e
-set -x
 # <set_variables>
 export WORKSPACE="<WORKSPACE_NAME>"
 export LOCATION="<WORKSPACE_LOCATION>"
