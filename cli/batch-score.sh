92cb57a50fc2eca8150d374515c7f822f57f8179
Tracy Chen <47156502+tracychms@users.noreply.github.com>
2021-09-14 11:21:42 -0700

    update batch sample

--- cli/batch-score.sh
+++ cli/batch-score.sh
@@ -14,7 +14,7 @@ az ml batch-endpoint create --name $ENDPOINT_NAME
 # </create_batch_endpoint>
 
 # <create_batch_deployment_set_default>
-az ml batch-deployment create --endpoint-name $ENDPOINT_NAME --file endpoints/batch/add-mlflow-deployment.yml --set-default
+az ml batch-deployment create --name mlflowdp --endpoint-name $ENDPOINT_NAME --file endpoints/batch/add-mlflow-deployment.yml --set-default
 # </create_batch_deployment_set_default>
 
 # <check_batch_endpooint_detail>
@@ -26,7 +26,7 @@ az ml batch-deployment show --name mlflowdp --endpoint-name $ENDPOINT_NAME
 # </check_batch_deployment_detail>
 
 # <start_batch_scoring_job>
-JOB_NAME=$(az ml batch-endpoint invoke --name $ENDPOINT_NAME --input-path https://pipelinedata.blob.core.windows.net/sampledata/nytaxi/taxi-tip-data.csv --query name -o tsv)
+JOB_NAME=$(az ml batch-endpoint invoke --name $ENDPOINT_NAME --input-path file:https://pipelinedata.blob.core.windows.net/sampledata/nytaxi/taxi-tip-data.csv --query name -o tsv)
 # </start_batch_scoring_job>
 
 # <show_job_in_studio>
@@ -54,7 +54,7 @@ fi
 # </check_job_status>
 
 # <start_batch_scoring_job_configure_output>
-JOB_NAME=$(az ml batch-endpoint invoke --name $ENDPOINT_NAME --input-path https://pipelinedata.blob.core.windows.net/sampledata/nytaxi/taxi-tip-data.csv --output-path azureml://datastores/workspaceblobstore/myoutput --set output_file_name=mypredictions.csv --query name -o tsv)
+JOB_NAME=$(az ml batch-endpoint invoke --name $ENDPOINT_NAME --input-path file:https://pipelinedata.blob.core.windows.net/sampledata/nytaxi/taxi-tip-data.csv --output-path folder:azureml://datastores/workspaceblobstore/myoutput --set output_file_name=mypredictions.csv --query name -o tsv)
 # </start_batch_scoring_job_configure_output>
 
 # <stream_job_logs_to_console>
@@ -78,11 +78,11 @@ fi
 # </check_job_status>
 
 # <create_new_deployment_not_default>
-az ml batch-deployment create --endpoint-name $ENDPOINT_NAME --file endpoints/batch/add-nonmlflow-deployment.yml
+az ml batch-deployment create --name nonmlflowdp --endpoint-name $ENDPOINT_NAME --file endpoints/batch/add-nonmlflow-deployment.yml
 # </create_new_deploymen_not_default>
 
 # <test_new_deployment>
-JOB_NAME=$(az ml batch-endpoint invoke --name $ENDPOINT_NAME --deployment-name nonmlflowdp --input-path https://pipelinedata.blob.core.windows.net/sampledata/mnist --query name -o tsv)
+JOB_NAME=$(az ml batch-endpoint invoke --name $ENDPOINT_NAME --deployment-name nonmlflowdp --input-path file:https://pipelinedata.blob.core.windows.net/sampledata/mnist --query name -o tsv)
 
 # <show_job_in_studio>
 az ml job show -n $JOB_NAME --web
@@ -114,7 +114,7 @@ az ml batch-endpoint update --name $ENDPOINT_NAME --defaults deployment_name:non
 # </update_default_deployment>
 
 # <test_new_default_deployment_with_new_settings>
-JOB_NAME=$(az ml batch-endpoint invoke --name $ENDPOINT_NAME --input-path https://pipelinedata.blob.core.windows.net/sampledata/mnist --mini-batch-size 10 --instance-count 2 --set max_concurrency_per_instance=5 --query name -o tsv)
+JOB_NAME=$(az ml batch-endpoint invoke --name $ENDPOINT_NAME --input-path file:https://pipelinedata.blob.core.windows.net/sampledata/mnist --mini-batch-size 10 --instance-count 2 --set max_concurrency_per_instance=5 --query name -o tsv)
 # </test_new_default_deployment_with_new_settings>
 
 # <stream_job_logs_to_console>
