10a537d8e7f7176949c1be43a76aca5757a8b0d2
Tracy Chen <47156502+tracychms@users.noreply.github.com>
2021-09-14 12:38:55 -0700

    add batch score rest sample

---
+++ cli/batch-score-rest.sh
@@ -0,0 +1,215 @@
+## IMPORTANT: this file and accompanying assets are the source for snippets in https://docs.microsoft.com/azure/machine-learning! 
+## Please reach out to the Azure ML docs & samples team before before editing for the first time.
+
+# <create_variables>
+SUBSCRIPTION_ID=$(az account show --query id | tr -d '\r"')
+LOCATION=$(az group show --query location | tr -d '\r"')
+RESOURCE_GROUP=$(az group show --query name | tr -d '\r"')
+
+WORKSPACE=$(az configure -l | jq -r '.[] | select(.name=="workspace") | .value')
+API_VERSION="2021-10-01"
+TOKEN=$(az account get-access-token --resource https://ml.azure.com --query accessToken -o tsv)
+#</create_variables>
+
+# <set_endpoint_name>
+export ENDPOINT_NAME="<YOUR_ENDPOINT_NAME>"
+# </set_endpoint_name>
+
+export ENDPOINT_NAME=endpt-`echo $RANDOM`
+
+echo "Using:\nSUBSCRIPTION_ID: $SUBSCRIPTION_ID\nLOCATION: $LOCATION\nRESOURCE_GROUP: $RESOURCE_GROUP\nWORKSPACE: $WORKSPACE\nENDPOINT_NAME: $ENDPOINT_NAME"
+
+# define how to wait  
+wait_for_completion () {
+    operation_id=$1
+    status="unknown"
+
+    while [[ $status != "Succeeded" && $status != "Failed" ]]
+    do
+        echo "Getting operation status from: $operation_id"
+        operation_result=$(curl --location --request GET $operation_id --header "Authorization: Bearer $TOKEN")
+        # TODO error handling here
+        status=$(echo $operation_result | jq -r '.status')
+        echo "Current operation status: $status"
+        sleep 5
+    done
+
+    if [[ $status == "Failed" ]]
+    then
+        error=$(echo $operation_result | jq -r '.error')
+        echo "Error: $error"
+    fi
+}
+
+# <get_storage_details>
+# Get values for storage account
+response=$(curl --location --request GET "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/datastores?api-version=$API_VERSION&isDefault=true" \
+--header "Authorization: Bearer $TOKEN")
+AZUREML_DEFAULT_DATASTORE=$(echo $response | jq -r '.value[0].name')
+AZUREML_DEFAULT_CONTAINER=$(echo $response | jq -r '.value[0].properties.contents.containerName')
+export AZURE_STORAGE_ACCOUNT=$(echo $response | jq -r '.value[0].properties.contents.accountName')
+# </get_storage_details>
+
+# <upload_code>
+az storage blob upload-batch -d $AZUREML_DEFAULT_CONTAINER/score -s endpoints/batch/mnist/code/
+# </upload_code>
+
+# <create_code>
+curl --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/codes/score-mnist/versions/1?api-version=$API_VERSION" \
+--header "Authorization: Bearer $TOKEN" \
+--header "Content-Type: application/json" \
+--data-raw "{
+  \"properties\": {
+    \"description\": \"Score code\",
+    \"datastoreId\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/datastores/$AZUREML_DEFAULT_DATASTORE\",
+    \"path\": \"score\"
+  }
+}"
+# </create_code>
+
+# <upload_model>
+az storage blob upload-batch -d $AZUREML_DEFAULT_CONTAINER/model -s endpoints/batch/model/
+# </upload_model>
+
+# <create_model>
+curl --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/models/mnist/versions/1?api-version=$API_VERSION" \
+--header "Authorization: Bearer $TOKEN" \
+--header "Content-Type: application/json" \
+--data-raw "{
+    \"properties\": {
+        \"datastoreId\":\"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/datastores/workspaceblobstore\",
+        \"path\": \"model/\",
+    }
+}"
+# </create_model>
+
+# <read_condafile>
+CONDA_FILE=$(cat endpoints/batch/mnist/environment/conda.yml)
+# <read_condafile>
+
+# <create_environment>
+ENV_VERSION=$RANDOM
+curl --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/environments/mnist-env/versions/$ENV_VERSION?api-version=$API_VERSION" \
+--header "Authorization: Bearer $TOKEN" \
+--header "Content-Type: application/json" \
+--data-raw "{
+    \"properties\":{
+        \"condaFile\": \"$CONDA_FILE\",
+        \"Docker\": {
+            \"DockerSpecificationType\": \"Image\",
+            \"DockerImageUri\": \"mcr.microsoft.com/azureml/openmpi3.1.2-ubuntu18.04:latest\"
+        }
+    }
+}"
+# </create_environment>
+
+# <create_compute>
+curl --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/computes/cpu-cluster?api-version=$API_VERSION" \
+--header "Authorization: Bearer $TOKEN" \
+--header "Content-Type: application/json" \
+--data-raw "{
+    \"properties\":{
+        \"computeType\": \"AmlCompute\",
+        \"properties\": {
+            \"osType\": \"Linux\",
+            \"vmSize\": \"STANDARD_D2_V2\"
+            \"scaleSettings\": {
+                \"maxNodeCount\": 3,
+                \"minNodeCount\": 0
+            },
+        }
+    \"location\": \"$LOCATION\"
+    }
+}"
+# </create_compute>
+
+#<create_endpoint>
+response=$(curl --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/batchEndpoints/$ENDPOINT_NAME?api-version=$API_VERSION" \
+--header "Content-Type: application/json" \
+--header "Authorization: Bearer $TOKEN" \
+--data-raw "{
+    \"properties\": {
+        \"authMode\": \"aadToken\"
+    },
+    \"location\": \"$LOCATION\"
+}")
+#</create_endpoint>
+
+echo "Endpoint response: $response"
+operation_id=$(echo $response | jq -r '.properties' | jq -r '.properties' | jq -r '.AzureAsyncOperationUri')
+wait_for_completion $operation_id
+
+# <create_deployment>
+response=$(curl --location --request PUT "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/batchEndpoints/$ENDPOINT_NAME/deployments/nonmlflowedp?api-version=$API_VERSION" \
+--header "Content-Type: application/json" \
+--header "Authorization: Bearer $TOKEN" \
+--data-raw "{
+    \"location\": \"$LOCATION\",
+    \"properties\": {        
+        \"model\": {
+            \"referenceType\": \"Id\",
+            \"assetId\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/models/mnist/versions/1\"
+        },
+        \"codeConfiguration\": {
+            \"codeId\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/codes/score-mnist/versions/1\",
+            \"scoringScript\": \"digit_identification.py\"
+        },
+        \"environmentId\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/environments/mnist-env/versions/$ENV_VERSION\",
+        \"compute\": \"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/compute/cpu-cluster\",
+        \"resources\": {
+            \"instanceCount\": 1
+        },
+        \"maxConcurrencyPerInstance\": \"4\",
+        \"retrySettings\": {
+            \"maxRetries\": 3,
+            \"timeout\": \"PT30S\"
+        },
+        \"errorThreshold\": \"10\",
+        \"loggingLevel\": \"info\",
+        \"miniBatchSize\": \"5\",
+    }
+}")
+#</create_deployment>
+
+echo "Endpoint response: $response"
+operation_id=$(echo $response | jq -r '.properties' | jq -r '.properties' | jq -r '.AzureAsyncOperationUri')
+wait_for_completion $operation_id
+
+# <get_endpoint>
+response=$(curl --location --request GET "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/batchEndpoints/$ENDPOINT_NAME?api-version=$API_VERSION" \
+--header "Content-Type: application/json" \
+--header "Authorization: Bearer $TOKEN")
+
+scoringUri=$(echo $response | jq -r '.properties' | jq -r '.scoringUri')
+# </get_endpoint>
+
+# <get_access_token>
+response=$(curl -H "Content-Length: 0" --location --request POST "https://login.microsoftonline.com/$TENENT_ID/oauth2/token" \
+--header "Authorization: Bearer $TOKEN")
+accessToken=$(echo $response | jq -r '.accessToken')
+# </get_access_token>
+
+# <score_endpoint>
+response=$(curl --location --request POST "$SCORING_URI" \
+--header "Authorization: Bearer $TOKEN" \
+--header "Content-Type: application/json" \
+--data-raw "{
+    \"properties\": {
+        \"dataset\": {
+            \"dataInputType\": \"DataUrl\",
+            \"Path\": \"https://pipelinedata.blob.core.windows.net/sampledata/mnist\"
+        }
+    }
+}")
+# </score_endpoint>
+
+# <check_job>
+
+# </check_job>
+
+# delete endpoint
+# <delete_endpoint>
+curl --location --request DELETE "https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE/batchEndpoints/$ENDPOINT_NAME?api-version=$API_VERSION" \
+--header "Content-Type: application/json" \
+--header "Authorization: Bearer $TOKEN" || true
+# </delete_endpoint>
