d67b24fc111157114c8f0b99859678c13e41cdb5
rsethur <rsethur@gmail.com>
2021-09-16 11:10:31 -0700

    cli version

--- cli/deploy-managed-online-endpoint.sh
+++ cli/deploy-managed-online-endpoint.sh
@@ -5,7 +5,7 @@ set -e
 
 #todo remove
 az extension remove -n ml
-az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/ml-0.0.9_october_cand-py3-none-any.whl --yes
+az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/ml-0.0.8_october_cand-py3-none-any.whl --yes
 
 # <set_endpoint_name> 
 export ENDPOINT_NAME="<YOUR_ENDPOINT_NAME>"
