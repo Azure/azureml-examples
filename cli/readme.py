01c3f43ff40c32d012c506b9b824aa688c779453
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-09-17 16:25:59 +0000

    remove cifar from untested jobs

--- cli/readme.py
+++ cli/readme.py
@@ -5,7 +5,7 @@ import glob
 import argparse
 
 # define constants
-EXCLUDED_JOBS = ["cifar"]
+EXCLUDED_JOBS = []
 EXCLUDED_ENDPOINTS = ["conda.yml", "environment.yml", "batch", "online"]
 EXCLUDED_RESOURCES = [
     "workspace",
