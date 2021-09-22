9a159805222ee17d576248a5d53deda867d7f8ff
Tracy Chen <47156502+tracychms@users.noreply.github.com>
2021-09-22 13:30:55 -0700

    minor update

--- cli/batch-score.sh
+++ cli/batch-score.sh
@@ -58,7 +58,7 @@ fi
 # </check_job_status>
 
 # <start_batch_scoring_job_configure_output_settings>
-JOB_NAME=$(az ml batch-endpoint invoke --name $ENDPOINT_NAME --input-path folder:https://pipelinedata.blob.core.windows.net/sampledata/mnist --output-path folder:azureml://datastores/workspaceblobstore/paths/myoutput --set output_file_name=mypredictions.csv --mini-batch-size 20 --instance-count 5 --set max_concurrency_per_instance=4 --query name -o tsv)
+JOB_NAME=$(az ml batch-endpoint invoke --name $ENDPOINT_NAME --input-path folder:https://pipelinedata.blob.core.windows.net/sampledata/mnist --output-path folder:azureml://datastores/workspaceblobstore/paths/myoutput --set output_file_name=mypredictions.csv --mini-batch-size 20 --instance-count 5 --query name -o tsv)
 # </start_batch_scoring_job_configure_output_settings>
 
 # <stream_job_logs_to_console>
@@ -82,7 +82,7 @@ fi
 # </check_job_status>
 
 # <list_all_jobs>
-az ml batch-endpoint list-jobs --name $ENDPOINT_NAME --query [].name
+az ml batch-deployment list-jobs --name nonmlflowdp --endpoint-name $ENDPOINT_NAME --query [].name
 # </list_all_jobs>
 
 # <create_new_deployment_not_default>
