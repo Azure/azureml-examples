ed0563566575db7294f2c8207f017a199455fed0
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-09-22 16:52:43 +0000

    save

---
+++ cli/create-workspace-private-link-endpoint.sh
@@ -0,0 +1,30 @@
+WORKSPACE="mlw-privatelink-dev"
+
+# <create_workspace>
+az ml workspace create --file resources/workspace/privatelink.yml -n $WORKSPACE
+# </create_workspace>
+
+# <export_variables>
+SUBSCRIPTION=$(az account show --query id -o tsv)
+GROUP=$(az ml workspace show --query resource_group -o tsv)
+LOCATION=$(az ml workspace show --query location -o tsv)
+VNET_NAME="myvnet"
+# </set_variables>
+
+# <az_network_create>
+az network vnet create -n $VNET_NAME --subnet-name default
+# </az_network_create>
+
+# <az_network_subnet_update_networkpolicies>
+az network vnet subnet update --name default --vnet-name $VNET_NAME --disable-private-endpoint-network-policies true
+# </az_network_subnet_update_networkpolicies>
+
+# <az_network_ple_create>
+az network private-endpoint create \
+    -n main \
+    --vnet-name $VNET_NAME \
+    --subnet default \
+    --private-connection-resource-id "/subscriptions/$SUBSCRIPTION/resourceGroups/$GROUP/providers/Microsoft.MachineLearningServices/workspaces/$WORKSPACE" \
+    --group-id amlworkspace \
+    --connection-name workspace -l $LOCATION
+# </az_network_ple_create>
