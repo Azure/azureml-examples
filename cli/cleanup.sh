ca2069466f01afed6c80d12f9fdb01131be0b733
Tracy Chen <47156502+tracychms@users.noreply.github.com>
2021-09-29 13:52:41 -0700

    clean up batch endpoints (#817)

--- cli/cleanup.sh
+++ cli/cleanup.sh
@@ -4,6 +4,12 @@ for val in $ENDPOINT_LIST; do
 	echo deleting $val
     `az ml online-endpoint delete -n "$val" --yes --no-wait`
 done
+ENDPOINT_LIST=$(az ml batch-endpoint list --query "[*].[name]" -o tsv)
+echo $ENDPOINT_LIST
+for val in $ENDPOINT_LIST; do
+	echo deleting $val
+    `az ml batch-endpoint delete -n "$val" --yes --no-wait`
+done
 STORAGE_ACCOUNT_LIST=$(az storage account list --query "[*].[name]" -o tsv)
 echo $STORAGE_ACCOUNT_LIST
 for val in $STORAGE_ACCOUNT_LIST; do
