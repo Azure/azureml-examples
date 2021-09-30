f73ad39852728c512930f546ea0c2bda6022fc41
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-09-30 20:31:05 +0000

    save

--- cli/jobs/single-step/basics/src/hello-mlflow.py
+++ cli/jobs/single-step/basics/src/hello-mlflow.py
@@ -3,25 +3,28 @@ import os
 import mlflow
 import argparse
 
-import pandas as pd
+from random import random
 
 # define functions
 def main(args):
-    pass
+    os.system(f"echo 'hello world' > {args.world_file}")
+    mlflow.log_param("hello_factor", args.hello_factor)
+    mlflow.log_metric("hello_measure", random())
+    mlflow.log_artifact("helloworld.txt")
 
 def parse_args():
     # setup arg parser
     parser = argparse.ArgumentParser()
 
     # add arguments
-    parser.add_argument("--iris-csv", type=str)
+    parser.add_argument("--hello-factor", type=str, default="world")
+    parser.add_argument("--world-file", type=str, default="helloworld.txt")
 
     # parse args
     args = parser.parse_args()
 
     # return args
     return args
-
 
 # run script
 if __name__ == "__main__":
