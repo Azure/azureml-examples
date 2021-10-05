6e2e9619044745f6e43a1d66c32462c80dfb0b79
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-10-05 14:51:33 -0700

    save

--- cli/jobs/basics/src/hello-mlflow.py
+++ cli/jobs/basics/src/hello-mlflow.py
@@ -1,37 +1,17 @@
 # imports
 import os
 import mlflow
-import argparse
 
 from random import random
 
 # define functions
-def main(args):
-    os.system(f"echo 'hello world' > {args.world_file}")
-    mlflow.log_param("hello_param", args.hello_param)
+def main():
+    mlflow.log_param("hello_param", "world")
     mlflow.log_metric("hello_metric", random())
+    os.system(f"echo 'hello world' > helloworld.txt")
     mlflow.log_artifact("helloworld.txt")
 
-
-def parse_args():
-    # setup arg parser
-    parser = argparse.ArgumentParser()
-
-    # add arguments
-    parser.add_argument("--hello-param", type=str, default="world")
-    parser.add_argument("--world-file", type=str, default="helloworld.txt")
-
-    # parse args
-    args = parser.parse_args()
-
-    # return args
-    return args
-
-
-# run script
+# run functions
 if __name__ == "__main__":
-    # parse args
-    args = parse_args()
-
     # run main function
-    main(args)
+    main()
