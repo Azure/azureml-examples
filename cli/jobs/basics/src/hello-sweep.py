8cf6a5afbd3bcbe980e9ad288c243194e316f18a
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-10-06 22:53:39 +0000

    save

--- cli/jobs/basics/src/hello-sweep.py
+++ cli/jobs/basics/src/hello-sweep.py
@@ -8,14 +8,14 @@ from random import random
 # define functions
 def main(args):
     # print inputs
-    print(f"inputA: {args.inputA}")
-    print(f"inputB: {args.inputB}")
-    print(f"inputC: {args.inputC}")
+    print(f"A: {args.A}")
+    print(f"B: {args.B}")
+    print(f"C: {args.C}")
 
     # log inputs as parameters
-    mlflow.log_param("inputA", args.inputA)
-    mlflow.log_param("inputB", args.inputB)
-    mlflow.log_param("inputC", args.inputC)
+    mlflow.log_param("A", args.A)
+    mlflow.log_param("B", args.B)
+    mlflow.log_param("C", args.C)
 
     # log a random metric
     mlflow.log_metric("random_metric", random())
@@ -26,9 +26,9 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     # add arguments
-    parser.add_argument("--inputA", type=float, default=0.5)
-    parser.add_argument("--inputB", type=str, default="A")
-    parser.add_argument("--inputC", type=float, default=1.0)
+    parser.add_argument("--A", type=float, default=0.5)
+    parser.add_argument("--B", type=str, default="hello world")
+    parser.add_argument("--C", type=float, default=1.0)
 
     # parse args
     args = parser.parse_args()
