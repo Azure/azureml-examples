5e5d9264be15a157dd6635c2fffc341669c8cb31
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-10-25 17:52:39 +0000

    Revert "run black"
    This reverts commit b649bb564df6eecfe3118d6ab0d74268c6c680f3.

--- cli/jobs/single-step/dask/nyctaxi/src/prep-nyctaxi.py
+++ cli/jobs/single-step/dask/nyctaxi/src/prep-nyctaxi.py
@@ -207,7 +207,7 @@ taxi_df = add_features(taxi_df)
 
 print(c)
 mlflow.log_text(str(c), "dask_cluster2")
-# output_path = "./outputs/nyctaxi_processed.parquet"
+#output_path = "./outputs/nyctaxi_processed.parquet"
 print("save parquet to ", output_path)
 # In this case, we are not using a mounted drive to save the job's output
 # but save to the ./outputs folder, which is on the nodes local drives.
@@ -216,11 +216,11 @@ print("save parquet to ", output_path)
 # on blob storage.
 # For this to work, the target folder needs to be created on each node.
 # Using DASK's Client.run method which executes a given function on each node of the cluster:
-# def create_output():
+#def create_output():
 #    return Path(output_path).mkdir(parents=True, exist_ok=True)
 
 
-# print("create_output", c.run(create_output))
+#print("create_output", c.run(create_output))
 
 
 start_time = time.time()
