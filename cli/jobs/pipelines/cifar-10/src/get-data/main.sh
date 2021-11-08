6bd1bd079c3766e9ab68159e337123cbf977ffb8
lochen <cloga0216@gmail.com>
2021-11-08 16:53:40 +0800

    work around for cifar

--- cli/jobs/pipelines/cifar-10/src/get-data/main.sh
+++ cli/jobs/pipelines/cifar-10/src/get-data/main.sh
@@ -1,3 +1,3 @@
-wget https://azuremlexamples.blob.core.windows.net/datasets/cifar-10-python.tar.gz
+wget https://azuremlexamples.blob.core.windows.net/datasets/cifar-10-python.tar.gz 
 tar -xvzf cifar-10-python.tar.gz -C $1
 rm cifar-10-python.tar.gz
