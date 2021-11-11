e675d2b044432dfab3afe8898350d7550c1ee66a
Harneet Virk <harnvir@microsoft.com>
2021-11-11 11:07:26 -0800

    Pin azure-cli

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -7,5 +7,5 @@ pip install pip==21.1.1
 # Downgrade az cli from outside of the CLI task
 # azure-cli 2.30.0 compatibility issue with azure-cli-ml extension
 echo "Installing pinned version of azure-cli"
-pip install azure-cli<2.30.0
+pip install azure-cli==2.29.2
 echo "Required package installed"
