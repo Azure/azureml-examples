f4d39ec67ebfcaef173a0a13279f9b5d36a7d937
Harneet Virk <harnvir@microsoft.com>
2021-11-16 17:59:41 -0800

    Manually installing .NETCore2.0 runtime

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -9,3 +9,13 @@ pip install pip==21.1.1
 echo "Installing pinned version of azure-cli"
 pip install azure-cli==2.29.2
 echo "Required package installed"
+echo "Add the Microsoft package signing key"
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+echo "Install the .NET Core 2.1 runtime"
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-2.1
+echo ".NET Core 2.1 runtime package installed"
