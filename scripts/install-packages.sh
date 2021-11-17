becb8f8a28ed8e0c69ea15c76f07f17c13264194
Harneet Virk <harnvir@microsoft.com>
2021-11-16 18:18:59 -0800

    Install az cli

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -7,7 +7,8 @@ pip install pip==21.1.1
 # Downgrade az cli from outside of the CLI task
 # azure-cli 2.30.0 compatibility issue with azure-cli-ml extension
 echo "Installing pinned version of azure-cli"
-pip install azure-cli==2.29.2
+# pip install azure-cli==2.29.2
+sudo apt-get install azure-cli=2.29.2-1~bullseye
 echo "Required package installed"
 echo "Add the Microsoft package signing key"
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
