cac32380353ed600de9b7a2940528f2aea28ecc4
Shize Su <shizs@microsoft.com>
2021-12-01 13:04:30 -0800

    added example of AML+ Arc + K8s + Shrike FL API

---
+++ accelerators/federated-learning/fl_arc_k8s/components/count_rows_and_log/run.py
@@ -0,0 +1,6 @@
+"""run.py for demo component"""
+
+from contoso.count_rows_and_log_script import main
+
+if __name__ == "__main__":
+    main()
---
+++ accelerators/federated-learning/fl_arc_k8s/components/preprocessing/run.py
@@ -0,0 +1,34 @@
+"""run.py for mock components"""
+import argparse
+from distutils.dir_util import copy_tree
+
+
+def get_arg_parser(parser=None):
+    """Parse the command line arguments for merge using argparse
+
+    Args:
+        parser (argparse.ArgumentParser or CompliantArgumentParser):
+        an argument parser instance
+
+    Returns:
+        ArgumentParser: the argument parser instance
+
+    Notes:
+        if parser is None, creates a new parser instance
+    """
+    # add arguments that are specific to the component
+    if parser is None:
+        parser = argparse.ArgumentParser(description=__doc__)
+    for arg in ["--input_data", "--message", "--results"]:
+        parser.add_argument(arg)
+    return parser
+
+
+if __name__ == "__main__":
+    # get the arguments
+    parser = get_arg_parser()
+    args = parser.parse_args()
+    args = vars(args)
+
+    print(args["message"])
+    copy_tree(args["input_data"], args["results"])
