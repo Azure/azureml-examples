769c01ece5ff933376c153c9f74fc3546969a74e
Cody <54814569+lostmygithubaccount@users.noreply.github.com>
2021-12-02 15:22:26 -0800

    add comment on file location

--- setup-ci/setup-git.sh
+++ setup-ci/setup-git.sh
@@ -16,7 +16,8 @@ git clone https://$USERNAME:$PAT@$REPO $LOCAL_DIR
 # change into repository
 cd $LOCAL_DIR
 
-# configure global git on the Compute Instance
+# Configure global git on the Compute Instance's local disk.
+# This is not the shared file system. You can `cd ~` to find this file later.
 git config --global user.name "$USERNAME"
 git config --global user.email "$EMAIL_ADDRESS"
 git config --global credential.helper 'store --file ~/.aml-git-credentials'
