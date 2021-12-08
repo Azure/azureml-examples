ab084079e52617f15100b0a301b3600754471a40
Banibrata De <banibrata@outlook.com>
2021-12-08 14:29:20 -0800

    all in one

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -2,6 +2,6 @@
 
 # Start the first process
 cd /workspaces/azureml-examples/notebooks
-jupyter lab --no-browser &
+jupyter lab --no-browser >/var/log/jupyter.log 2>&1 < /var/log/jupyter.log &
 
 /bin/bash
