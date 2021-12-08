c123eec1be84292df439b1bf0edc485fbedfadbb
Banibrata De <banibrata@outlook.com>
2021-12-07 22:58:33 -0800

    docker custom full build

---
+++ .devcontainer/jupyter_notebook_config.py
@@ -0,0 +1,8 @@
+## Whether to allow the user to run the notebook as root.
+c.NotebookApp.allow_root = True
+
+c.NotebookApp.ip = '*'
+
+c.NotebookApp.port = 8888
+
+c.NotebookApp.token = ''
---
+++ .devcontainer/wrapper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Start the first process
+jupyter lab --no-browser Examples.example_dir=/workspaces/azureml-examples/notebooks &
+
+/bin/bash
+
