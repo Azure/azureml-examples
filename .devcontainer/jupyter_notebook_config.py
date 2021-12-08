c08c6456d20812781c207a0abd69c1148f4021f1
Banibrata De <banibrata@outlook.com>
2021-12-08 10:17:01 -0800

    all in one

--- .devcontainer/jupyter_notebook_config.py
+++
@@ -1,7 +0,0 @@
-## Whether to allow the user to run the notebook as root.
-c.NotebookApp.allow_root = True
-c.NotebookApp.ip = '*'
-c.NotebookApp.port = 8888
-c.NotebookApp.token = ''
-c.disable_check_xsrf=True 
-c.allow_origin='\"*\"'
