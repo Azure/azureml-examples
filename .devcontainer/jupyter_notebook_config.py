799b25925ef74a834d68e941a087832302bd9018
Banibrata De <banibrata@outlook.com>
2021-12-08 09:20:22 -0800

    all in one

--- .devcontainer/jupyter_notebook_config.py
+++ .devcontainer/jupyter_notebook_config.py
@@ -1,8 +1,7 @@
 ## Whether to allow the user to run the notebook as root.
 c.NotebookApp.allow_root = True
-
 c.NotebookApp.ip = '*'
-
 c.NotebookApp.port = 8888
-
 c.NotebookApp.token = ''
+c.disable_check_xsrf=True 
+c.allow_origin='\"*\"'
--- .devcontainer/wrapper.sh
+++ .devcontainer/wrapper.sh
@@ -2,6 +2,6 @@
 
 # Start the first process
 cd /workspaces/azureml-examples/notebooks
-jupyter lab --no-browser /workspaces/azureml-examples/notebooks &
+jupyter lab --no-browser &
 
 /bin/bash
