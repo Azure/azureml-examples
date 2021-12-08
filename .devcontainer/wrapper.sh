a0ce01a88f8a8d2d5bb08acc9dd412e0f53c432b
Banibrata De <banibrata@outlook.com>
2021-12-08 08:52:46 -0800

    all in one

--- .devcontainer/wrapper.sh
+++ .devcontainer/wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the first process
-jupyter lab --no-browser Examples.example_dir=/workspaces/azureml-examples/notebooks &
+cd /workspaces/azureml-examples/notebooks
+jupyter lab --no-browser /workspaces/azureml-examples/notebooks &
 
 /bin/bash
-
