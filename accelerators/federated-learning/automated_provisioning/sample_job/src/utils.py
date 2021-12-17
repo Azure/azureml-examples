c668297546e33d6d373401b254130c4fab98429e
thomas portet <XXX@me.com>
2021-12-17 15:21:58 -0800

    black

--- accelerators/federated-learning/automated_provisioning/sample_job/src/utils.py
+++ accelerators/federated-learning/automated_provisioning/sample_job/src/utils.py
@@ -9,11 +9,11 @@ import struct
 # load compressed MNIST gz files and return numpy arrays
 def load_data(filename, label=False):
     with gzip.open(filename) as gz:
-        struct.unpack('I', gz.read(4))
-        n_items = struct.unpack('>I', gz.read(4))
+        struct.unpack("I", gz.read(4))
+        n_items = struct.unpack(">I", gz.read(4))
         if not label:
-            n_rows = struct.unpack('>I', gz.read(4))[0]
-            n_cols = struct.unpack('>I', gz.read(4))[0]
+            n_rows = struct.unpack(">I", gz.read(4))[0]
+            n_cols = struct.unpack(">I", gz.read(4))[0]
             res = np.frombuffer(gz.read(n_items[0] * n_rows * n_cols), dtype=np.uint8)
             res = res.reshape(n_items[0], n_rows * n_cols)
         else:
