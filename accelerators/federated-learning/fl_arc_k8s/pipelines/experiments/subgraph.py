17fca72699ad062448f8281ff0885e2075130f50
Dan Miller <danmill@microsoft.com>
2021-12-17 13:02:58 -0800

    clean up code

--- accelerators/federated-learning/fl_arc_k8s/pipelines/experiments/subgraph.py
+++ accelerators/federated-learning/fl_arc_k8s/pipelines/experiments/subgraph.py
@@ -1,36 +1,17 @@
 """
-The AML subgraph for demo graph
+TODO
 """
-# pylint: disable=no-member
-# NOTE: because it raises 'dict' has no 'outputs' member in dsl.pipeline construction
-import os
-import sys
 
 from azure.ml.component import dsl
 from shrike.pipeline.pipeline_helper import AMLPipelineHelper
 
-# NOTE: if you need to import from pipelines.*
-ACCELERATOR_ROOT_PATH = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "..", "..")
-)
-if ACCELERATOR_ROOT_PATH not in sys.path:
-    print(f"Adding to path: {ACCELERATOR_ROOT_PATH}")
-    sys.path.append(str(ACCELERATOR_ROOT_PATH))
 
-
-class DemoSubgraph(AMLPipelineHelper):
-    """Runnable/reusable pipeline helper class
-
-    This class inherits from AMLPipelineHelper which provides
-    helper functions to create reusable production pipelines.
-    """
-
+class FederatedSubgraph(AMLPipelineHelper):
     def build(self, config):
-        component = self.component_load("AggregateModelWeights")
+        component = self.component_load("aggregatemodelweights")
 
         @dsl.pipeline(
-            name="demo-subgraph",
-            description="The AML pipeline for the demo subgraph",
+            name="federated-subgraph",
             default_datastore=config.compute.compliant_datastore,
         )
         def demosubgraph_pipeline_function(
@@ -43,20 +24,15 @@ class DemoSubgraph(AMLPipelineHelper):
                 input_data_02=input_data_02,
             )
 
-            self.apply_recommended_runsettings(
-                "AggregateModelWeights", component_step_1, gpu=True
-            )
+            self.apply_smart_runsettings(component_step_1, gpu=True)
 
             component_step_2 = component(
                 input_data_01=component_step_1.outputs.results,
                 input_data_02=input_data_03,
             )
 
-            self.apply_recommended_runsettings(
-                "AggregateModelWeights", component_step_2, gpu=True
-            )
+            self.apply_smart_runsettings(component_step_2, gpu=True)
 
-            # return {key: output}
             return {"subgraph_results": component_step_2.outputs.results}
 
         # finally return the function itself to be built by helper code
