6038632287d2b57c7ad8ca33c02aed1bf6029621
Daniel Schneider <daniel.schneider@microsoft.com>
2022-02-02 17:02:52 -0800

    fix failing dask runs

--- cli/jobs/single-step/dask/nyctaxi/src/startDask.py
+++ cli/jobs/single-step/dask/nyctaxi/src/startDask.py
@@ -10,6 +10,12 @@ import mlflow
 
 from notebook.notebookapp import list_running_servers
 
+def write_freeze():
+    # log pip list before doing anything else
+    from pathlib import Path
+    import os
+    Path("./outputs").mkdir(parents=True, exist_ok=True)
+    os.system("pip list > outputs/pip_list.txt")
 
 def flush(proc, proc_log):
     while True:
@@ -24,6 +30,7 @@ def flush(proc, proc_log):
 
 
 if __name__ == "__main__":
+    write_freeze()
     parser = argparse.ArgumentParser()
     parser.add_argument("--jupyter_token", default=uuid.uuid1().hex)
     parser.add_argument("--script")
