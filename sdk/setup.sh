439dd0be2b8a543c211a79e5e3db8f856365f26f
Bala P V <balapv@microsoft.com>
2022-03-08 14:41:51 -0800

    Installing new build

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-pip install azure-ml==0.0.139 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
+#pip install azure-ml==0.0.139 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
+
+pip install https://dpv2builds.blob.core.windows.net/builds/sdk/azure_ml-0.0.58117153-py3-none-any.whl
 
 pip list
