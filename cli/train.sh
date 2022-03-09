e141fa5b2cca21431c96b0269551e039106c0460
Abraham Omorogbe <abrahamomor@gmail.com>
2022-03-09 13:50:46 -0800

    update to MLflow specific

--- cli/train.sh
+++ cli/train.sh
@@ -146,7 +146,7 @@ do
 done
 
 # <sklearn_download_register_model>
-az ml model create -n sklearn-iris-example -p azureml://jobs/$run_id/outputs/artifacts/paths/model
+az ml model create -n sklearn-iris-example -v 1 -p runs:/$run_id/model --type mlflow_model
 # </sklearn_download_register_model>
 rm -r $run_id
 
