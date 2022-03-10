2bae1199e75f898529f080dbe54e360d8de379ad
rsethur <rsethur@gmail.com>
2022-03-10 12:01:31 -0800

    fix the branch name

--- cli/deploy-moe-vnet.sh
+++ cli/deploy-moe-vnet.sh
@@ -30,18 +30,38 @@ export SUBNET_NAME="snet-scoring"
 export ENDPOINT_NAME=endpt-vnet-`echo $RANDOM`
 
 # Get the current branch name of the azureml-examples. Useful in PR scenario. Since the sample code is cloned and executed from a VM, we need to pass the branch name
-echo '!!!!!!!!!!!!!'
-echo GITHUB_HEAD_REF is $GITHUB_HEAD_REF
-echo GITHUB_REF is $GITHUB_REF
-echo GITHUB_REF_NAME is $GITHUB_REF_NAME
-export AA=$(git branch)
-echo $AA
-export BB=$(git branch --show-current)
-echo $BB
-echo `git branch`
-echo `git branch --show-current`
-export GIT_BRANCH_SAMPLES=$(git rev-parse --abbrev-ref HEAD)
-echo !!!!!$GIT_BRANCH_SAMPLES
+# If running from local machine, change it to your branch name
+export GIT_BRANCH=$GITHUB_HEAD_REF
 
+# We use a different workspace for managed vnet endpoints
+az configure --defaults workspace=$WORKSPACE
 
+### setup VM & deploy/test ###
+# if vm exists, wait for 15 mins before trying to delete
+export VM_EXISTS=$(az vm list -o tsv --query "[?name=='$VM_NAME'].name")
+if [ "$VM_EXISTS" != "" ];
+then
+    echo "VM already exists from previous run. Waiting for 15 mins before deleting."
+	sleep 15m
+	az vm delete -n $VM_NAME -y
+fi
 
+# create the VM
+az deployment group create --template-file endpoints/online/managed/vnet/test_scoring/vm-main.bicep --parameters vmName=$VM_NAME identityName=$IDENTITY_NAME vnetName=$VNET_NAME subnetName=$SUBNET_NAME
+
+## Note for doc: create a VM: az vm create -n $VM_NAME
+
+# command in script: az deployment group create --template-file endpoints/online/managed/vnet/setup/vm_main.bicep #identity name is hardcoded uai-identity 
+az vm run-command invoke -n $VM_NAME --command-id RunShellScript --scripts @endpoints/online/managed/vnet/test_scoring/scripts/vmsetup.sh --parameters "SUBSCRIPTION:$SUBSCRIPTION" "RESOURCE_GROUP:$RESOURCE_GROUP" "LOCATION:$LOCATION" "IDENTITY_NAME:$IDENTITY_NAME" "GIT_BRANCH:$GIT_BRANCH"
+
+# build image
+az vm run-command invoke -n $VM_NAME --command-id RunShellScript --scripts @endpoints/online/managed/vnet/test_scoring/scripts/build_image.sh --parameters "SUBSCRIPTION:$SUBSCRIPTION" "RESOURCE_GROUP:$RESOURCE_GROUP" "LOCATION:$LOCATION" "IDENTITY_NAME:$IDENTITY_NAME" "ACR_NAME=$ACR_NAME"
+
+# create endpoint/deployment inside managed vnet and invoke it
+az vm run-command invoke -n $VM_NAME --command-id RunShellScript --scripts @endpoints/online/managed/vnet/test_scoring/scripts/create_moe.sh --parameters "SUBSCRIPTION:$SUBSCRIPTION" "RESOURCE_GROUP:$RESOURCE_GROUP" "LOCATION:$LOCATION" "IDENTITY_NAME:$IDENTITY_NAME" "WORKSPACE:$WORKSPACE" "ENDPOINT_NAME:$ENDPOINT_NAME" "ACR_NAME=$ACR_NAME"
+
+### Cleanup (run from build agent)
+# note: endpoint deletion happens from the script that runs within the vm (above)
+# <delete_vm> 
+az vm delete -n $VM_NAME -y
+# </delete_vm> 
