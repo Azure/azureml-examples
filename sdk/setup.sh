6ceb4d021fd79be93e427d7dd214b5c7ade99b39
Bala P V <balapv@microsoft.com>
2022-03-11 09:35:45 -0800

    Updating to latest version of SDK/CLI and adding ipython_genutils

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -2,7 +2,7 @@
 
 #pip install azure-ml==0.0.139 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 
-#using build # 0.0.58344031
-pip install azure-ml==0.0.58344031 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
+#using build # 0.0.58414767
+pip install azure-ml==0.0.58414767 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 
 pip list
