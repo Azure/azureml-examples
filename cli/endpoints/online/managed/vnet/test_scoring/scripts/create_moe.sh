57fcfe3f4051a8b6d3d429a943dafa6ab0ec6a9f
rsethur <rsethur@gmail.com>
2022-03-14 13:29:05 -0700

    additional comments

--- cli/endpoints/online/managed/vnet/test_scoring/scripts/create_moe.sh
+++ cli/endpoints/online/managed/vnet/test_scoring/scripts/create_moe.sh
@@ -13,9 +13,13 @@ az account set --subscription $SUBSCRIPTION
 az configure --defaults group=$RESOURCE_GROUP workspace=$WORKSPACE location=$LOCATION
 
 # <create_vnet_deployment> 
+# navigate to the cli directory in the azurem-examples repo
 cd /home/samples/azureml-examples/cli/
+# enable private preivew features in cli
 export AZURE_ML_CLI_PRIVATE_FEATURES_ENABLED=true
+# create endpoint
 az ml online-endpoint create --name $ENDPOINT_NAME -f endpoints/online/managed/vnet/endpoint.yml
+# create deployment in managed vnet
 az ml online-deployment create --name blue --endpoint $ENDPOINT_NAME -f endpoints/online/managed/vnet/blue-deployment-vnet.yml --all-traffic --set environment.image="$ACR_NAME.azurecr.io/repo/img:v1" private_network_connection="true"
 # </create_vnet_deployment> 
 
