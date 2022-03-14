bc3916023ac84557087ac9d6a33c0fc39e18339d
Bala P V <balapv@microsoft.com>
2022-03-14 10:12:38 -0700

    Updating to Build # 58490278

--- cli/setup.sh
+++ cli/setup.sh
@@ -5,7 +5,7 @@
 # rc install - uncomment and adjust below to run all tests on a CLI release candidate
 az extension remove -n ml
 #az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/ml-0.0.22_october_cand-py3-none-any.whl --yes
-az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/ml-0.0.58414767-py3-none-any.whl --yes
+az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/ml-0.0.58490278-py3-none-any.whl --yes
 
 # <set_variables>
 GROUP="azureml-examples-rg"
