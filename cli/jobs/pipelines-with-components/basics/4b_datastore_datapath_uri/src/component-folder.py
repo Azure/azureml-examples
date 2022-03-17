2046e16a726d9ab1e9649221c08d953feb9a1a67
lochen <cloga0216@gmail.com>
2022-03-17 21:40:21 +0800

    merge datastore uri sample

---
+++ cli/jobs/pipelines-with-components/basics/4b_datastore_datapath_uri/src/component-folder.py
@@ -0,0 +1,21 @@
+import argparse
+import os
+from datetime import datetime
+
+print("Hello Python World")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_data", type=str)
+parser.add_argument("--input_string", type=str)
+parser.add_argument("--output_data", type=str)
+
+args = parser.parse_args()
+
+print("sample_input_string: %s" % args.input_string)
+print("sample_input_data path: %s" % args.input_data)
+print("sample_output_data path: %s" % args.output_data)
+
+with open(
+    os.path.join(args.input_data, "hello-world.txt"), "wt"
+) as text_file:
+    print("hello world", file=text_file)
