06c360c14b048a11c5646398c2a9d59d85b9d169
Bala P V <balapv@microsoft.com>
2022-03-18 11:44:01 -0700

    Making setup files Unix friendly by removing \r

--- setup-repo/create-datasets.sh
+++ setup-repo/create-datasets.sh
@@ -1,17 +1,34 @@
 # this dataset is needed for the sample under cli/jobs/pipelines-with-components/basics/4d_dataset_input
+
 az ml data create -f ../cli/jobs/pipelines-with-components/basics/4d_dataset_input/data.yml
 
+
+
 # <download_untar_cifar>
+
 mkdir data
+
 wget "https://azuremlexamples.blob.core.windows.net/datasets/cifar-10-python.tar.gz"
+
 tar -xvzf cifar-10-python.tar.gz -C data
+
 # </download_untar_cifar>
 
+
+
 # <create_cifar>
+
 az ml data create --name cifar-10-example --version 1 --set path=data
+
 # </create_cifar>
 
+
+
 # <cleanup_cifar>
+
 rm cifar-10-python.tar.gz
+
 rm -r data
+
 # </cleanup_cifar>
+
