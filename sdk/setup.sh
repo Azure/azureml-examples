8f5a7ee9f4f1e4855a36d3dc1d37ce639b5519aa
Bala P V <balapv@microsoft.com>
2022-03-22 15:19:00 -0700

    Fixing source directory for data and updating CLI/SDK build

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -2,7 +2,7 @@
 
 #pip install azure-ml==0.0.139 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 
-#using build # 0.0.58947794
-pip install azure-ml==0.0.58947794 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
+#using build # 0.0.59143193
+pip install azure-ml==0.0.59143193 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 
 pip list
