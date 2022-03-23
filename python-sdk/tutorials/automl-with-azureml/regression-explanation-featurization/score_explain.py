866b53db765874ef201e26ad43b7dd1ffa3f7787
Jeff Shepherd <jeffshep@microsoft.com>
2022-03-23 16:34:28 -0700

    Applied black

--- python-sdk/tutorials/automl-with-azureml/regression-explanation-featurization/score_explain.py
+++ python-sdk/tutorials/automl-with-azureml/regression-explanation-featurization/score_explain.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import joblib
 from azureml.core.model import Model
-from azureml.train.automl.runtime.automl_explain_utilities import automl_setup_model_explanations
+from azureml.train.automl.runtime.automl_explain_utilities import (
+    automl_setup_model_explanations,
+)
 import scipy as sp
 
 
@@ -12,17 +14,17 @@ def init():
 
     # Retrieve the path to the model file using the model name
     # Assume original model is named original_prediction_model
-    automl_model_path = Model.get_model_path('automl_model')
-    scoring_explainer_path = Model.get_model_path('scoring_explainer')
+    automl_model_path = Model.get_model_path("automl_model")
+    scoring_explainer_path = Model.get_model_path("scoring_explainer")
 
     automl_model = joblib.load(automl_model_path)
     scoring_explainer = joblib.load(scoring_explainer_path)
 
 
 def is_multi_dimensional(matrix):
-    if hasattr(matrix, 'ndim') and matrix.ndim > 1:
+    if hasattr(matrix, "ndim") and matrix.ndim > 1:
         return True
-    if hasattr(matrix, 'shape') and matrix.shape[1]:
+    if hasattr(matrix, "shape") and matrix.shape[1]:
         return True
     return False
 
@@ -37,21 +39,30 @@ def convert_matrix(matrix):
 
 def run(raw_data):
     # Get predictions and explanations for each data point
-    data = pd.read_json(raw_data, orient='records')
+    data = pd.read_json(raw_data, orient="records")
     # Make prediction
     predictions = automl_model.predict(data)
     # Setup for inferencing explanations
-    automl_explainer_setup_obj = automl_setup_model_explanations(automl_model,
-                                                                 X_test=data, task='regression')
+    automl_explainer_setup_obj = automl_setup_model_explanations(
+        automl_model, X_test=data, task="regression"
+    )
     # Retrieve model explanations for engineered explanations
-    engineered_local_importance_values = scoring_explainer.explain(automl_explainer_setup_obj.X_test_transform)
-    engineered_local_importance_values = convert_matrix(engineered_local_importance_values)
+    engineered_local_importance_values = scoring_explainer.explain(
+        automl_explainer_setup_obj.X_test_transform
+    )
+    engineered_local_importance_values = convert_matrix(
+        engineered_local_importance_values
+    )
 
     # Retrieve model explanations for raw explanations
-    raw_local_importance_values = scoring_explainer.explain(automl_explainer_setup_obj.X_test_transform, get_raw=True)
+    raw_local_importance_values = scoring_explainer.explain(
+        automl_explainer_setup_obj.X_test_transform, get_raw=True
+    )
     raw_local_importance_values = convert_matrix(raw_local_importance_values)
 
     # You can return any data type as long as it is JSON-serializable
-    return {'predictions': predictions.tolist(),
-            'engineered_local_importance_values': engineered_local_importance_values,
-            'raw_local_importance_values': raw_local_importance_values}
+    return {
+        "predictions": predictions.tolist(),
+        "engineered_local_importance_values": engineered_local_importance_values,
+        "raw_local_importance_values": raw_local_importance_values,
+    }
