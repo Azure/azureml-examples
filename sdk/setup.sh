c90471084816d3440d19adc990055807b1823ea5
Bala P V <33712765+balapv@users.noreply.github.com>
2022-03-25 10:36:12 -0700

    Update setup.sh

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -2,7 +2,7 @@
 
 #pip install azure-ml==0.0.139 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 
-#using build # 0.0.59266094
-pip install azure-ml==0.0.59266094 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
+#using build # 0.0.59342943
+pip install azure-ml==0.0.59342943 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 
 pip list
