64585763de7bb2cca2831ca19a0f42aead0c9f51
zhangxingzhi <zhangxingzhi@microsoft.com>
2022-03-28 10:16:10 +0800

    feat: refine sweep examples with dummy components

--- sdk/jobs/pipelines/1c_pipeline_with_hyperparameter_sweep/dsl_component.py
+++ sdk/jobs/pipelines/1c_pipeline_with_hyperparameter_sweep/dsl_component.py
@@ -1,32 +1,108 @@
-import os, json
 from pathlib import Path
-
+from random import randint
+from uuid import uuid4
 
 from azure.ml import dsl, ArtifactInput, ArtifactOutput
 from azure.ml.entities import Environment
+from azureml.core import Run
+from random import random
 
+# init customer environment with conda YAML
+# the YAML file shall be put under your code folder.
 conda_env = Environment(
     conda_file=Path(__file__).parent / "conda.yaml",
     image="mcr.microsoft.com/azureml/openmpi3.1.2-ubuntu18.04"
 )
 
+
 @dsl.command_component(
-    name="TF_mnist",
-    version="1",
-    display_name="TF_mnist",
-    description="Train a basic neural network with TensorFlow on the MNIST dataset, distributed via TensorFlow.",
+    name="dsl_train_component",
+    display_name="Train",
+    description="A dummy train component defined by dsl component.",
+    version="0.0.2",
+    # specify distribution type if needed
+    # distribution={'type': 'mpi'},
+    # specify customer environment, note that azure-ml must be included.
     environment=conda_env,
-    distribution={
-        "type": "tensorflow",
-        "worker_count": 2
-    },
+    # specify your code folder, default code folder is current file's parent
+    # code='.'
 )
-def tf_func(
-    trained_model_output: ArtifactOutput, 
-    epochs=3,
-    steps_per_epoch=70,
-    per_worker_batch_size=64,
+def train_component_func(
+    training_data: ArtifactInput,
+    max_epochs: int,
+    model_output: ArtifactOutput,
+    learning_rate=0.02,
 ):
-    # avoid dependency issue
-    from train import train_and_save_model
-    train_and_save_model(per_worker_batch_size, epochs, steps_per_epoch, trained_model_output)
+    accuracy = random()
+    lines = [
+        f"Training data path: {training_data}",
+        f"Max epochs: {max_epochs}",
+        f"Learning rate: {learning_rate}",
+        f"Model output path: {model_output}",
+        f"Accuracy: {accuracy}",
+    ]
+
+    for line in lines:
+        print(line)
+
+    run = Run.get_context()
+    run.log("accuracy", accuracy)
+
+    # Do the train and save the trained model as a file into the output folder.
+    # Here only output a dummy data for demo.
+    model = str(uuid4())
+    (Path(model_output) / "model").write_text(model)
+
+
+@dsl.command_component(
+    name="dsl_score_component",
+    display_name="Score",
+    description="A dummy score component defined by dsl component.",
+    version="0.0.1",
+)
+def score_component_func(
+    model_input: ArtifactInput,
+    test_data: ArtifactInput,
+    score_output: ArtifactOutput,
+):
+
+    lines = [
+        f"Model path: {model_input}",
+        f"Test data path: {test_data}",
+        f"Scoring output path: {score_output}",
+    ]
+
+    for line in lines:
+        print(line)
+
+    # Load the model from input port
+    # Here only print the model as text since it is a dummy one
+    model = (Path(model_input) / "model").read_text()
+    print("Model:", model)
+
+    # Do scoring with the input model
+    # Here only print text to output file as demo
+    (Path(score_output) / "score").write_text("scored with {}".format(model))
+
+
+@dsl.command_component(
+    name="dsl_eval_component",
+    display_name="Evaluate",
+    description="A dummy evaluate component defined by dsl component.",
+    version="0.0.1",
+)
+def eval_component_func(
+    scoring_result: ArtifactInput,
+    eval_output: ArtifactOutput,
+):
+    lines = [
+        f"Scoring result path: {scoring_result}",
+        f"Evaluation output path: {eval_output}",
+    ]
+
+    for line in lines:
+        print(line)
+
+    # Evaluate the incoming scoring result and output evaluation result.
+    # Here only output a dummy file for demo.
+    (Path(eval_output) / "eval_result").write_text("eval_result")
