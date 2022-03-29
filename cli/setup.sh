5abe91bd22ca844611ba8e26adac1b3b5b50d5bb
Bala P V <balapv@microsoft.com>
2022-03-29 07:44:09 -0700

    Running latest build on eastus

--- cli/setup.sh
+++ cli/setup.sh
@@ -4,13 +4,13 @@
 
 # rc install - uncomment and adjust below to run all tests on a CLI release candidate
 az extension remove -n ml
-az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/ml-0.0.59513820-py3-none-any.whl --yes
+az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/ml-0.0.59558695-py3-none-any.whl --yes
 #az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/ml-0.0.22_october_cand-py3-none-any.whl --yes
  
 # <set_variables>
-# GROUP="azureml-examples"
-# LOCATION="eastus"
-# WORKSPACE="main"
+GROUP="azureml-examples"
+LOCATION="eastus"
+WORKSPACE="main"
 # </set_variables>
 
 # <set_variables>
@@ -22,9 +22,9 @@ az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheel
 
 # <set_variables>
 # Run in canary
-GROUP="azureml-examples"
-LOCATION="eastus2euap"
-WORKSPACE="main-canary"
+# GROUP="azureml-examples"
+# LOCATION="eastus2euap"
+# WORKSPACE="main-canary"
 # </set_variables>
 
 
