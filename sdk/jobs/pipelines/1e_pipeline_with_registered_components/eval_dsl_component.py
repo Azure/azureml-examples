7205f59041db9963c5bff3df89678cc361a17dcf
Clement Wang <clwan@microsoft.com>
2022-03-29 19:38:23 +0800

    update 1e

--- sdk/jobs/pipelines/1e_pipeline_with_registered_components/eval_dsl_component.py
+++ sdk/jobs/pipelines/1e_pipeline_with_registered_components/eval_dsl_component.py
@@ -5,7 +5,7 @@ from azure.ml import dsl, ArtifactInput, ArtifactOutput
     name="eval_model",
     display_name="Eval Model",
     description="A dummy eval component defined by dsl component.",
-    version="0.0.1",
+    version="0.0.3",
 )
 def eval_model(
     scoring_result: ArtifactInput,
