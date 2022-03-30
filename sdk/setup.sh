310f20675877b36dfd915f17a3b31368a1f0d411
Bala P V <balapv@microsoft.com>
2022-03-30 12:24:05 -0700

    Updating to latest build and testing in eastus

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -2,7 +2,7 @@
 
 #pip install azure-ml==0.0.139 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 
-#using build # 0.0.59558695
-pip install azure-ml==0.0.59558695 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
+#using build # 0.0.59687177
+pip install azure-ml==0.0.59687177 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 
 pip list
