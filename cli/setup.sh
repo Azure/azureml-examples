93418edb12e3285a29d12268ec5459a012f98457
zhangxingzhi <zhangxingzhi@microsoft.com>
2022-03-31 17:35:31 +0800

    feat: refine sweep node example to align with dsl

--- cli/setup.sh
+++ cli/setup.sh
@@ -4,7 +4,7 @@
 
 # rc install - uncomment and adjust below to run all tests on a CLI release candidate
 az extension remove -n ml
-az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/ml-0.0.59687177-py3-none-any.whl --yes
+az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/test-sdk-cli-v2/ml-0.0.59707384-py3-none-any.whl --yes
 #az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/ml-0.0.22_october_cand-py3-none-any.whl --yes
 
 # <set_variables>
@@ -14,5 +14,5 @@ WORKSPACE="main"
 # </set_variables>
 
 # <az_configure_defaults>
-az configure --defaults group=$GROUP workspace=$WORKSPACE location=$LOCATION
+# az configure --defaults group=$GROUP workspace=$WORKSPACE location=$LOCATION
 # </az_configure_defaults>
