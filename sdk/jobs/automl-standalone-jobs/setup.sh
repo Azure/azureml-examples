b282a2cbdd7858fe505561e7896c3070aae916b3
Razvan Tanase <ratanase@microsoft.com>
2022-03-30 17:23:31 -0700

    update version

--- sdk/jobs/automl-standalone-jobs/setup.sh
+++ sdk/jobs/automl-standalone-jobs/setup.sh
@@ -1,1 +1,1 @@
-pip install https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/59684499/azure_ml-0.0.59684499-py3-none-any.whl
+pip install https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/59621554/azure_ml-0.0.59621554-py3-none-any.whl
