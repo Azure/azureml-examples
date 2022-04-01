dcbd6a058f457b118eafa19249c34995c867eb9a
Lifeng Wei <lfwei@ucdavis.edu>
2022-04-01 10:25:45 -0700

    Lifengwei/update nlp notebooks (#1090)
    * remove some packages
    
    * add back a package
    
    * update notebooks, mainly change multilabel data file
    
    * data file
    
    * fix typos, codes in notebooks
    
    * update experiment name
    
    * fix typo, remove comments
    
    Co-authored-by: Lifeeng Wei <lifengwei@microsoft.com>

--- python-sdk/tutorials/automl-with-azureml/automl-nlp-multilabel/preprocessing.py
+++ python-sdk/tutorials/automl-with-azureml/automl-nlp-multilabel/preprocessing.py
@@ -1,11 +1,17 @@
 import ast
 import os
 
+import numpy as np
 import pandas as pd
 from collections import Counter
 from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
 
 N = 10  # the number of most popular labels to keep
+train_ratio = 0.2
+valid_ratio = 0.4
+test_ratio = 1 - train_ratio - valid_ratio
+seed = 101
 
 data = pd.read_csv("arxiv_data.csv")
 data["terms"] = data["terms"].apply(ast.literal_eval)
@@ -30,7 +36,19 @@ data["terms"] = data["terms"].apply(filter_labels)
 data["titles"] = data["titles"].apply(lambda x: x.replace("\n", " "))
 data["summaries"] = data["summaries"].apply(lambda x: x.replace("\n", " "))
 
+all_index = np.arange(data.shape[0])
+train_index, valid_index = train_test_split(all_index, train_size=train_ratio)
+valid_index, test_index = train_test_split(
+    valid_index, train_size=valid_ratio / (1 - train_ratio)
+)
+
+train_data = data.iloc[train_index, :]
+valid_data = data.iloc[valid_index, :]
+test_data = data.iloc[test_index, :]
+
 # Create the folder if not already exists, save dataset
 if not os.path.exists("data"):
     os.mkdir("data")
-data.to_csv("./data/arxiv_abstract.csv", index=False)
+train_data.to_csv("./data/arxiv_abstract_train.csv", index=False)
+valid_data.to_csv("./data/arxiv_abstract_valid.csv", index=False)
+test_data.to_csv("./data/arxiv_abstract_test.csv", index=False)
