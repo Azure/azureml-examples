6944565b2a342b6f7644d705bd9343576f17e52f
Razvan Tanase <ratanase@microsoft.com>
2022-04-01 15:47:31 -0700

    switch to private wheel for automl testing.

--- sdk/jobs/automl-standalone-jobs/setup.sh
+++ sdk/jobs/automl-standalone-jobs/setup.sh
@@ -1,5 +1,5 @@
-#pip install https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/59621554/azure_ml-0.0.59621554-py3-none-any.whl
-pip install azure-ml==2.2.1 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2-public
-# pip install mlflow
-# pip install azureml-mlflow
+pip install https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/59621554/azure_ml-0.0.59621554-py3-none-any.whl
+#pip install azure-ml==2.2.1 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2-public
+pip install mlflow
+pip install azureml-mlflow
 pip list
