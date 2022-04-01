0261ebaa18a819d7354d9e528279c67386712536
Han Wang <phoenix.seek@gmail.com>
2022-04-01 18:49:49 +0800

    change component name (#1075)
    * change component name
    
    * rename
    
    * add march-cli-preview in pipeline CI
    
    * update registered case
    
    * swith CI to master
    
    * add back component creation
    
    * update 3b to use rw_mount (#1097)
    
    * fix: fix run-pipeline-jobs.sh
    
    * change upload mode to rw_mount
    
    Co-authored-by: Han Wang <wanhan@microsoft.com>
    Co-authored-by: Cloga Chen <cloga0216@gmail.com>
    Co-authored-by: zhangxingzhi <zhangxingzhi@microsoft.com>

--- cli/run-pipeline-jobs.sh
+++ cli/run-pipeline-jobs.sh
@@ -1,86 +1,111 @@
 target_version="$RANDOM"
 
+az ml compute create -n cpu-cluster --type amlcompute --min-instances 0 --max-instances 8
+az ml compute create -n gpu-cluster --type amlcompute --min-instances 0 --max-instances 4 --size Standard_NC12
+
 cd jobs/pipelines-with-components/basics/1a_e2e_local_components
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../../
 
 cd jobs/pipelines-with-components/basics/1b_e2e_registered_components
+pwd
 az ml component create --file train.yml --set version=$target_version
 az ml component create --file score.yml --set version=$target_version
 az ml component create --file eval.yml --set version=$target_version
-az ml job create --file pipeline.yml --web
-az ml job create --file pipeline.yml --set jobs.train.compute=azureml:gpu-cluster --web
+az ml job create --file pipeline.yml
+az ml job create --file pipeline.yml --set jobs.train.compute=azureml:gpu-cluster
 cd ../../../../
 
 cd jobs/pipelines-with-components/basics/2a_basic_component
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../../
 
 cd jobs/pipelines-with-components/basics/2b_component_with_input_output
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../../
 
 cd jobs/pipelines-with-components/basics/3a_basic_pipeline
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../../
 
 cd jobs/pipelines-with-components/basics/3b_pipeline_with_data
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../../
 
 cd jobs/pipelines-with-components/basics/4a_local_data_input
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../../
 
 cd jobs/pipelines-with-components/basics/4b_datastore_datapath_uri
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../../
 
-cd jobs/pipelines-with-components/basics/4c_dataset_input
-az ml data create --file data.yml --version $target_version
-az ml job create --file pipeline.yml --web
-cd ../../../../
+# cd jobs/pipelines-with-components/basics/4c_dataset_input
+# pwd
+# az ml data create --file data.yml --version $target_version
+# az ml job create --file pipeline.yml
+# cd ../../../../
 
-cd jobs/pipelines-with-components/basics/4d_web_url_input
-az ml job create --file pipeline.yml --web
+cd jobs/pipelines-with-components/basics/4c_web_url_input
+pwd
+az ml job create --file pipeline.yml
 cd ../../../../
 
 cd jobs/pipelines-with-components/basics/5a_env_public_docker_image
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../../
 
 cd jobs/pipelines-with-components/basics/5b_env_registered
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../../
 
 cd jobs/pipelines-with-components/basics/5c_env_conda_file
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../../
 
 cd jobs/pipelines-with-components/basics/6a_tf_hello_world
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../../
 
 cd jobs/pipelines-with-components/basics/6b_pytorch_hello_world
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../../
 
 cd jobs/pipelines-with-components/basics/6c_r_iris
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../../
 
 cd jobs/pipelines-with-components/image_classification_with_densenet
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../
 
 cd jobs/pipelines-with-components/nyc_taxi_data_regression
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../
 
 cd jobs/pipelines/nyc-taxi
-az ml job create --file pipeline.yml --web
+pwd
+az ml job create --file pipeline.yml
 cd ../../../
 
 cd jobs/pipelines/cifar-10
+pwd
 az ml job create --file pipeline.yml --web
 cd ../../../
+
+az --version
