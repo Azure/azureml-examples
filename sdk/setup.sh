4f5629a92e4fcbdea3bd6fdc183853870327c27a
Razvan Tanase <ratanase@microsoft.com>
2022-04-01 22:45:24 -0700

    updating to latest private wheel

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #bugbash wheel
-pip install https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/59621554/azure_ml-0.0.59621554-py3-none-any.whl
+#pip install https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/59621554/azure_ml-0.0.59621554-py3-none-any.whl
 #latest wheel
-#pip install https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/59876861/azure_ml-0.0.59876861-py3-none-any.whl
+pip install https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/59876861/azure_ml-0.0.59876861-py3-none-any.whl
 pip install mlflow
 pip install azureml-mlflow
 pip list
