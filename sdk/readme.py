dfecf358596795cf5520b2118f2879f7483d4be7
Razvan Tanase <ratanase@microsoft.com>
2022-04-04 14:22:46 -0700

    merged from parent branch.

--- sdk/readme.py
+++ sdk/readme.py
@@ -18,7 +18,7 @@ NOT_SCHEDULED_NOTEBOOKS = [
 # use if running on a release candidate, else make it empty
 BRANCH = "main"  # default - do not change
 BRANCH = "sdk-preview"  # this should be deleted when this branch is merged to main
-BRANCH = "apri-preview-automl"  # this should be deleted when this branch is merged to sdk-preview
+BRANCH = "april-preview-automl"  # this should be deleted when this branch is merged to sdk-preview
 
 
 def main(args):
@@ -73,8 +73,7 @@ on:\n"""
     workflow_yaml += f"""  pull_request:
     branches:
       - main
-      - sdk-preview
-      - april-preview-automl\n"""
+      - sdk-preview\n"""
     if BRANCH != "main":
         workflow_yaml += f"""      - {BRANCH}\n"""
     workflow_yaml += f"""    paths:
@@ -115,26 +114,26 @@ jobs:
           sed -i -e "s/<AML_WORKSPACE_NAME>/main/g" {name}.ipynb
           sed -i -e "s/InteractiveBrowserCredential/AzureCliCredential/g" {name}.ipynb\n"""
     if name == "workspace":
-      workflow_yaml += f"""
+        workflow_yaml += f"""
           # generate a random workspace name
           # sed -i -e "s/mlw-basic-prod/mlw-basic-prod-$(echo $RANDOM | md5sum | head -c 10)/g" {name}.ipynb
           # skip other workpace creation commands for now
           sed -i -e "s/ml_client.begin_create_or_update(ws_with_existing)/# ml_client.begin_create_or_update(ws_with_existing)/g" {name}.ipynb        
           sed -i -e "s/ml_client.workspaces.begin_create(ws_private_link)/# ml_client.workspaces.begin_create(ws_private_link)/g" {name}.ipynb        
-          sed -i -e "s/ml_client.workspaces.begin_create(ws_private_link)/# ws_from_config = MLClient.from_config()/g" {name}.ipynb\n"""            
-    
+          sed -i -e "s/ml_client.workspaces.begin_create(ws_private_link)/# ws_from_config = MLClient.from_config()/g" {name}.ipynb\n"""
+
     if not ("automl" in folder):
-      workflow_yaml += f"""          
+        workflow_yaml += f"""          
             papermill -k python {name}.ipynb {name}.output.ipynb
         working-directory: sdk/{folder}"""
     elif "nlp" in folder or "image" in folder:
-      # need GPU cluster, so override the compute cluster name to dedicated 
-      workflow_yaml += f"""          
+        # need GPU cluster, so override the compute cluster name to dedicated
+        workflow_yaml += f"""          
           papermill -k python -p compute_name automl-gpu-cluster {name}.ipynb {name}.output.ipynb
       working-directory: sdk/{folder}"""
     else:
-        # need GPU cluster, so override the compute cluster name to dedicated 
-      workflow_yaml += f"""          
+        # need GPU cluster, so override the compute cluster name to dedicated
+        workflow_yaml += f"""          
           papermill -k python -p compute_name automl-cpu-cluster {name}.ipynb {name}.output.ipynb
       working-directory: sdk/{folder}"""
 
@@ -146,9 +145,7 @@ jobs:
         name: {name}
         path: sdk/{folder}\n"""
 
-    workflow_file = (
-        f"../.github/workflows/sdk-{classification}-{name}.yml"
-    )
+    workflow_file = f"../.github/workflows/sdk-{classification}-{name}.yml"
     workflow_before = ""
     if os.path.exists(workflow_file):
         with open(workflow_file, "r") as f:
@@ -161,58 +158,58 @@ jobs:
 
 
 def write_readme(notebooks):
-  if BRANCH=="":
-    branch="main"
-  else:
-    branch=BRANCH
-    # read in prefix.md and suffix.md
-    with open("prefix.md", "r") as f:
-        prefix = f.read()
-    with open("suffix.md", "r") as f:
-        suffix = f.read()
+    if BRANCH == "":
+        branch = "main"
+    else:
+        branch = BRANCH
+        # read in prefix.md and suffix.md
+        with open("prefix.md", "r") as f:
+            prefix = f.read()
+        with open("suffix.md", "r") as f:
+            suffix = f.read()
 
-    # define markdown tables
-    notebook_table = f"Test Status is for branch - **_{branch}_**\n|Area|Sub-Area|Notebook|Description|Status|\n|--|--|--|--|--|\n"
-    for notebook in notebooks:
-        # get notebook name
-        name = notebook.split("/")[-1].replace(".ipynb", "")
-        area = notebook.split("/")[0]
-        sub_area = notebook.split("/")[1]
-        folder = os.path.dirname(notebook)
-        classification = folder.replace("/", "-")
+        # define markdown tables
+        notebook_table = f"Test Status is for branch - **_{branch}_**\n|Area|Sub-Area|Notebook|Description|Status|\n|--|--|--|--|--|\n"
+        for notebook in notebooks:
+            # get notebook name
+            name = notebook.split("/")[-1].replace(".ipynb", "")
+            area = notebook.split("/")[0]
+            sub_area = notebook.split("/")[1]
+            folder = os.path.dirname(notebook)
+            classification = folder.replace("/", "-")
 
-        try:
-            # read in notebook
-            with open(notebook, "r") as f:
-                data = json.load(f)
-
-            description = "*no description*"
             try:
-                if data["metadata"]["description"] is not None:
-                    description = data["metadata"]["description"]["description"]
+                # read in notebook
+                with open(notebook, "r") as f:
+                    data = json.load(f)
+
+                description = "*no description*"
+                try:
+                    if data["metadata"]["description"] is not None:
+                        description = data["metadata"]["description"]["description"]
+                except:
+                    pass
             except:
+                print("Could not load", notebook)
                 pass
-        except:
-            print("Could not load", notebook)
-            pass
 
-        if any(excluded in notebook for excluded in NOT_TESTED_NOTEBOOKS):
-            description += " - _This sample is excluded from automated tests_"
-        if any(excluded in notebook for excluded in NOT_SCHEDULED_NOTEBOOKS):
-            description += " - _This sample is only tested on demand_"
+            if any(excluded in notebook for excluded in NOT_TESTED_NOTEBOOKS):
+                description += " - _This sample is excluded from automated tests_"
+            if any(excluded in notebook for excluded in NOT_SCHEDULED_NOTEBOOKS):
+                description += " - _This sample is only tested on demand_"
 
-        # write workflow file
-        notebook_table += (
-            write_readme_row(
-                branch, notebook, name, classification, area, sub_area, description
+            # write workflow file
+            notebook_table += (
+                write_readme_row(
+                    branch, notebook, name, classification, area, sub_area, description
+                )
+                + "\n"
             )
-            + "\n"
-        )
 
-    print("writing README.md...")
-    with open("README.md", "w") as f:
-        f.write(prefix + notebook_table + suffix)
-    print("finished writing README.md")
+        print("writing README.md...")
+        with open("README.md", "w") as f:
+            f.write(prefix + notebook_table + suffix)
+        print("finished writing README.md")
 
 
 def write_readme_row(
