195e2dc883211320c44de81f2ede1e51407ca97c
Bala P V <balapv@microsoft.com>
2022-04-03 21:13:19 -0700

    Fixing readme.py

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#pip install azure-ml==0.0.139 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
-
-#using build # 0.0.59687177
+# <azure-ml_install>
 pip install azure-ml==2.2.1 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2-public
+# </azure-ml_install>
+
+#using private build # 0.0.59687177
+#pip install azure-ml==59687177 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 
 pip list
