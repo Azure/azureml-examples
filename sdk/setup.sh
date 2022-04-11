8570c39d6d572bac3024a5e745f9e662e55d2bec
Clement Wang <clwan@microsoft.com>
2022-04-11 17:58:03 +0800

    update dev build version

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -5,7 +5,7 @@
 #pip install azure-ml==2.2.1 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2-public
 
 #using dev build
-pip install azure-ml==0.0.59831705 --extra-index-url https://azuremlsdktestpypi.azureedge.net/test-sdk-cli-v2
+pip install azure-ml==0.0.60488751 --extra-index-url https://azuremlsdktestpypi.azureedge.net/test-sdk-cli-v2
 
 
 pip list
