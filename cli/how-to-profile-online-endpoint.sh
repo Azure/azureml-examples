404d9cfea9e30f2a6574fb3096161eb5f2acb437
Xin Yong <xiyon@microsoft.com>
2022-04-15 16:59:32 +0800

    fix profiling example

--- cli/how-to-profile-online-endpoint.sh
+++ cli/how-to-profile-online-endpoint.sh
@@ -117,12 +117,9 @@ else
 fi
 
 # create role assignment for acessing workspace resources
-compute_resource_id=`az ml compute show --name $PROFILER_COMPUTE_NAME --query id -o tsv`
-workspace_resource_id=`echo $compute_resource_id | sed 's/\(.*\)\/computes\/.*/\1/'`
-access_token=`az account get-access-token --query accessToken -o tsv`
-compute_info=`curl https://management.azure.com$compute_resource_id?api-version=2021-03-01-preview -H "Content-Type: application/json" -H "Authorization: Bearer $access_token"`
-if [[ $? -ne 0 ]]; then echo "Failed to get info for compute $PROFILER_COMPUTE_NAME" && exit 1; fi
-identity_object_id=`echo $compute_info | jq '.identity.principalId' | sed "s/\"//g"`
+compute_info=`az ml compute show --name $PROFILER_COMPUTE_NAME --query '{"id": id, "identity_object_id": identity.principal_id}' -o json`
+workspace_resource_id=`echo $compute_info | jq -r '.id' | sed 's/\(.*\)\/computes\/.*/\1/'`
+identity_object_id=`echo $compute_info | jq -r '.identity_object_id'`
 az role assignment create --role Contributor --assignee-object-id $identity_object_id --scope $workspace_resource_id
 if [[ $? -ne 0 ]]; then echo "Failed to create role assignment for compute $PROFILER_COMPUTE_NAME" && exit 1; fi
 # </create_compute_cluster_for_hosting_the_profiler>
