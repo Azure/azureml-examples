f61bfb8c3054b7b2a24b2388e5dc5a47bfcf0577
Srujan Saggam <41802116+srsaggam@users.noreply.github.com>
2022-04-15 10:29:00 -0700

    Update setup.sh

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -5,7 +5,7 @@
 #pip install azure-ml==2.2.1 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2-public
 
 #using dev build
-pip install azure-ml==0.0.60376978 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
+pip install azure-ml==0.0.60868327 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 
 pip install mlflow
 pip install azureml-mlflow
