077ae20d91277d8cca46473f377a9f04495b4fcb
Clement Wang <clwan@microsoft.com>
2022-04-18 09:29:41 +0800

    update setup.sh

--- cli/setup.sh
+++ cli/setup.sh
@@ -7,7 +7,7 @@ az extension remove -n ml
 
 # Use a daily build
 # az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2/ml-0.0.60868327-py3-none-any.whl --yes
-az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/azureml-v2-cli-e2e-test/60866320/ml-0.0.60866320-py3-none-any.whl --pip-extra-index-urls https://azuremlsdktestpypi.azureedge.net/azureml-v2-cli-e2e-test/60866320 --yes
+az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/azureml-v2-cli-e2e-test/61025031/ml-0.0.61025031-py3-none-any.whl --pip-extra-index-urls https://azuremlsdktestpypi.azureedge.net/azureml-v2-cli-e2e-test/61025031 --yes
  
 # <set_variables>
 GROUP="azureml-examples"
--- sdk/setup.sh
+++ sdk/setup.sh
@@ -5,8 +5,8 @@
 #pip install azure-ml==2.2.1 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2-public
 
 #using dev build
-pip install azure-ml==0.0.60868327 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
-pip install azure-ml==0.0.60864551 --extra-index-url https://azuremlsdktestpypi.azureedge.net/azureml-v2-cli-e2e-test/60864551
+# pip install azure-ml==0.0.60868327 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
+pip install azure-ml==0.0.61025031 --extra-index-url https://azuremlsdktestpypi.azureedge.net/azureml-v2-cli-e2e-test/61025031
 
 # pip install mlflow
 # pip install azureml-mlflow
