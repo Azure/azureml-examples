5bc2406ca18de48f8eafd09974e9576b815a6ac1
Bala P V <balapv@microsoft.com>
2022-04-18 09:30:08 -0700

    Updating to latest build

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -5,7 +5,7 @@
 #pip install azure-ml==2.2.1 --extra-index-url  https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2-public
 
 #using dev build
-pip install azure-ml==0.0.61029422 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
+pip install azure-ml==0.0.61038385 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 
 # pip install mlflow
 # pip install azureml-mlflow
