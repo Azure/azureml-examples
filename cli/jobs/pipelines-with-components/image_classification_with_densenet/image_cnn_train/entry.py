cdc31bd8b7c86e98a154b6565d5ba7b694b18f4d
Clement Wang <clwan@microsoft.com>
2022-04-18 14:38:52 +0800

    update Input

--- cli/jobs/pipelines-with-components/image_classification_with_densenet/image_cnn_train/entry.py
+++ cli/jobs/pipelines-with-components/image_classification_with_densenet/image_cnn_train/entry.py
@@ -7,9 +7,8 @@ from multiprocessing.pool import ThreadPool
 from multiprocessing import cpu_count
 import functools
 from enum import Enum
-from azure.ml import dsl
+from azure.ml import dsl, Input
 from azure.ml.dsl._component import ComponentExecutor
-from azure.ml.dsl._types import URIFolderInput, NumberInput
 
 
 class Data_BackendEnum(Enum):
@@ -82,8 +81,8 @@ def convert_image_directory_to_specific_format(
     name="imagecnn_train", description="imagecnn_train main function"
 )
 def main(
-    train_data: URIFolderInput(description="path to train dataset") = None,
-    val_data: URIFolderInput(description="path to valid dataset") = None,
+    train_data: Input(type="uri_folder", description="path to train dataset") = None,
+    val_data: Input(type="uri_folder", description="path to valid dataset") = None,
     data_backend="dali-cpu",
     arch="resnet50",
     model_config="classic",
