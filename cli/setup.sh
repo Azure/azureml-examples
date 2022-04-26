360e9ac33a4db08f480d216600d87779b411c51b
rsethur <rsethur@gmail.com>
2022-04-26 11:26:39 -0700

    revert  setup.sh changes

--- cli/setup.sh
+++ cli/setup.sh
@@ -1,14 +1,10 @@
 # <az_ml_install>
-# todo: remove comment
-#az extension add -n ml -y
+az extension add -n ml -y
 # </az_ml_install>
 
 # rc install - uncomment and adjust below to run all tests on a CLI release candidate
 #z extension remove -n ml
 #az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2-public/ml-2.2.1-py3-none-any.whl --yes
-# todo: remove the below 2 lines
-az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/azureml-v2-cli-e2e-test/61426493/ml-0.0.61426493-py3-none-any.whl --pip-extra-index-urls https://azuremlsdktestpypi.azureedge.net/azureml-v2-cli-e2e-test/61426493 --yes
-export AZURE_ML_CLI_PRIVATE_FEATURES_ENABLED=true
  
 # <set_variables>
 GROUP="azureml-examples"
