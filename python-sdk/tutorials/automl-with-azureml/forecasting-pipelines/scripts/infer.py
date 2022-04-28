b1728a3ef511ada72a2148182d249f96f153d429
Yuan Zhuang <yuzhua@microsoft.com>
2022-04-27 18:23:04 -0700

    add fix

--- python-sdk/tutorials/automl-with-azureml/forecasting-pipelines/scripts/infer.py
+++ python-sdk/tutorials/automl-with-azureml/forecasting-pipelines/scripts/infer.py
@@ -42,7 +42,9 @@ def infer_forecasting_dataset_tcn(
         ),
         name=output_dataset_name,
     )
-    df_all.to_csv(os.path.join(output_path, output_dataset_name + ".csv"), index=False)
+    df_all.to_csv(
+        os.path.join([output_path, output_dataset_name + ".csv"]), index=False
+    )
 
 
 def map_location_cuda(storage, loc):
