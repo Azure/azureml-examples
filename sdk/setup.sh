99f9f62419aa0038ff3904502b1b20200941762c
Brynn <1046236388@qq.com>
2022-04-28 18:27:04 +0800

    Add sdk 1f pipeline using schedule (#1191)
    * Add sdk pipeline using schedule sample

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # <az_ml_install>
-pip install azure-ml==0.0.61299632 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
+pip install azure-ml==0.0.61771647 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 # </az_ml_install>
 
 pip list
