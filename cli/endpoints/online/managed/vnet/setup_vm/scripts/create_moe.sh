94eb43aea55d4f4f631492588f4b5e80f12e09bb
rsethur <rsethur@gmail.com>
2022-05-02 11:38:44 -0700

    test

--- cli/endpoints/online/managed/vnet/setup_vm/scripts/create_moe.sh
+++ cli/endpoints/online/managed/vnet/setup_vm/scripts/create_moe.sh
@@ -17,11 +17,11 @@ az configure --defaults group=$RESOURCE_GROUP workspace=$WORKSPACE location=$LOC
 cd /home/samples/azureml-examples/cli/
 
 # create endpoint
-az ml online-endpoint create --local --name $ENDPOINT_NAME -f $ENDPOINT_FILE_PATH --set public_network_access="disabled"
+#az ml online-endpoint create --local --name $ENDPOINT_NAME -f $ENDPOINT_FILE_PATH --set public_network_access="disabled"
 # create deployment in managed vnet
-az ml online-deployment create --local --name blue --endpoint $ENDPOINT_NAME -f $DEPLOYMENT_FILE_PATH --all-traffic --set environment.image="$ACR_NAME.azurecr.io/repo/$IMAGE_NAME:v1" egress_public_network_access="disabled"
+#az ml online-deployment create --local --name blue --endpoint $ENDPOINT_NAME -f $DEPLOYMENT_FILE_PATH --all-traffic --set environment.image="$ACR_NAME.azurecr.io/repo/$IMAGE_NAME:v1" egress_public_network_access="disabled"
 # check if scoring works
-az ml online-endpoint invoke --local --name $ENDPOINT_NAME --request-file $SAMPLE_REQUEST_PATH
+#az ml online-endpoint invoke --local --name $ENDPOINT_NAME --request-file $SAMPLE_REQUEST_PATH
 # </create_local_deployment> 
 
 # <create_vnet_deployment> 
