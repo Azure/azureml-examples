446450a9cd275227737617b47e3bed6d621b7491
rsethur <rsethur@gmail.com>
2022-05-04 17:03:43 -0700

    new cli build

--- cli/endpoints/online/managed/vnet/setup_vm/scripts/create_moe.sh
+++ cli/endpoints/online/managed/vnet/setup_vm/scripts/create_moe.sh
@@ -29,9 +29,9 @@ az ml online-endpoint invoke --local --name $ENDPOINT_NAME --request-file $SAMPL
 cd /home/samples/azureml-examples/cli/
 
 # create endpoint
-#az ml online-endpoint create --name $ENDPOINT_NAME -f $ENDPOINT_FILE_PATH --set public_network_access="disabled"
+az ml online-endpoint create --name $ENDPOINT_NAME -f $ENDPOINT_FILE_PATH --set public_network_access="disabled"
 # create deployment in managed vnet
-#az ml online-deployment create --name blue --endpoint $ENDPOINT_NAME -f $DEPLOYMENT_FILE_PATH --all-traffic --set environment.image="$ACR_NAME.azurecr.io/repo/$IMAGE_NAME:v1" egress_public_network_access="disabled"
+az ml online-deployment create --name blue --endpoint $ENDPOINT_NAME -f $DEPLOYMENT_FILE_PATH --all-traffic --set environment.image="$ACR_NAME.azurecr.io/repo/$IMAGE_NAME:v1" egress_public_network_access="disabled"
 # </create_vnet_deployment> 
 
 # <get_logs> 
--- cli/endpoints/online/managed/vnet/setup_vm/scripts/vmsetup.sh
+++ cli/endpoints/online/managed/vnet/setup_vm/scripts/vmsetup.sh
@@ -20,7 +20,7 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash && az extension add --upg
 #--- todo: remove the below lines before merging with main---#
 az extension remove -n ml
 export AZURE_ML_CLI_PRIVATE_FEATURES_ENABLED=true
-az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/azureml-v2-cli-e2e-test/61741959/ml-0.0.61741959-py3-none-any.whl --pip-extra-index-urls https://azuremlsdktestpypi.azureedge.net/azureml-v2-cli-e2e-test/61741959 --yes
+az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/azureml-v2-cli-e2e-test/62191563/ml-0.0.62191563-py3-none-any.whl --pip-extra-index-urls https://azuremlsdktestpypi.azureedge.net/azureml-v2-cli-e2e-test/62191563 --yes
 #---end of lines to be removed---#
 
 # login using az cli. 
