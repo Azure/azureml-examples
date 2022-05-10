7163bdd2ea294f7dfd66be863f80dca47b37e812
Alex Wallace <v-alwallace@microsoft.com>
2022-05-09 17:13:56 -0700

    init

--- cli/deploy-custom-container-triton.sh
+++ cli/deploy-custom-container-triton.sh
@@ -18,18 +18,23 @@ export ENDPOINT_NAME="endpoint-12343"
 export BASE_PATH="endpoints/online/custom-container/triton"
 mkdir $BASE_PATH/models
 cp -r endpoints/online/triton/single-model/models $BASE_PATH
+cp -r endpoints/online/triton/single-model/triton_densenet_scoring.py $BASE_PATH
+cp -r endpoints/online/triton/single-model/densenet_labels.txt $BASE_PATH
 # </set_base_path_and_copy_model>
 
 # <login_to_acr>
 az acr login -n ${ACR_NAME} 
 # </login_to_acr> 
 
+# TODO: Delete
+docker build -t azureml-examples/triton-cc:latest $BASE_PATH
+
 # <build_with_acr>
 az acr build -t azureml-examples/triton-cc:latest -r $ACR_NAME $BASE_PATH
 # </build_with_acr>
 
 # <create_endpoint>
-az ml online-endpoint create -n $ENDPOINT_NAME --auth-mode key 
+az ml online-endpoint create -n $ENDPOINT_NAME --auth-mode key
 # </create_endpoint>
 
 cp $BASE_PATH/deployment.yaml $BASE_PATH/deployment_original.yaml
@@ -42,6 +47,12 @@ az ml online-deployment create --endpoint-name $ENDPOINT_NAME -f $BASE_PATH/depl
 # <test_online_endpoint>
 az ml online-endpoint invoke -n ${ENDPOINT_NAME} --request-file "$BASE_PATH/model-1/sample-request.json"
 # </test_online_endpoint>
+ 
+curl -X GET https://aka.ms/peacock-pic -O
+
+curl -X POST  localhost:5001/score -data-file "@peacock-pic"  -H "Content-Type: image/jpeg"
+
+curl -X GET localhost:5001/score
 
 # <delete_online_endpoint>
 az ml online-endpoint delete -y -n $ENDPOINT_NAME
