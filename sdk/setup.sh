7d7ee68208072c6c4e547cfd605eea3e1d6e4e54
Bala P V <balapv@microsoft.com>
2022-05-17 09:14:35 -0700

    Updating to latest builds

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -7,7 +7,7 @@
 # <az_ml_sdk_test_install>
 #pip install azure-ml==0.0.61212840 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 #pip install azure-ai-ml==0.0.62601697 --extra-index-url https://azuremlsdktestpypi.azureedge.net/azureml-v2-cli-e2e-test/62601697
-pip install azure-ai-ml==0.0.63030801 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
+pip install azure-ai-ml==0.0.63075866 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 # </az_ml_sdk_test_install>
 
 pip list
