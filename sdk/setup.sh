e8805eb72003bcf12533834bc8c71fa6024b5994
Miseon Park <mipark@microsoft.com>
2022-05-23 14:23:38 -0700

    update index

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # <az_ml_sdk_install>
-pip install --pre azure-ai-ml
+# pip install --pre azure-ai-ml
 # </az_ml_sdk_install>
 
 # <mldesigner_install>
@@ -9,7 +9,7 @@ pip install mldesigner==0.0.62828565 --extra-index-url https://azuremlsdktestpyp
 # </mldesigner_install>
 
 # <az_ml_sdk_test_install>
-# pip install azure-ai-ml==0.0.63075866 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
+pip install azure-ai-ml==0.0.63572232 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 # pip install -i https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/ azure-ai-ml==0.1.0b1
 # </az_ml_sdk_test_install>
 
