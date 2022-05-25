2683aef7e9b8d9323a2186d135bb1d9a9ff96a18
Dennis Eikelenboom <92387505+deeikele@users.noreply.github.com>
2022-05-25 10:43:00 -0700

    fix test

--- cli/deploy-mlcompute-create-with-user-identity.sh
+++ cli/deploy-mlcompute-create-with-user-identity.sh
@@ -1,1 +1,1 @@
-az ml compute create --name mycluster --type amlcompute --identity-type userassigned --user-assigned-identities user_identity_resource_id
+az ml compute create --name mycluster --type amlcompute --identity-type user_assigned --user-assigned-identities user_identity_resource_id
