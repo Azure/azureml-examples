fbac58b3a4c59554ab2d9c34fdd5aa78dc843363
Daniel Holstein <daholste@microsoft.com>
2022-06-06 15:23:15 -0700

    rev

---
+++ cli/jobs/training/vision/object-detection-using-built-in-component/README.md
@@ -0,0 +1,28 @@
+# Object Detection Using Built-In Component
+
+## Overview
+
+Using built-in components, you can train an object detection model (and other vision models) without writing any code. You can refer to documentation [here](https://github.com/Azure/azureml-assets/tree/main/training/vision/components/README.md) for more information on built-in vision components.
+
+
+## How to Run
+
+1. Prepare the data
+
+    You first need to prepare the data: The image based datasets need to be uploaded and later the JSONL dataset needs to be generated based on the uploaded images URIs.
+
+    You do this by running the prepare_data.py available in this folder, with a command like:
+
+    ``` bash
+    /> python prepare_data.py --workspace-name [YOUR_AZURE_WORKSPACE] --resource-group [YOUR_AZURE_RESOURCE_GROUP] --subscription [YOUR_AZURE_SUBSCRIPTION]
+    ```
+
+1. Run the CLI command
+
+    Run the CLI command pointing to the pipeline .YML file in this folder plus the Azure ML IDs needed.
+
+    Note: Your compute cluster should be GPU-based when training with images or text. You need to specify/change the name of the cluster in the .YML file (compute: azureml:gpu-cluster).
+
+    ``` bash
+    /> az ml job create --file ./object-detection-using-built-in-component.yml --workspace-name [YOUR_AZURE_WORKSPACE] --resource-group [YOUR_AZURE_RESOURCE_GROUP] --subscription [YOUR_AZURE_SUBSCRIPTION]
+    ```
---
+++ cli/jobs/training/vision/object-detection-using-built-in-component/custom-components/src/predict.py
@@ -0,0 +1,56 @@
+import argparse
+import base64
+import os
+import urllib.request
+from zipfile import ZipFile
+
+import mlflow.pyfunc
+import pandas as pd
+
+if __name__ == "__main__":
+    # Parse input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str)
+    args, _ = parser.parse_known_args()
+
+    # Download images to score
+    download_url = "https://cvbp-secondary.z19.web.core.windows.net/datasets/object_detection/odFridgeObjects.zip"
+    data_file = "data/odFridgeObjects.zip"
+    os.mkdir("data")
+    print("Downloading images...")
+    urllib.request.urlretrieve(download_url, filename=data_file)
+    print("Download completed")
+
+    # Extract downloaded images
+    with ZipFile(data_file, "r") as zip:
+        print("Extracting downloaded images...")
+        zip.extractall(path="./data")
+
+    # Images to score
+    test_image_paths = [
+        "./data/odFridgeObjects/images/1.jpg",
+        "./data/odFridgeObjects/images/2.jpg",
+        "./data/odFridgeObjects/images/3.jpg",
+    ]
+
+    # Define helper method to read the bytes of a file from disk
+    def read_file_bytes(image_path):
+        with open(image_path, "rb") as f:
+            return f.read()
+
+    # Read the test images into a pandas DataFrame
+    test_df = pd.DataFrame(
+        data=[base64.encodebytes(read_file_bytes(image_path)) for image_path in test_image_paths],
+        columns=["image"],
+    )
+    print(f"Test image DataFrame shape: {test_df.shape}")
+
+    # Load the trained model
+    pyfunc_model = mlflow.pyfunc.load_model(args.model)
+
+    # Score the test images
+    result = pyfunc_model.predict(test_df).to_json(orient="records")
+
+    # Print the predictions
+    print('Predictions:')
+    print(result)
