8e6c440193da085df78f5f76081a1f1181cff15f
Ankit Singhal <30610298+singankit@users.noreply.github.com>
2022-06-08 17:31:23 -0700

    Update setup.sh

--- cli/setup.sh
+++ cli/setup.sh
@@ -6,8 +6,8 @@ az extension add -n ml -y
 az extension remove -n ml
 
 # Use a daily build
-# az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2-public/ml-2.5.0-py3-none-any.whl --yes
-az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/azureml-v2-cli-e2e-test/64533945/ml-0.0.64533945-py3-none-any.whl --pip-extra-index-urls https://azuremlsdktestpypi.azureedge.net/azureml-v2-cli-e2e-test/64533945 --yes
+az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2-public/ml-2.5.0-py3-none-any.whl --yes
+# az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/azureml-v2-cli-e2e-test/64533945/ml-0.0.64533945-py3-none-any.whl --pip-extra-index-urls https://azuremlsdktestpypi.azureedge.net/azureml-v2-cli-e2e-test/64533945 --yes
  
 # <set_variables>
 GROUP="azureml-examples"
