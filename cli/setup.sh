cef79450753bea594cdb3b36ddc25785e7c8e19b
zhangxingzhi <zhangxingzhi@microsoft.com>
2022-06-10 08:53:02 +0000

    fix: correct set environment variable

--- cli/setup.sh
+++ cli/setup.sh
@@ -16,7 +16,7 @@ export AZURE_ML_CLI_PRIVATE_FEATURES_ENABLED="true"
 # </set_variables>
 
 # <set_env_variables>
-echo AZURE_ML_CLI_PRIVATE_FEATURES_ENABLED
+echo $AZURE_ML_CLI_PRIVATE_FEATURES_ENABLED
 # </set_env_variables>
 
 # <az_configure_defaults>
