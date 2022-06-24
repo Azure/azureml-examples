fa446a4cc2c7e86cabe9c69382b12b5e04d3aab6
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2022-06-24 12:09:35 -0700

    Update sdk setup.sh

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # <az_ml_sdk_install>
-pip install --pre azure-ai-ml
+#pip install --pre azure-ai-ml
 # </az_ml_sdk_install>
 
 # <mldesigner_install>
@@ -9,7 +9,7 @@ pip install mldesigner
 # </mldesigner_install>
 
 # <az_ml_sdk_test_install>
-# pip install azure-ai-ml==0.0.63075866 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
+# pip install azure-ai-ml==0.0.65807702 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 # </az_ml_sdk_test_install>
 
 pip list
