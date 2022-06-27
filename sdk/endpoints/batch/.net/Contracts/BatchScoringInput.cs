c23d0c85fbb2a5b6224cb32c283287f40414dc31
Mathieu St-Louis <58675685+mastloui@users.noreply.github.com>
2022-06-27 15:34:06 -0700

    Add .NET sample for invoking batch endpoint

---
+++ sdk/endpoints/batch/.net/Contracts/BatchScoringInput.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="BatchScoringInput.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace BatchInferencingSamples
+{
+    public class BatchScoringInput
+    {
+        /// <summary>
+        /// The type of input, either or file or a folder.
+        /// </summary>
+        public JobInputType JobInputType { get; set; }
+
+        /// <summary>
+        /// The URI, either an AML datastore, asset or open dataset.
+        /// </summary>
+        public Uri? Uri { get; set; }
+    }
+}
---
+++ sdk/endpoints/batch/.net/Contracts/BatchScoringProperties.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="BatchScoringProperties.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace BatchInferencingSamples
+{
+    internal class BatchScoringProperties
+    {
+        /// <summary>
+        /// Settings of the source data.
+        /// </summary>
+        public Dictionary<string, BatchScoringInput> InputData { get; set; } = new Dictionary<string, BatchScoringInput>();
+        
+        /// <summary>
+        /// Settings of the output data.
+        /// </summary>
+        public Dictionary<string, BatchScoringOutput> OutputData { get; set; } = new Dictionary<string, BatchScoringOutput>();
+    }
+}
