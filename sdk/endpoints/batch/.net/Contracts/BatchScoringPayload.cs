1983921ba03336ac41696d290404dccd1f85007e
Mathieu St-Louis <58675685+mastloui@users.noreply.github.com>
2022-06-27 16:04:33 -0700

    Adding .net samples for batch inferencing

---
+++ sdk/endpoints/batch/.net/Contracts/BatchScoringPayload.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="BatchScoringPayload.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace BatchInferencingSamples
+{
+    internal class BatchScoringPayload
+    {
+        /// <summary>
+        /// Scoring properties leveraged by the inferencing job.
+        /// </summary>
+        public BatchScoringProperties Properties { get; set; } = new BatchScoringProperties();
+    }
+}
---
+++ sdk/endpoints/batch/.net/Contracts/JobOutputType.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="JobOutputType.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+using System.Text.Json.Serialization;
+
+namespace BatchInferencingSamples
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum JobOutputType
+    {
+        UriFile
+    }
+}
