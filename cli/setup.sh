c38ba3ddc267eb65d6386e79af91304dd4ce8486
Ankit Singhal <30610298+singankit@users.noreply.github.com>
2022-07-12 14:05:44 -0700

    Update setup.sh

--- cli/setup.sh
+++ cli/setup.sh
@@ -5,7 +5,7 @@
 # rc install - uncomment and adjust below to run all tests on a CLI release candidate
 az extension remove -n ml
 
-# Use a daily build
+# Use a daily build for test
 az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2-public/ml-2.6.0-py3-none-any.whl --yes
  
 # <set_variables>
