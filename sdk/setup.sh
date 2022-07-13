19bba02b09785c1db50698212487cb4984ee9bc6
Clement Wang <clwan@microsoft.com>
2022-07-13 23:23:55 +0800

    update

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -9,7 +9,7 @@ pip install mldesigner==0.0.67229541 --extra-index-url https://azuremlsdktestpyp
 # </mldesigner_install>
 
 # <az_ml_sdk_test_install>
-pip install azure-ai-ml==0.0.67208580 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
+pip install azure-ai-ml==0.0.67235317 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 # </az_ml_sdk_test_install>
 
 pip list
