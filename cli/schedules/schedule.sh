269a34e28cb0ecfaad51143844f77376fcae9ada
lochen <cloga0216@gmail.com>
2022-08-02 18:23:35 +0800

    remove file prefix, add no-wait for async

--- cli/schedules/schedule.sh
+++ cli/schedules/schedule.sh
@@ -1,28 +1,29 @@
 # <create_schedule>
-az ml schedule create --file cron-schedule.yml
+# This action will create related resources for a schedule. It will take dozens of seconds to complete.
+az ml schedule create --file cron-schedule.yml --no-wait
 # </create_schedule>
 
 # <show_schedule>
-az ml schedule show -n schedule_name
+az ml schedule show -n simple_cron_schedule
 # </show_schedule>
 
 # <update_schedule>
-az ml schedule update -n schedule_name  --set create_job = 'azureml:job_name'
+az ml schedule update -n simple_cron_schedule  --set description="new description" --no-wait
 # </update_schedule>
 
 # <disable_schedule>
-az ml schedule disable -n schedule_name
+az ml schedule disable -n simple_cron_schedule --no-wait
 # </disable_schedule>
 
 # <enable_schedule>
-az ml schedule disable -n schedule_name
+az ml schedule enable -n simple_cron_schedule --no-wait
 # </enable_schedule>
 
 # <query_triggered_jobs>
-# query triggered jobs from schedule, please replace the <schedule name> to real schedule name
-az ml job list --query "[?contains(display_name,'<schedule_name>')]" 
+# query triggered jobs from schedule, please replace the simple_cron_schedule to your schedule name
+az ml job list --query "[?contains(display_name,'simple_cron_schedule')]" 
 # </query_triggered_jobs>
 
 # <delete_schedule>
-az ml schedule delete -n schedule_name
+az ml schedule delete -n simple_cron_schedule
 # </delete_schedule>
