43b99b45508ed523305d8d3cd658d419a29ebb31
jeff-shepherd <39775772+jeff-shepherd@users.noreply.github.com>
2022-08-03 13:33:38 -0700

    Updated actions to only run if the notebook folder changes rather than if any notebook changes (#1538)

--- sdk/readme.py
+++ sdk/readme.py
@@ -107,9 +107,10 @@ on:\n"""
         if is_pipeline_notebook:
             workflow_yaml += "      - pipeline/*\n"
     workflow_yaml += f"""    paths:
-      - sdk/**
+      - sdk/{posix_folder}/**
       - .github/workflows/sdk-{classification}-{name}.yml
       - sdk/dev-requirements.txt
+      - sdk/setup.sh
 jobs:
   build:
     runs-on: ubuntu-latest
