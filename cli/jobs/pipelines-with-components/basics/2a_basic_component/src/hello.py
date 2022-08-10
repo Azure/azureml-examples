0c2cb16118bdbc5b5a81d0dbf7e6fc3438b728c7
lochen <cloga0216@gmail.com>
2022-08-10 21:29:25 +0800

    update bool input test

--- cli/jobs/pipelines-with-components/basics/2a_basic_component/src/hello.py
+++ cli/jobs/pipelines-with-components/basics/2a_basic_component/src/hello.py
@@ -1,1 +1,11 @@
-print("Hello Python World")
+import argparse
+from pathlib import Path
+from uuid import uuid4
+from datetime import datetime
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--bool_input", type=bool, help="A boolean input")
+args = parser.parse_args()
+print(args.bool_input)
+print(type(args.bool_input))
