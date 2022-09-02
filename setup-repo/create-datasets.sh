dba699accc9930f7dffc8b60d3c326e14e5b0c8f
Harneet Virk <harnvir@microsoft.com>
2022-09-02 16:39:41 -0700

    Refactoring some of the existing scipts

--- setup-repo/create-datasets.sh
+++ setup-repo/create-datasets.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
 # this dataset is needed for the sample under cli/jobs/pipelines-with-components/basics/4d_dataset_input
 
-az ml data create -f ../cli/jobs/pipelines-with-components/basics/4d_dataset_input/data.yml
+# The filename of this script for help messages
+SCRIPT_PATH="${BASH_SOURCE[0]:-$0}"
+SCRIPT_DIR="$( cd "$( dirname "${SCRIPT_PATH}" )" && pwd )"
+ROOT_DIR=$(cd "${SCRIPT_DIR}/../" && pwd)
 
-
+az ml data create -f "${ROOT_DIR}"/cli/jobs/pipelines-with-components/basics/4d_dataset_input/data.yml
 
 # <download_untar_cifar>
 
@@ -15,13 +19,11 @@ tar -xvzf cifar-10-python.tar.gz -C data
 # </download_untar_cifar>
 
 
-
 # <create_cifar>
 
 az ml data create --name cifar-10-example --version 1 --set path=data
 
 # </create_cifar>
-
 
 
 # <cleanup_cifar>
@@ -32,9 +34,8 @@ rm -r data
 
 # </cleanup_cifar>
 
-
 # <create_rai_data>
-az ml data create -f ../cli/jobs/pipelines-with-components/rai_pipeline_adult_analyse/data/data_adult_test.yaml
-az ml data create -f ../cli/jobs/pipelines-with-components/rai_pipeline_adult_analyse/data/data_adult_train.yaml
+az ml data create -f "${ROOT_DIR}"/cli/jobs/pipelines-with-components/rai_pipeline_adult_analyse/data/data_adult_test.yaml
+az ml data create -f "${ROOT_DIR}"/cli/jobs/pipelines-with-components/rai_pipeline_adult_analyse/data/data_adult_train.yaml
 
 # </create_rai_data>
