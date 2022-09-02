4c8130085d7fef8c4e6ac6bf6b541ddcf75ae460
Harneet Virk <harnvir@microsoft.com>
2022-09-02 12:18:28 -0700

    Updating all of the CLI and SDK workflows

--- cli/setup.sh
+++ cli/setup.sh
@@ -1,9 +1,29 @@
-# <az_ml_install>
-# az extension add -n ml -y
-# </az_ml_install>
 
 # rc install - uncomment and adjust below to run all tests on a CLI release candidate
 # az extension remove -n ml
 
 # Use a daily build
 # az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2-public/ml-2.6.0-py3-none-any.whl --yes
+ 
+# <set_variables>
+GROUP="azureml-examples"
+LOCATION="eastus"
+WORKSPACE="main"
+# </set_variables>
+
+if [[ $(az group exists --name $GROUP -o tsv) = true ]]
+then
+    # Installing extension temporarily assuming the run is on old subscription
+    # without bootstrap script.
+
+    # <az_ml_install>
+    az extension add -n ml -y
+    # </az_ml_install>
+
+    # <az_configure_defaults>
+    az configure --defaults group=$GROUP workspace=$WORKSPACE location=$LOCATION
+    # </az_configure_defaults>
+    echo "Default resource group set to $GROUP"
+else
+    echo "Specified resource group doesn't exist in the current subscription"
+fi
