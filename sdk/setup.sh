e68bd98fc10e58162ccdedc11a64af5c88a72ceb
Harneet Virk <harnvir@microsoft.com>
2022-09-07 11:53:38 -0700

    Setting up the release branch for examples repo

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # <az_ml_sdk_install>
-pip install --pre azure-ai-ml
+# pip install --pre azure-ai-ml
 # </az_ml_sdk_install>
 
 # <mldesigner_install>
@@ -14,7 +14,8 @@ pip install mltable
 
 
 # <az_ml_sdk_test_install>
-pip install azure-ai-ml==0.1.0.b6
+# pip install azure-ai-ml==0.1.0.b6
+pip install https://docsupport.blob.core.windows.net/ml-sample-submissions/1836020/azure_ai_ml-0.1.0b7-py3-none-any.whl
 # </az_ml_sdk_test_install>
 
 pip list
