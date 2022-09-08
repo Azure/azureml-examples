fb830ba4b0d6021973c31b50bcc48d6041ea3bfc
Korin <0mza987@gmail.com>
2022-09-08 12:08:41 +0800

    Update mldesigner

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -5,7 +5,7 @@ pip install --pre azure-ai-ml
 # </az_ml_sdk_install>
 
 # <mldesigner_install>
-pip install mldesigner
+pip install mldesigner==0.0.71435212 --extra-index-url=https://azuremlsdktestpypi.azureedge.net/test-sdk-cli-v2/
 # </mldesigner_install>
 
 # <mltable_install>
@@ -14,7 +14,7 @@ pip install mltable
 
 
 # <az_ml_sdk_test_install>
-pip install azure-ai-ml==0.1.0.b6
+pip install https://docsupport.blob.core.windows.net/ml-sample-submissions/1836020/azure_ai_ml-0.1.0b7-py3-none-any.whl
 # </az_ml_sdk_test_install>
 
 pip list
