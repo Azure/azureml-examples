d191a7e1f304fde2b59217a8d1c8d597a9f834d2
Alexander Wallace <v-alwallace@microsoft.com>
2022-09-09 20:26:09 +0000

    Initial commit

--- cli/endpoints/online/custom-container/r/scripts/plumber.R
+++ cli/endpoints/online/custom-container/r/scripts/plumber.R
@@ -1,6 +1,11 @@
 # plumber.R
 
-accident_model <- readRDS("model.rds")
+model_dir <- Sys.getenv("AZUREML_MODEL_DIR")
+
+model_names <- tolower(LETTERS[1:3])
+load_model <- function(model_name){readRDS(paste0(model_dir,"/models/",model_name,".rds"))}
+models <- lapply(model_names, load_model)
+names(models) <- model_names
 
 #* Liveness check
 #* @get /live
@@ -14,26 +19,14 @@ function() {
   "ready"
 }
 
-#* Returns likelihood of fatality given information about a car accident
-#* @param dvcat, velocity, possible values "1-9km/h" "10-24"   "25-39"   "40-54"   "55+"  
-#* @param seatbelt, possible values "none" "belted"  
-#* @param frontal, possible values "notfrontal" "frontal"
-#* @param sex, possible values "f" "m"
-#* @param ageOfocc, possible values age in years, 16-97
-#* @param yearVeh, year of vehicle, possible values 1955-2003
-#* @param airbag, was an airbag present? possible values "none" "airbag"
-#* @param occRole, possible values "driver" "pass" 
+#* @param model
+#* @param x1
+#* @param x2 
 #* @post /score
-function(dvcat, seatbelt, frontal, sex, ageOfocc, yearVeh, airbag, occRole) {
+function(model, x1, x2) {
   newdata <- data.frame(
-    dvcat=dvcat,  
-    seatbelt=seatbelt,
-    frontal=frontal,
-    sex=sex,
-    ageOFocc=as.integer(ageOfocc),
-    yearVeh=as.integer(yearVeh),
-    airbag=airbag,
-    occRole=occRole
+    x1=x1, 
+    x2=x2
     )
-  as.numeric(predict(accident_model, newdata, type="response") * 100)
+  as.numeric(predict(models[[model]], newdata, type="response"))
 }
