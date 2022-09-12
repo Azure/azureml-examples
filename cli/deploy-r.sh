9bed7c20fad7c4054513038576dfdad6021fa95e
Alex Wallace <80542152+xanwal@users.noreply.github.com>
2022-09-12 10:55:31 -0700

    Update R Custom Container (#1629)
    * Initial commit
    
    * Uncomment cleanup
    
    * Pre-archive model so runner will run
    
    * Change model name for runner

--- cli/deploy-r.sh
+++ cli/deploy-r.sh
@@ -7,9 +7,6 @@ export ENDPOINT_NAME="<YOUR_ENDPOINT_NAME>"
 
 export ENDPOINT_NAME=endpt-r-`echo $RANDOM`
 
-# Download model
-wget https://aka.ms/r-model -O $BASE_PATH/scripts/model.rds
-
 # Get name of workspace ACR, build image
 WORKSPACE=$(az config get --query "defaults[?name == 'workspace'].value" -o tsv)
 ACR_NAME=$(az ml workspace show -n $WORKSPACE --query container_registry -o tsv | cut -d'/' -f9-)
@@ -22,7 +19,7 @@ fi
 
 az acr login -n $ACR_NAME
 IMAGE_TAG=${ACR_NAME}.azurecr.io/r_server
-az acr build $BASE_PATH -f $BASE_PATH/Dockerfile -t $IMAGE_TAG -r $ACR_NAME
+az acr build $BASE_PATH -f $BASE_PATH/r.dockerfile -t $IMAGE_TAG -r $ACR_NAME
 
 # Clean up utility
 cleanup(){
@@ -32,16 +29,19 @@ cleanup(){
 }
 
 # Run image locally for testing
-docker run --rm -d -p 8000:8000 \
-    -v $PWD/$BASE_PATH/scripts:/var/azureml-app/azureml-models/plumber/1/scripts \
-    --name="r_server" $IMAGE_TAG
-
+docker run --rm -d -p 8000:8000 -v "$PWD/$BASE_PATH/scripts:/var/azureml-app" \
+  -v "$PWD/$BASE_PATH/models":"/var/azureml-models/models" \
+  -e AZUREML_MODEL_DIR=/var/azureml-models \
+  -e AML_APP_ROOT=/var/azureml-app \
+  -e AZUREML_ENTRY_SCRIPT=plumber.R \
+  --name="r_server" $IMAGE_TAG
+    
 sleep 10
 
 # Check liveness, readiness, scoring locally
 curl "http://localhost:8000/live"
 curl "http://localhost:8000/ready"
-curl -H "Content-Type: application/json" --data @$BASE_PATH/sample_request.json http://localhost:8000/score
+curl -d @$BASE_PATH/sample_request.json -H 'Content-Type: application/json' http://localhost:8000/score
 
 docker stop r_server
 
@@ -80,12 +80,11 @@ fi
 
 # Test remotely
 echo "Testing endpoint"
-for i in {1..10}
+for model in {a..c}
 do
-   RESPONSE=$(az ml online-endpoint invoke -n $ENDPOINT_NAME --request-file $BASE_PATH/sample_request.json)
+  echo "Model $model tested successfully, response was $RESPONSE."
+  RESPONSE=$(az ml online-endpoint invoke -n $ENDPOINT_NAME --request-file <(jq --arg m "$model" '.model=$m' $BASE_PATH/sample_request.json))
 done
-
-echo "Tested successfully, response was $RESPONSE. Cleaning up..."
 
 # Clean up
 cleanup
