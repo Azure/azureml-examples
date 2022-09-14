467e954074831fe4ec5eb383d7cfc2be357bafd4
Harneet Virk <harnvir@microsoft.com>
2022-09-14 09:56:50 -0700

    Adding logging

--- sdk/readme.py
+++ sdk/readme.py
@@ -148,7 +148,8 @@ jobs:
           source "{github_workspace}/infra/sdk_helpers.sh";
           source "{github_workspace}/infra/init_environment.sh";
           bash "{github_workspace}/infra/sdk_helpers.sh" generate_workspace_config "../../.azureml/config";
-          bash [[ -f "../../.azureml/config" ]] && cat "../../.azureml/config";"""
+          pwd;
+          [ -f "../../.azureml/config" ] && cat "../../.azureml/config";"""
     if is_pipeline_notebook:
         # pipeline-job uses different cred
         cred_replace = f"""
