fd04721bece01080af35d77e673f4bb6492ab896
Harneet Virk <harnvir@microsoft.com>
2022-09-13 18:24:15 -0700

    Adding helper scripts for resource provisioning

--- infra/verify_prerequisites.sh
+++ infra/verify_prerequisites.sh
@@ -7,4 +7,4 @@
 command -v az >/dev/null 2>&1 || { echo_warning "azure cli but it's not installed. See https://docs.microsoft.com/cli/azure/?view=azure-cli-latest/. Aborting."; exit 1; }
 command -v jq >/dev/null 2>&1 || { echo_warning "jq is required but not installed. See https://stedolan.github.io/jq/.  Aborting."; exit 1; }
 type sed >/dev/null 2>&1 || { echo >&2 "Error: You do not have 'sed' installed."; exit 1; }
-command -v sed >/dev/null 2>&1 || { echo_warning "You do not have 'sed' installed.  Aborting."; exit 1; }
+# command -v kubectl >/dev/null 2>&1 || { echo >&2 "'kubectl' is required but not installed. Aborting."; exit 1; }
