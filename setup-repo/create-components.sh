977f412e35ff0b779de3cbefa554a51e4e32d088
Harneet Virk <harnvir@microsoft.com>
2022-09-16 14:46:20 -0700

    Reverting the set-up repo changes

--- setup-repo/create-components.sh
+++ setup-repo/create-components.sh
@@ -1,17 +1,7 @@
-#!/bin/bash
 # these components are needed for the sample under cli/jobs/pipelines-with-components/basics/1b_e2e_registered_components
 
-# The filename of this script for help messages
-SCRIPT_PATH="${BASH_SOURCE[0]:-$0}"
-SCRIPT_DIR="$( cd "$( dirname "${SCRIPT_PATH}" )" && pwd )"
-ROOT_DIR=$(cd "${SCRIPT_DIR}/../" && pwd)
+az ml component create --file ../cli/jobs/pipelines-with-components/basics/1b_e2e_registered_components/train.yml
 
-pushd "$ROOT_DIR" > /dev/null
+az ml component create --file ../cli/jobs/pipelines-with-components/basics/1b_e2e_registered_components/score.yml
 
-az ml component create --file "./cli/jobs/pipelines-with-components/basics/1b_e2e_registered_components/train.yml"
-
-az ml component create --file "./cli/jobs/pipelines-with-components/basics/1b_e2e_registered_components/score.yml"
-
-az ml component create --file "./cli/jobs/pipelines-with-components/basics/1b_e2e_registered_components/eval.yml"
-
-popd > /dev/null
+az ml component create --file ../cli/jobs/pipelines-with-components/basics/1b_e2e_registered_components/eval.yml
