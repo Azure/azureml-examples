08d2b1552bfd5948d91e8768207e67ff5d3b5588
Harneet Virk <harnvir@microsoft.com>
2022-09-16 14:46:20 -0700

    Reverting the set-up repo changes

--- setup-repo/create-datasets.sh
+++ setup-repo/create-datasets.sh
@@ -1,14 +1,8 @@
-#!/bin/bash
 # this dataset is needed for the sample under cli/jobs/pipelines-with-components/basics/4d_dataset_input
 
-# The filename of this script for help messages
-SCRIPT_PATH="${BASH_SOURCE[0]:-$0}"
-SCRIPT_DIR="$( cd "$( dirname "${SCRIPT_PATH}" )" && pwd )"
-ROOT_DIR=$(cd "${SCRIPT_DIR}/../" && pwd)
+az ml data create -f ../cli/jobs/pipelines-with-components/basics/4d_dataset_input/data.yml
 
-pushd "$ROOT_DIR" > /dev/null
 
-az ml data create -f "./cli/jobs/pipelines-with-components/basics/4d_data_input/data.yml"
 
 # <download_untar_cifar>
 
@@ -21,14 +15,13 @@ tar -xvzf cifar-10-python.tar.gz -C data
 # </download_untar_cifar>
 
 
+
 # <create_cifar>
 
-
-# This step times out. May need to use az copy to upload the dataset - TODO
-# az ml data create --name cifar-10-example --version 1 --set path=data
-
+az ml data create --name cifar-10-example --version 1 --set path=data
 
 # </create_cifar>
+
 
 
 # <cleanup_cifar>
@@ -37,14 +30,11 @@ rm cifar-10-python.tar.gz
 
 rm -r data
 
-
 # </cleanup_cifar>
 
-# <create_rai_data>
 
-az ml data create -f "./cli/jobs/pipelines-with-components/rai_pipeline_adult_analyse/data/data_adult_test.yaml"
-az ml data create -f "./cli/jobs/pipelines-with-components/rai_pipeline_adult_analyse/data/data_adult_train.yaml"
+# <create_rai_data>
+az ml data create -f ../cli/jobs/pipelines-with-components/rai_pipeline_adult_analyse/data/data_adult_test.yaml
+az ml data create -f ../cli/jobs/pipelines-with-components/rai_pipeline_adult_analyse/data/data_adult_train.yaml
 
 # </create_rai_data>
-
-popd > /dev/null
--- setup-repo/update-datasets.sh
+++ setup-repo/update-datasets.sh
@@ -1,14 +1,8 @@
-#!/bin/bash
 # setup datastore name
 
-# The filename of this script for help messages
-SCRIPT_PATH="${BASH_SOURCE[0]:-$0}"
-SCRIPT_DIR="$( cd "$( dirname "${SCRIPT_PATH}" )" && pwd )"
-ROOT_DIR=$(cd "${SCRIPT_DIR}/../" && pwd)
-
-pushd "$ROOT_DIR" > /dev/null
-
 datastore="workspaceblobstore"
+
+
 
 # query datastore
 
@@ -21,23 +15,21 @@ container=$(az ml datastore show -n $datastore --query container_name -o tsv)
 container=$(echo $container|tr -d '\r')
 
 
+
 # replace storage account and container names in the YAML files
 
-chmod u+x "./cli/**/*.yml"
-sed -i 's/account-name/'"$account"'/g' "./cli/assets/data/cloud-folder-https.yml"
+sed -i 's/account-name/'"$account"'/g' ../cli/assets/dataset/cloud-folder-https.yml
 
-sed -i 's/container-name/'"$container"'/g' "./cli/assets/data/cloud-folder-https.yml"
+sed -i 's/container-name/'"$container"'/g' ../cli/assets/dataset/cloud-folder-https.yml
 
-sed -i 's/account-name/'"$account"'/g' "./cli/assets/data/cloud-file-https.yml"
+sed -i 's/account-name/'"$account"'/g' ../cli/assets/dataset/cloud-file-https.yml
 
-sed -i 's/container-name/'"$container"'/g' "./cli/assets/data/cloud-file-https.yml"
+sed -i 's/container-name/'"$container"'/g' ../cli/assets/dataset/cloud-file-https.yml
 
-sed -i 's/account-name/'"$account"'/g' "./cli/assets/data/cloud-folder-wasbs.yml"
+sed -i 's/account-name/'"$account"'/g' ../cli/assets/dataset/cloud-folder-wasbs.yml
 
-sed -i 's/container-name/'"$container"'/g' "./cli/assets/data/cloud-folder-wasbs.yml"
+sed -i 's/container-name/'"$container"'/g' ../cli/assets/dataset/cloud-folder-wasbs.yml
 
-sed -i 's/account-name/'"$account"'/g' "./cli/assets/data/cloud-file-wasbs.yml"
+sed -i 's/account-name/'"$account"'/g' ../cli/assets/dataset/cloud-file-wasbs.yml
 
-sed -i 's/container-name/'"$container"'/g' "./cli/assets/data/cloud-file-wasbs.yml"
-
-popd > /dev/null
+sed -i 's/container-name/'"$container"'/g' ../cli/assets/dataset/cloud-file-wasbs.yml
