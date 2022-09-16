105a5926a451382fdea6d99ccc798b3aa1b15d90
Ayush Mishra <61145377+novaturient95@users.noreply.github.com>
2022-09-16 22:59:36 +0530

    fix fomatting

--- cli/jobs/pipelines/automl/image-instance-segmentation-task-fridge-items-pipeline/components/src/register.py
+++ cli/jobs/pipelines/automl/image-instance-segmentation-task-fridge-items-pipeline/components/src/register.py
@@ -31,23 +31,23 @@ def parse_args():
 
     # parse args
     args = parser.parse_args()
-    print("Path: "+ args.model_input_path)
+    print("Path: " + args.model_input_path)
     # return args
     return args
 
 
 def main(args):
-    '''
+    """
     Register Model Example
-    '''
-    #Set Tracking URI
+    """
+    # Set Tracking URI
     current_experiment = Run.get_context().experiment
     tracking_uri = current_experiment.workspace.get_mlflow_tracking_uri()
     print("tracking_uri: {0}".format(tracking_uri))
     mlflow.set_tracking_uri(tracking_uri)
     mlflow.set_experiment(current_experiment.name)
 
-    #Get Run ID from model path
+    # Get Run ID from model path
     print("Getting model path")
     mlmodel_path = os.path.join(args.model_input_path, "MLmodel")
     runid = ""
@@ -56,15 +56,14 @@ def main(args):
             if "run_id" in line:
                 runid = line.split(":")[1].strip()
 
-    #Construct Model URI from run ID extract previously
+    # Construct Model URI from run ID extract previously
     model_uri = "runs:/{}/outputs/".format(runid)
     print("Model URI: " + model_uri)
 
-    #Register the model with Model URI and Name of choice
+    # Register the model with Model URI and Name of choice
     registered_name = args.model_base_name
     print(f"Registering model as {registered_name}")
     mlflow.register_model(model_uri, registered_name)
-
 
 
 # run script
@@ -74,4 +73,3 @@ if __name__ == "__main__":
 
     # run main function
     main(args)
-    
