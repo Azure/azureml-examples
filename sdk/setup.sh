fcb341ebf7d1878cf1d572301f5969adb077e9fc
azure-sdk <azuresdk@microsoft.com>
2022-09-19 23:39:48 +0000

    Update sdk/setup.sh to target fresh built azure-ai-ml wheel.

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # <az_ml_sdk_install>
-pip install --pre azure-ai-ml
 # </az_ml_sdk_install>
 
 # <mldesigner_install>
@@ -15,7 +14,7 @@ pip install pandas
 
 
 # <az_ml_sdk_test_install>
-pip install azure-ai-ml==0.1.0.b6
+pip install https://docsupport.blob.core.windows.net/ml-sample-submissions/1860282/azure_ai_ml-0.1.0b7-py3-none-any.whl
 # </az_ml_sdk_test_install>
 
 pip list
