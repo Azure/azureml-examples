f7a4f99eab106353b3f4fa4bdf743195f7341e7c
Harneet Virk <harnvir@microsoft.com>
2022-09-19 16:44:54 -0700

    Updating the SDK candidate and merging changes from main

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -16,7 +16,7 @@ pip install pandas
 
 # <az_ml_sdk_test_install>
 # pip install azure-ai-ml==0.1.0.b6
-pip install https://docsupport.blob.core.windows.net/ml-sample-submissions/1842015/azure_ai_ml-0.1.0b7-py3-none-any.whl
+pip install https://docsupport.blob.core.windows.net/ml-sample-submissions/1860282/azure_ai_ml-0.1.0b7-py3-none-any.whl
 # </az_ml_sdk_test_install>
 
 pip list
