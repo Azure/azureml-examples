ac3a85e0482af450144a7e16ca75fe8130b31456
Harneet Virk <harnvir@microsoft.com>
2022-09-19 19:28:08 -0700

    Archieve the model while running endpoint workflows and create vnets

--- cli/deploy-custom-container-multimodel-minimal.sh
+++ cli/deploy-custom-container-multimodel-minimal.sh
@@ -13,12 +13,12 @@ export ACR_NAME=$(az ml workspace show --query container_registry -o tsv | cut -
 # <setup_build_directory> 
 export BASE_PATH=endpoints/online/custom-container/multimodel-minimal
 mkdir -p $BASE_PATH/{code,build,test-data}
-cp "$(dirname $BASE_PATH)/{multimodel-minimal*} $BASE_PATH/" 
-mv $BASE_PATH/multmodel-minimal.dockerfile $BASE_PATH/build/
-mv $BASE_PATH/multmodel-minimal-score.py $BASE_PATH/code/
+cp "$(dirname $BASE_PATH)/{multimodel-minimal*}" "$BASE_PATH/"
+mv "$BASE_PATH/multmodel-minimal.dockerfile" "$BASE_PATH/build/"
+mv "$BASE_PATH/multmodel-minimal-score.py" "$BASE_PATH/code/"
 # </setup_build_directory> 
 
-cd $BASE_PATH
+cd "$BASE_PATH"
 
 # <generate_models_and_test_data> 
 python multimodel-minimal-train.py
@@ -27,15 +27,15 @@ python multimodel-minimal-train.py
 cd - 
 
 # <build_image> 
-az acr build -t azureml-examples/minimal-multimodel:1 -r $ACR_NAME -f $BASE_PATH/build/multmodel-minimal.dockerfile $BASE_PATH/build 
+az acr build -t azureml-examples/minimal-multimodel:1 -r "$ACR_NAME" -f "$BASE_PATH/build/multmodel-minimal.dockerfile" "$BASE_PATH/build"
 # </build_image> 
 
 # <create_endpoint> 
-az online-endpoint create -n $ENDPOINT_NAME -f $BASE_PATH/multimodel-minimal-endpoint.yml
+az online-endpoint create -n "$ENDPOINT_NAME" -f "$BASE_PATH/multimodel-minimal-endpoint.yml"
 # </create_endpoint> 
 
 # Check if endpoint was successful
-endpoint_status=`az ml online-endpoint show --name $ENDPOINT_NAME --query "provisioning_state" -o tsv`
+endpoint_status=`az ml online-endpoint show --name "$ENDPOINT_NAME" --query "provisioning_state" -o tsv`
 echo $endpoint_status
 if [[ $endpoint_status == "Succeeded" ]]
 then
@@ -46,11 +46,11 @@ else
 fi
 
 # <create_deployment> 
-az online-endpoint create -e $ENDPOINT_NAME -f $BASE_PATH/multimodel-minimal-deployment.yml
+az online-endpoint create -e "$ENDPOINT_NAME" -f "$BASE_PATH/multimodel-minimal-deployment.yml"
 # </create_deployment> 
 
 # Check if deployment was successful 
-deploy_status=`az ml online-deployment show --name blue --endpoint $ENDPOINT_NAME --query "provisioning_state" -o tsv`
+deploy_status=`az ml online-deployment show --name blue --endpoint "$ENDPOINT_NAME" --query "provisioning_state" -o tsv`
 echo $deploy_status
 if [[ $deploy_status == "Succeeded" ]]
 then
@@ -62,11 +62,11 @@ fi
 
 # Get key
 echo "Getting access key..."
-KEY=$(az ml online-endpoint get-credentials -n $ENDPOINT_NAME --query primaryKey -o tsv)
+KEY=$(az ml online-endpoint get-credentials -n "$ENDPOINT_NAME" --query primaryKey -o tsv)
 
 # Get scoring url
 echo "Getting scoring url..."
-SCORING_URL=$(az ml online-endpoint show -n $ENDPOINT_NAME --query scoring_uri -o tsv)
+SCORING_URL=$(az ml online-endpoint show -n "$ENDPOINT_NAME" --query scoring_uri -o tsv)
 echo "Scoring url is $SCORING_URL"
 
 # <test_online_endpoints> 
@@ -75,5 +75,5 @@ curl -H "Authorization: Bearer $KEY" -H "Content-Type: application/json" -d @"$B
 # </test_online_endpoints> 
 
 # <delete_online_endpoint>
-az ml online-endpoint delete -y -n $ENDPOINT_NAME
+az ml online-endpoint delete -y -n "$ENDPOINT_NAME"
 # </delete_online_endpoint>
