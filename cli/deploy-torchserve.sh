c3c6b772be65b440bcd61d00206daf3730611ff9
Shohei Nagata <7954174+shohei1029@users.noreply.github.com>
2022-09-22 18:13:23 +0900

    change links to test files

--- cli/deploy-torchserve.sh
+++ cli/deploy-torchserve.sh
@@ -1,3 +1,5 @@
+set -e
+
 BASE_PATH=endpoints/online/custom-container
 AML_MODEL_NAME=torchserve-densenet161
 echo $AML_MODEL_NAME
@@ -9,17 +11,15 @@ DEPLOYMENT_NAME=torchserve-deployment
 # Download model and config file
 echo "Downling model and config file..."
 mkdir $BASE_PATH/torchserve
-wget --progress=dot:mega https://aka.ms/torchserve-densenet161 -O $BASE_PATH/torchserve/densenet161.mar
-wget --progress=dot:mega https://aka.ms/torchserve-config -O $BASE_PATH/torchserve/config.properties
+wget --progress=dot:mega https://stamldata.blob.core.windows.net/public/densenet161.mar -O $BASE_PATH/torchserve/densenet161.mar
 
 # Get name of workspace ACR, build image
 WORKSPACE=$(az config get --query "defaults[?name == 'workspace'].value" -o tsv)
 ACR_NAME=$(az ml workspace show --name $WORKSPACE --query container_registry -o tsv | cut -d'/' -f9-)
 
-if [[ $ACR_NAME == "" ]]
-then
-    echo "ACR login failed, exiting"
-    exit 1
+if [[ $ACR_NAME == "" ]]; then
+  echo "ACR login failed, exiting"
+  exit 1
 fi
 
 az acr login -n $ACR_NAME
@@ -28,14 +28,14 @@ az acr build $BASE_PATH/ -f $BASE_PATH/torchserve.dockerfile -t $IMAGE_TAG -r $A
 
 # Run image locally for testing
 docker run --rm -d -p 8080:8080 --name torchserve-test \
-    -e MODEL_BASE_PATH=$MODEL_BASE_PATH \
-    -v $PWD/$BASE_PATH/torchserve:$MODEL_BASE_PATH/torchserve $IMAGE_TAG
+  -e MODEL_BASE_PATH=$MODEL_BASE_PATH \
+  -v $PWD/$BASE_PATH/torchserve:$MODEL_BASE_PATH/torchserve $IMAGE_TAG
 
 sleep 10
 
-cleanTestingFiles(){
-    rm -r $BASE_PATH/torchserve
-    rm kitten_small.jpg
+cleanTestingFiles() {
+  rm -r $BASE_PATH/torchserve
+  rm kitten_small.jpg
 }
 
 # Check Torchserve health
@@ -44,7 +44,7 @@ curl http://localhost:8080/ping
 
 # Download test image
 echo "Downloading test image..."
-wget https://aka.ms/torchserve-test-image -O kitten_small.jpg
+wget https://raw.githubusercontent.com/pytorch/serve/master/examples/image_classifier/kitten.jpg -O kitten_small.jpg #Todo: change to aka.ms link
 
 # Check scoring locally
 echo "Uploading testing image, the scoring is..."
@@ -57,8 +57,7 @@ sed -i 's/{{acr_name}}/'$ACR_NAME'/' $BASE_PATH/$DEPLOYMENT_NAME.yml
 
 EXISTS=$(az ml online-endpoint show -n $ENDPOINT_NAME --query name -o tsv)
 # Update endpoint if exists, else create
-if [[ $EXISTS == $ENDPOINT_NAME ]]
-then 
+if [[ $EXISTS == $ENDPOINT_NAME ]]; then
   az ml online-endpoint update -f $BASE_PATH/$ENDPOINT_NAME.yml
 else
   az ml online-endpoint create -f $BASE_PATH/$ENDPOINT_NAME.yml
@@ -67,8 +66,7 @@ fi
 ENDPOINT_STATUS=$(az ml online-endpoint show --name $ENDPOINT_NAME --query "provisioning_state" -o tsv)
 echo "Endpoint status is $ENDPOINT_STATUS"
 
-if [[ $ENDPOINT_STATUS == "Succeeded" ]]
-then  
+if [[ $ENDPOINT_STATUS == "Succeeded" ]]; then
   echo "Endpoint created successfully"
 else
   echo "Something went wrong when creating endpoint. Cleaning up..."
@@ -80,10 +78,9 @@ fi
 echo "Creating deployment..."
 az ml online-deployment create --name $DEPLOYMENT_NAME --endpoint $ENDPOINT_NAME --file $BASE_PATH/$DEPLOYMENT_NAME.yml --all-traffic
 
-deploy_status=`az ml online-deployment show --name $DEPLOYMENT_NAME --endpoint $ENDPOINT_NAME --query "provisioning_state" -o tsv`
+deploy_status=$(az ml online-deployment show --name $DEPLOYMENT_NAME --endpoint $ENDPOINT_NAME --query "provisioning_state" -o tsv)
 echo $deploy_status
-if [[ $deploy_status == "Succeeded" ]]
-then
+if [[ $deploy_status == "Succeeded" ]]; then
   echo "Deployment completed successfully"
 else
   echo "Deployment failed"
