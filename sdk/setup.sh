b827dee0124518bf4ecff2b5965f7779e983a30e
Brynn Yin <biyi@microsoft.com>
2022-09-23 17:40:24 +0800

    Fix setup
    Signed-off-by: Brynn Yin <biyi@microsoft.com>

--- sdk/setup.sh
+++ sdk/setup.sh
@@ -8,6 +8,11 @@ pip install --pre azure-ai-ml
 pip install mldesigner
 # </mldesigner_install>
 
+# <mltable_install>
+pip install mltable
+pip install pandas
+# </mltable_install>
+
 # <az_ml_sdk_test_install>
 # pip install azure-ai-ml==0.0.63075866 --extra-index-url https://azuremlsdktestpypi.azureedge.net/sdk-cli-v2
 # </az_ml_sdk_test_install>
