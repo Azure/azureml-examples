5de802c1e9f94feececb6faf5dd7f1bad0c63433
nick863 <30440255+nick863@users.noreply.github.com>
2022-10-06 22:19:07 -0700

    Fixes

--- sdk/python/jobs/automl-standalone-jobs/automl-forecasting-task-energy-demand/metrics_helper.py
+++ sdk/python/jobs/automl-standalone-jobs/automl-forecasting-task-energy-demand/metrics_helper.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
 
 def APE(actual, pred):
     """
@@ -20,3 +22,19 @@ def MAPE(actual, pred):
     actual_safe = actual[not_na & not_zero]
     pred_safe = pred[not_na & not_zero]
     return np.mean(APE(actual_safe, pred_safe))
+
+
+def calculate_metrics(actual, pred):
+    not_na = ~(np.isnan(actual) | np.isnan(pred))
+    actual_safe = actual[not_na]
+    pred_safe = pred[not_na]
+    rmse = np.sqrt(mean_squared_error(actual_safe, pred_safe))
+    metrics_dict = {}
+    metrics_dict["R2 score"] = r2_score(actual_safe, pred_safe)
+    metrics_dict["mean absolute error"] = mean_absolute_error(actual_safe, pred_safe)
+    metrics_dict["mean_absolute_percentage_error"] = MAPE(actual_safe, pred_safe)
+    metrics_dict["root mean squared error"] = rmse
+    metrics_dict["normalized root mean squared error"] = rmse / np.abs(
+        actual_safe.max() - actual_safe.min()
+    )
+    return pd.DataFrame(metrics_dict.items(), columns=["metric name", "score"])
