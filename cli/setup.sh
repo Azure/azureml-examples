d21b5b2228fb87aae771a6db500ff0100d6c03d7
Harneet Virk <harnvir@microsoft.com>
2022-10-11 11:09:43 -0700

    USe publicly available artifacts for SDK and CLI

--- cli/setup.sh
+++ cli/setup.sh
@@ -1,12 +1,12 @@
 # <az_ml_install>
-# az extension add -n ml -y
+az extension add -n ml -y
 # </az_ml_install>
 
 # rc install - uncomment and adjust below to run all tests on a CLI release candidate
 # az extension remove -n ml
 
 # Use a daily build
-az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2-public/ml-2.10.0-py3-none-any.whl --yes
+# az extension add --source https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2-public/ml-2.10.0-py3-none-any.whl --yes
  
 # <set_variables>
 GROUP="azureml-examples"
