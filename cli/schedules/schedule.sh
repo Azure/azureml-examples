bcbe782617c0967c10bbc9eb6afb374d97bf3450
Cloga Chen <cloga0216@gmail.com>
2022-10-13 07:20:33 +0800

    Update schedule.sh
    change the cli name

--- cli/schedules/schedule.sh
+++ cli/schedules/schedule.sh
@@ -4,7 +4,7 @@ az ml schedule create --file cron-schedule.yml --no-wait
 # </create_schedule>
 
 # <show_schedule>
-az ml schedule show -n simple_cron_schedule
+az ml schedule show -n simple_cron_job_schedule
 # </show_schedule>
 
 # <list_schedule>
@@ -12,22 +12,22 @@ az ml schedule list
 # </list_schedule>
 
 # <update_schedule>
-az ml schedule update -n simple_cron_schedule  --set description="new description" --no-wait
+az ml schedule update -n simple_cron_job_schedule  --set description="new description" --no-wait
 # </update_schedule>
 
 # <disable_schedule>
-az ml schedule disable -n simple_cron_schedule --no-wait
+az ml schedule disable -n simple_cron_job_schedule --no-wait
 # </disable_schedule>
 
 # <enable_schedule>
-az ml schedule enable -n simple_cron_schedule --no-wait
+az ml schedule enable -n simple_cron_job_schedule --no-wait
 # </enable_schedule>
 
 # <query_triggered_jobs>
-# query triggered jobs from schedule, please replace the simple_cron_schedule to your schedule name
+# query triggered jobs from schedule, please replace the simple_cron_job_schedule to your schedule name
 az ml job list --query "[?contains(display_name,'simple_cron_schedule')]" 
 # </query_triggered_jobs>
 
 # <delete_schedule>
-az ml schedule delete -n simple_cron_schedule
+az ml schedule delete -n simple_cron_job_schedule
 # </delete_schedule>
