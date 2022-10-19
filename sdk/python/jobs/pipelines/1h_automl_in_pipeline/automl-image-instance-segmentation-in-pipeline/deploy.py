907beb530d467efd857a6745c54688efa1a75212
HrishikeshGeedMS <hgeed@microsoft.com>
2022-10-19 10:56:09 +0530

    code reformatted

--- sdk/python/jobs/pipelines/1h_automl_in_pipeline/automl-image-instance-segmentation-in-pipeline/deploy.py
+++ sdk/python/jobs/pipelines/1h_automl_in_pipeline/automl-image-instance-segmentation-in-pipeline/deploy.py
@@ -7,17 +7,26 @@ import datetime
 from pathlib import Path
 
 from azure.ai.ml import MLClient
-from azure.ai.ml.entities import ManagedOnlineEndpoint,ManagedOnlineDeployment,ProbeSettings
+from azure.ai.ml.entities import (
+    ManagedOnlineEndpoint,
+    ManagedOnlineDeployment,
+    ProbeSettings,
+)
 from azure.identity import ManagedIdentityCredential
+
 
 def parse_args():
     # setup arg parser
     parser = argparse.ArgumentParser()
 
     # add arguments
-    parser.add_argument("--model_id_path", type=str, help="Path where registered model id is saved")
     parser.add_argument(
-        "--endpoint_name", type=str, help="Name of the the endpoint where model needs to be deployed"
+        "--model_id_path", type=str, help="Path where registered model id is saved"
+    )
+    parser.add_argument(
+        "--endpoint_name",
+        type=str,
+        help="Name of the the endpoint where model needs to be deployed",
     )
     parser.add_argument(
         "--deployment_name", type=str, help="Name of the the deployment"
@@ -27,6 +36,7 @@ def parse_args():
     print("args received ", args)
     # return args
     return args
+
 
 def get_endpoint(endpoint_name):
     # Creating a unique endpoint name with current datetime to avoid conflicts
@@ -39,20 +49,20 @@ def get_endpoint(endpoint_name):
         auth_mode="key",
         tags={"foo": "bar"},
     )
-    print("Endpoint created with name ",endpoint.name)
+    print("Endpoint created with name ", endpoint.name)
     return endpoint
+
 
 def get_ml_client():
     # returns ML client by autherizing credentials via MSI
     credential = ManagedIdentityCredential(client_id="<MSI_CLIENT_ID>")
     ml_client = MLClient(
-         credential, 
-        "<SUBSCRIPTION_ID>",
-        "<RESOURCE_GROUP>",
-        "<AML_WORKSPACE_NAME>")
-    
+        credential, "<SUBSCRIPTION_ID>", "<RESOURCE_GROUP>", "<AML_WORKSPACE_NAME>"
+    )
+
     return ml_client
-    
+
+
 def main(args):
     """
     Register Model Example
@@ -60,39 +70,40 @@ def main(args):
     ml_client = get_ml_client()
     registered_model_id = (Path(args.model_id_path) / "reg_id.txt").read_text()
     endpoint = get_endpoint(args.endpoint_name)
-    
+
     ml_client.begin_create_or_update(endpoint)
     # deployment
-    
+
     deployment = ManagedOnlineDeployment(
-    name=args.deployment_name,
-    endpoint_name=endpoint.name,
-    model=registered_model_id,
-    instance_type="Standard_NC6s_v3",
-    instance_count=1,
-    liveness_probe=ProbeSettings(
-        failure_threshold=30,
-        success_threshold=1,
-        timeout=2,
-        period=10,
-        initial_delay=2000,
-    ),
-    readiness_probe=ProbeSettings(
-        failure_threshold=10,
-        success_threshold=1,
-        timeout=10,
-        period=10,
-        initial_delay=2000,
-    ),
+        name=args.deployment_name,
+        endpoint_name=endpoint.name,
+        model=registered_model_id,
+        instance_type="Standard_NC6s_v3",
+        instance_count=1,
+        liveness_probe=ProbeSettings(
+            failure_threshold=30,
+            success_threshold=1,
+            timeout=2,
+            period=10,
+            initial_delay=2000,
+        ),
+        readiness_probe=ProbeSettings(
+            failure_threshold=10,
+            success_threshold=1,
+            timeout=10,
+            period=10,
+            initial_delay=2000,
+        ),
     )
     ml_client.online_deployments.begin_create_or_update(deployment)
-    
-    print("Deployment done with name ",deployment.name)
-    #deployment to take 100% traffic
-    
+
+    print("Deployment done with name ", deployment.name)
+    # deployment to take 100% traffic
+
     endpoint.traffic = {deployment.name: 100}
     ml_client.begin_create_or_update(endpoint)
-    
+
+
 # run script
 if __name__ == "__main__":
     # parse args
