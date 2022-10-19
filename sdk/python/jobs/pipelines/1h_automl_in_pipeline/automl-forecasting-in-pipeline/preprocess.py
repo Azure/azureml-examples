ea4a6d3a15529547a51b50b84323f95a4be5dc4e
HrishikeshGeedMS <hgeed@microsoft.com>
2022-10-19 10:52:01 +0530

    formatting done

--- sdk/python/jobs/pipelines/1h_automl_in_pipeline/automl-forecasting-in-pipeline/preprocess.py
+++ sdk/python/jobs/pipelines/1h_automl_in_pipeline/automl-forecasting-in-pipeline/preprocess.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import yaml
 from mltable import load
 
+
 def parse_args():
     # setup arg parser
     parser = argparse.ArgumentParser()
@@ -17,12 +18,14 @@ def parse_args():
     # return args
     return args
 
+
 def get_preprocessed_data(dataframe):
     """
     Do preprocessing as needed
     Currently we are just passing pandas dataframe as it is
     """
     return dataframe
+
 
 def main(args):
     """
@@ -31,20 +34,24 @@ def main(args):
     train_data_table = load(args.train_data)
     train_dataframe = train_data_table.to_pandas_dataframe()
     preprocessed_train_dataframe = get_preprocessed_data(train_dataframe)
-    
+
     # write preprocessed train data in output path
-    preprocessed_train_dataframe.to_csv(args.preprocess_train_data+'/nyc_energy_training_clean.csv',index = False, header = True)
+    preprocessed_train_dataframe.to_csv(
+        args.preprocess_train_data + "/nyc_energy_training_clean.csv",
+        index=False,
+        header=True,
+    )
 
     # Write MLTable yaml file as well in output folder
     # Since in this example we are not doing any preprocessing, we are just copying same yaml file from input,change it if needed
-   
+
     # read and write MLModel yaml file for train data
-    with open(args.train_data+'/MLTable','r') as file:
+    with open(args.train_data + "/MLTable", "r") as file:
         yaml_file = yaml.safe_load(file)
-    with open(args.preprocess_train_data+'/MLTable', 'w') as file:
+    with open(args.preprocess_train_data + "/MLTable", "w") as file:
         yaml.dump(yaml_file, file)
 
-        
+
 # run script
 if __name__ == "__main__":
     # parse args
--- sdk/python/jobs/pipelines/1h_automl_in_pipeline/automl-text-classification-multilabel-paper-categorization-in-pipeline/preprocess.py
+++ sdk/python/jobs/pipelines/1h_automl_in_pipeline/automl-text-classification-multilabel-paper-categorization-in-pipeline/preprocess.py
@@ -4,24 +4,22 @@ from pathlib import Path
 import yaml
 from mltable import load
 
+
 def parse_args():
     # setup arg parser
     parser = argparse.ArgumentParser()
 
     # add arguments
     parser.add_argument("--train_data", type=str)
-    parser.add_argument(
-        "--validation_data", type=str
-    )
+    parser.add_argument("--validation_data", type=str)
     parser.add_argument("--preprocess_train_data", type=str)
-    parser.add_argument(
-        "--preprocess_validation_data", type=str
-    )
+    parser.add_argument("--preprocess_validation_data", type=str)
     # parse args
     args = parser.parse_args()
     print("args received ", args)
     # return args
     return args
+
 
 def get_preprocessed_data(dataframe):
     """
@@ -30,6 +28,7 @@ def get_preprocessed_data(dataframe):
     """
     return dataframe
 
+
 def main(args):
     """
     Preprocessing of training/validation data
@@ -37,34 +36,37 @@ def main(args):
     train_data_table = load(args.train_data)
     train_dataframe = train_data_table.to_pandas_dataframe()
     preprocessed_train_dataframe = get_preprocessed_data(train_dataframe)
-    
+
     # write preprocessed train data in output path
-    preprocessed_train_dataframe.to_csv(args.preprocess_train_data+'/train_data.csv',index = False, header = True)
-    
+    preprocessed_train_dataframe.to_csv(
+        args.preprocess_train_data + "/train_data.csv", index=False, header=True
+    )
+
     validation_data_table = load(args.validation_data)
     validation_dataframe = validation_data_table.to_pandas_dataframe()
     preprocessed_validation_dataframe = get_preprocessed_data(validation_dataframe)
 
     # write preprocessed validation data in output path
-    preprocessed_validation_dataframe.to_csv(args.preprocess_validation_data+'/valid_data.csv',index = False, header = True)
-    
-    
+    preprocessed_validation_dataframe.to_csv(
+        args.preprocess_validation_data + "/valid_data.csv", index=False, header=True
+    )
+
     # Write MLTable yaml file as well in output folder
     # Since in this example we are not doing any preprocessing, we are just copying same yaml file from input,change it if needed
-   
+
     # read and write MLModel yaml file for train data
-    with open(args.train_data+'/MLTable','r') as file:
+    with open(args.train_data + "/MLTable", "r") as file:
         yaml_file = yaml.safe_load(file)
-    with open(args.preprocess_train_data+'/MLTable', 'w') as file:
+    with open(args.preprocess_train_data + "/MLTable", "w") as file:
         yaml.dump(yaml_file, file)
-    
-    
+
     # read and write MLModel yaml file for validation data
-    with open(args.validation_data+'/MLTable','r') as file:
+    with open(args.validation_data + "/MLTable", "r") as file:
         yaml_file = yaml.safe_load(file)
-    with open(args.preprocess_validation_data+'/MLTable', 'w') as file:
+    with open(args.preprocess_validation_data + "/MLTable", "w") as file:
         yaml.dump(yaml_file, file)
-        
+
+
 # run script
 if __name__ == "__main__":
     # parse args
