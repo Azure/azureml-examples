eaddd6dab5c1abf5dbaf8f43de2ff5e11c67d240
Alexander Wallace <v-alwallace@microsoft.com>
2022-10-20 03:15:26 +0000

    Test

--- cli/deploy-test1.sh
+++ cli/deploy-test1.sh
@@ -2,6 +2,8 @@ GROUP=$(az config get --query "defaults[?name == 'group'].value" -o tsv)
 KV_NAME="kv${RANDOM}"
 az keyvault create -n $KV_NAME -g $GROUP
 
-az keyvault --vault-name $KV_NAME -n foo --value bar
+az keyvault secret set --vault-name $KV_NAME -n foo --value bar
 
 az keyvault scret show --vault-name $KV_NAME -n foo 
+
+az keyvault delete -n $KV_NAME
