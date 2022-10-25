b820e6e666b9bfaa8bb901c7c8f4a8340a19156a
Alexander Wallace <v-alwallace@microsoft.com>
2022-10-25 10:13:22 +0000

    Formatting

--- cli/endpoints/online/managed/binary-payloads/code/multi-file-to-json-score.py
+++ cli/endpoints/online/managed/binary-payloads/code/multi-file-to-json-score.py
@@ -1,13 +1,16 @@
 from azureml.contrib.services.aml_request import AMLRequest, rawhttp
 from PIL import Image
 
-def init(): 
-    pass 
+
+def init():
+    pass
+
 
 @rawhttp
-def run(req : AMLRequest):
-    sizes = [{"filename" : f.filename,
-        "size" : Image.open(f.stream).size}
-        for f in req.files.getlist("file[]")]
+def run(req: AMLRequest):
+    sizes = [
+        {"filename": f.filename, "size": Image.open(f.stream).size}
+        for f in req.files.getlist("file[]")
+    ]
 
-    return {"response" : sizes}
+    return {"response": sizes}
--- cli/endpoints/online/managed/binary-payloads/code/single-file-to-file-score.py
+++ cli/endpoints/online/managed/binary-payloads/code/single-file-to-file-score.py
@@ -3,26 +3,28 @@ from azureml.contrib.services.aml_response import AMLResponse
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 from PIL import Image
-import io 
+import io
 
 default_resize = (128, 128)
 
-def init(): 
-    pass 
+
+def init():
+    pass
+
 
 @rawhttp
-def run(req : AMLRequest):
+def run(req: AMLRequest):
     try:
         data = req.files.getlist("file")[0]
     except IndexError:
         return AMLResponse("No file uploaded", status_code=422)
-    
+
     img = Image.open(data.stream)
     img = img.resize(default_resize)
 
     output = io.BytesIO()
     img.save(output, format="JPEG")
-    resp = AMLResponse(message = output.getvalue(), status_code=200)
+    resp = AMLResponse(message=output.getvalue(), status_code=200)
     resp.mimetype = "image/jpg"
 
     return resp
