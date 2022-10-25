d0ac0bbcfeae4c2c609b0c188f22f0cc087b5b8f
Alexander Wallace <v-alwallace@microsoft.com>
2022-10-25 10:07:42 +0000

    Init

---
+++ cli/endpoints/online/managed/binary-payloads/code/multi-file-to-json-score.py
@@ -0,0 +1,13 @@
+from azureml.contrib.services.aml_request import AMLRequest, rawhttp
+from PIL import Image
+
+def init(): 
+    pass 
+
+@rawhttp
+def run(req : AMLRequest):
+    sizes = [{"filename" : f.filename,
+        "size" : Image.open(f.stream).size}
+        for f in req.files.getlist("file[]")]
+
+    return {"response" : sizes}
