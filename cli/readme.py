2535b56e04a9ce07f362707daa1acaa6c931d033
Chuyuan Qu <chuyuanqu@microsoft.com>
2022-10-26 11:04:12 -0700

    ran readme.py, edited readme.py, edited workflow

--- cli/readme.py
+++ cli/readme.py
@@ -14,6 +14,7 @@ EXCLUDED_RESOURCES = [
     "instance",
     "connections",
     "compute/cluster-user-identity",
+    "registry"
 ]
 EXCLUDED_ASSETS = ["conda-yamls", "mlflow-models"]
 EXCLUDED_SCHEDULES = []
