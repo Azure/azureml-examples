734cd3e8d73f48d37e68b355c346dbd059ef2ee1
Alexander Wallace <v-alwallace@microsoft.com>
2022-10-31 09:31:55 +0000

    Format black

--- cli/endpoints/online/managed/openapi/decorated/code/score.py
+++ cli/endpoints/online/managed/openapi/decorated/code/score.py
@@ -5,7 +5,10 @@ import numpy as np
 import joblib
 from inference_schema.schema_decorators import input_schema, output_schema
 from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
-from inference_schema.parameter_types.standard_py_parameter_type import StandardPythonParameterType
+from inference_schema.parameter_types.standard_py_parameter_type import (
+    StandardPythonParameterType,
+)
+
 
 def init():
     global model
@@ -16,15 +19,12 @@ def init():
     model = joblib.load(model_path)
     logging.info("Init complete")
 
+
 @input_schema(
     param_name="data",
-    param_type=NumpyParameterType(np.array([[1,2,3,4,5,6,7,8,9,10]]))
+    param_type=NumpyParameterType(np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])),
 )
-@output_schema(
-    output_type=StandardPythonParameterType(
-        {"output": [1.0, 1.0]}
-    )
-)
+@output_schema(output_type=StandardPythonParameterType({"output": [1.0, 1.0]}))
 def run(data):
     logging.info("model 1: request received")
     result = model.predict(data)
