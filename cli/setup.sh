a82978dd40bfc0859502c982ee34fefb0da79e3e
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2022-10-31 22:20:01 -0700

    Update setup files with new versions

--- cli/setup.sh
+++ cli/setup.sh
@@ -3,7 +3,7 @@
 # az extension remove -n ml
 
 # <az_ml_install>
-# az extension add -n ml -y
+az extension add -n ml -y
 # </az_ml_install>
 
 # Use a daily build
@@ -17,7 +17,7 @@ if az extension show -n ml &>/dev/null; then
     echo -n 'Re-installing ml...'
 fi
 
-if ! az extension add --yes --source "https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2-public/ml-2.10.0-py3-none-any.whl" -o none --only-show-errors &>/dev/null; then
+if ! az extension add --yes --source "https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-cli-v2-public/ml-2.11.0-py3-none-any.whl" -o none --only-show-errors &>/dev/null; then
     echo 'Error failed to install ml azure-cli extension' >&2
     exit 1
 fi
