3692bc2761b0948ae907fd15845133cd9cc9ee22
bupt-wenxiaole <506539136@qq.com>
2022-11-04 15:12:37 +0800

    black reformat

--- cli/jobs/pipelines/mnist-batch-identification-using-parallel/script/prepare_data.py
+++ cli/jobs/pipelines/mnist-batch-identification-using-parallel/script/prepare_data.py
@@ -5,19 +5,19 @@ import idx2numpy
 import urllib.request
 from PIL import Image
 
-parser = argparse.ArgumentParser(
-        allow_abbrev=False, description="parse user arguments"
-    )
+parser = argparse.ArgumentParser(allow_abbrev=False, description="parse user arguments")
 parser.add_argument("--output_folder", type=str, default=0)
 
 args, _ = parser.parse_known_args()
 
-data_folder = os.path.join(args.output_folder, 'mnist')
+data_folder = os.path.join(args.output_folder, "mnist")
 os.makedirs(data_folder, exist_ok=True)
-urllib.request.urlretrieve('https://azureopendatastorage.blob.core.windows.net/mnist/t10k-images-idx3-ubyte.gz',
-                           filename=os.path.join(os.getcwd(), 'test-images.gz'))
+urllib.request.urlretrieve(
+    "https://azureopendatastorage.blob.core.windows.net/mnist/t10k-images-idx3-ubyte.gz",
+    filename=os.path.join(os.getcwd(), "test-images.gz"),
+)
 
-file_handler = gzip.open('test-images.gz', 'r')
+file_handler = gzip.open("test-images.gz", "r")
 imagearray = idx2numpy.convert_from_file(file_handler)
 
 # Choose the first 1000 images and save to the output folder.
