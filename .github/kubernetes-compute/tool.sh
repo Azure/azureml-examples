641564aa23cc76c24be59898ad0a9f91404709d9
Zeliang Tian <zetia@microsoft.com>
2022-11-06 13:27:28 +0800

    fix basic workflow

--- .github/kubernetes-compute/tool.sh
+++ .github/kubernetes-compute/tool.sh
@@ -91,8 +91,9 @@ install_tools(){
     curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl \
     && chmod +x ./kubectl  \
     && sudo mv ./kubectl /usr/local/bin/kubectl  
-
+    
     pip install azureml-core 
+    pip install shyaml
 
     pip list || true
     az version || true
