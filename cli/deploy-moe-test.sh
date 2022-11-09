b3564782f8906c98db0be69874fb6bdb65fedef7
Alexander Wallace <v-alwallace@microsoft.com>
2022-11-09 22:29:08 +0000

    Functional role creation test

--- cli/deploy-moe-test.sh
+++ cli/deploy-moe-test.sh
@@ -6,6 +6,7 @@ IDENTITY_CLIENTID=$(az identity create -n $ID_NAME --query "clientId" -o tsv)
 RESOURCE_GROUP_ID=$(az group show --name "${GROUP}" --query id -o tsv | tail -n1 | tr -d "[:cntrl:]")
 az role assignment create --assignee-object-id $IDENTITY_CLIENTID --assignee-principal-type ServicePrincipal --role "Contributor" --scope $RESOURCE_GROUP_ID
 
+az identity delete -n $ID_NAME 
 
 # az ml compute create -f endpoints/online/managed/profiler/compute.yml \
 #     --set name=$PROFILER_COMPUTE_NAME
