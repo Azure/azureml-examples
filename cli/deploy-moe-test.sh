e145237b662ab7be0dd768bb03744da8e42ceb7d
Alexander Wallace <v-alwallace@microsoft.com>
2022-11-09 22:50:42 +0000

    test clientid

--- cli/deploy-moe-test.sh
+++ cli/deploy-moe-test.sh
@@ -2,4 +2,5 @@ ID_NAME="foo${RANDOM}"
 
 GROUP=$(az config get --query "defaults[?name == 'group'].value" -o tsv)
 
+RESOURCE_GROUP_ID=$(az group show --name "${GROUP}" --query id -o tsv | tail -n1 | tr -d "[:cntrl:]")
 az role assignment create --assignee-object-id $CLIENT_ID --assignee-principal-type ServicePrincipal --role "Contributor" --scope $RESOURCE_GROUP_ID
