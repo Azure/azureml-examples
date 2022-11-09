6d855f6faf63fb38ada92ec506ebc6eefe69345a
Alexander Wallace <v-alwallace@microsoft.com>
2022-11-09 22:38:01 +0000

    Test clientid

--- cli/deploy-moe-test.sh
+++ cli/deploy-moe-test.sh
@@ -1,12 +1,5 @@
 ID_NAME="foo${RANDOM}" 
 
 GROUP=$(az config get --query "defaults[?name == 'group'].value" -o tsv)
-IDENTITY_CLIENTID=$(az identity create -n $ID_NAME --query "clientId" -o tsv)
 
-RESOURCE_GROUP_ID=$(az group show --name "${GROUP}" --query id -o tsv | tail -n1 | tr -d "[:cntrl:]")
-az role assignment create --assignee-object-id $IDENTITY_CLIENTID --assignee-principal-type ServicePrincipal --role "Contributor" --scope $RESOURCE_GROUP_ID
-
-az identity delete -n $ID_NAME 
-
-# az ml compute create -f endpoints/online/managed/profiler/compute.yml \
-#     --set name=$PROFILER_COMPUTE_NAME
+az role assignment create --assignee-object-id $CLIENT_ID --assignee-principal-type ServicePrincipal --role "Contributor" --scope $RESOURCE_GROUP_ID
