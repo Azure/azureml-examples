91fac220d4084c7b67ccffd7e48878218d385c35
Alexander Wallace <v-alwallace@microsoft.com>
2022-11-10 00:31:51 +0000

    test keys

--- cli/deploy-moe-test.sh
+++ cli/deploy-moe-test.sh
@@ -4,6 +4,7 @@ GROUP=$(az config get --query "defaults[?name == 'group'].value" -o tsv)
 
 RESOURCE_GROUP_ID=$(az group show --name "${GROUP}" --query id -o tsv | tail -n1 | tr -d "[:cntrl:]")
 
-echo $AZURE_CREDENTIALS | jq 'keys'
+echo $(echo $AZURE_CREDENTIALS | jq 'keys')
+echo $(echo $AZURE_CREDS | jq 'keys')
 
 az role assignment create --assignee-object-id $CLIENT_ID --assignee-principal-type ServicePrincipal --role "Contributor" --scope $RESOURCE_GROUP_ID
