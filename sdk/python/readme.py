04228f3a14303ec0c4be358ecb5e28c292261aa7
HrishikeshGeedMS <hgeed@microsoft.com>
2022-11-10 13:41:31 +0530

    Kernel spec by running readme.py

--- sdk/python/readme.py
+++ sdk/python/readme.py
@@ -238,7 +238,6 @@ jobs:
 
     if workflow_yaml != workflow_before:
         # write workflow
-        print("hrishi", workflow_file)
         with open(workflow_file, "w") as f:
             f.write(workflow_yaml)
 
