298ee23984d1b718b2f79a5ba2195959f7a1978f
Facundo Santiago <fasantia@microsoft.com>
2022-11-30 19:39:21 +0000

    fix: CI

--- sdk/python/endpoints/batch/imagenet-classifier-mlflow/code/module_loader.py
+++ sdk/python/endpoints/batch/imagenet-classifier-mlflow/code/module_loader.py
@@ -1,7 +1,9 @@
 
+import pandas as pd
+import tensorflow as tf
+
 class TfClassifier():
     def __init__(self, model_path: str, labels_path: str):
-        import tensorflow as tf
         import numpy as np
         from tensorflow.keras.models import load_model
         
@@ -9,13 +11,18 @@ class TfClassifier():
         self.imagenet_labels = np.array(open(labels_path).read().splitlines())
 
     def predict(self, data):
+
         preds = self.model.predict(data)
 
         pred_prob = tf.reduce_max(preds, axis=-1)
         pred_class = tf.argmax(preds, axis=-1)
         pred_label = [self.imagenet_labels[pred] for pred in pred_class]
 
-        return pd.DataFrame([pred_class, pred_prob, pred_label], columns=["class", "probability", "label"])
+        return pd.DataFrame({
+            "class": pred_class, 
+            "probability": pred_prob,
+            "label": pred_label
+        })
 
 def _load_pyfunc(data_path: str):
     import os
