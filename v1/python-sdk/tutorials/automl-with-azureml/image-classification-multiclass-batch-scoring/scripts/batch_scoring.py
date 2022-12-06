e71d7db626445d1e1998c5b8f7c99eb2393ec8b5
Radu-Mihai Dondera <rdondera@microsoft.com>
2022-12-06 15:58:05 -0800

    Unbreak scoring code and data (fix bad merge).

--- v1/python-sdk/tutorials/automl-with-azureml/image-classification-multiclass-batch-scoring/scripts/batch_scoring.py
+++ v1/python-sdk/tutorials/automl-with-azureml/image-classification-multiclass-batch-scoring/scripts/batch_scoring.py
@@ -8,16 +8,26 @@ import json
 from azureml.core.model import Model
 from azureml.automl.core.shared import logging_utilities
 
-from azureml.automl.dnn.vision.common.logging_utils import get_logger
-from azureml.automl.dnn.vision.common.model_export_utils import (
-    load_model,
-    run_inference_batch,
-)
-from azureml.automl.dnn.vision.classification.inference.score import (
-    _score_with_model,
-)
-from azureml.automl.dnn.vision.common.utils import _set_logging_parameters
-
+try:
+    from azureml.automl.dnn.vision.common.logging_utils import get_logger
+    from azureml.automl.dnn.vision.common.model_export_utils import (
+        load_model,
+        run_inference_batch,
+    )
+    from azureml.automl.dnn.vision.classification.inference.score import (
+        _score_with_model,
+    )
+    from azureml.automl.dnn.vision.common.utils import _set_logging_parameters
+except ImportError:
+    from azureml.contrib.automl.dnn.vision.common.logging_utils import get_logger
+    from azureml.contrib.automl.dnn.vision.common.model_export_utils import (
+        load_model,
+        run_inference_batch,
+    )
+    from azureml.contrib.automl.dnn.vision.classification.inference.score import (
+        _score_with_model,
+    )
+    from azureml.contrib.automl.dnn.vision.common.utils import _set_logging_parameters
 
 TASK_TYPE = "image-classification"
 logger = get_logger("azureml.automl.core.scoring_script_images")
@@ -26,8 +36,6 @@ logger = get_logger("azureml.automl.core.scoring_script_images")
 def init():
     global model
     global batch_size
-    global model_explainability
-    global xai_parameters
 
     # Set up logging
     _set_logging_parameters(TASK_TYPE, {})
@@ -37,15 +45,10 @@ def init():
     )
     parser.add_argument("--model_name", dest="model_name", required=True)
     parser.add_argument("--batch_size", dest="batch_size", type=int, required=False)
-    parser.add_argument("--model_explainability", dest="model_explainability",
-                        type=bool, default=False, required=False)
-    parser.add_argument("--xai_parameters", dest="xai_parameters",
-                        type=str, default=json.dumps(dict()), required=False)
     args, _ = parser.parse_known_args()
 
     batch_size = args.batch_size
-    model_explainability = args.model_explainability
-    xai_parameters = json.loads(args.xai_parameters)
+
     model_path = os.path.join(Model.get_model_path(args.model_name), "model.pt")
     print(model_path)
 
@@ -61,11 +64,6 @@ def init():
 
 def run(mini_batch):
     logger.info("Running inference.")
-    result = run_inference_batch(model,
-                                 mini_batch,
-                                 _score_with_model,
-                                 batch_size,
-                                 model_explainability,
-                                 **xai_parameters)
+    result = run_inference_batch(model, mini_batch, _score_with_model, batch_size)
     logger.info("Finished inferencing.")
     return result
