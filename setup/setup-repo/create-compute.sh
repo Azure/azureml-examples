3326ca9dff8aab2c67010abf4990662375e67067
HrishikeshGeedMS <hgeed@microsoft.com>
2023-01-02 00:12:29 +0530

    code for compute added

--- setup/setup-repo/create-compute.sh
+++ setup/setup-repo/create-compute.sh
@@ -4,6 +4,8 @@ az ml compute create -n cpu-cluster --type amlcompute --min-instances 0 --max-in
 
 az ml compute create -n gpu-cluster --type amlcompute --min-instances 0 --max-instances 4 --size Standard_NC12
 
+az ml compute create -n cpu-cluster-hri --type amlcompute --min-instances 0 --max-instances 8 --identity-type UserAssigned --user-assigned-identities /subscriptions/72c03bf3-4e69-41af-9532-dfcdc3eefef4/resourcegroups/static-test-resources/providers/Microsoft.ManagedIdentity/userAssignedIdentities/aml-training-build-managed-identity
+
 # </create_computes>
 
 
