54997747a2620f291317fbe3e3919ba7083d81ec
Cassie Esvelt <cassieesvelt@gmail.com>
2023-01-17 16:26:32 -0800

    add latest tag to env

--- cli/jobs/deepspeed/deepspeed-training/src/create-job.sh
+++ cli/jobs/deepspeed/deepspeed-training/src/create-job.sh
@@ -12,7 +12,7 @@ command: bash start-deepspeed.sh --force_multi train.py --with_aml_log=True --de
 
 experiment_name: DistributedJob-DeepsSpeed-Training-cifar
 code: .
-environment: azureml:AzureML-ACPT-pytorch-1.12-py39-cuda11.6-gpu
+environment: azureml:AzureML-ACPT-pytorch-1.12-py39-cuda11.6-gpu@latest
 environment_variables:
   AZUREML_COMPUTE_USE_COMMON_RUNTIME: 'True'
   AZUREML_COMMON_RUNTIME_USE_INTERACTIVE_CAPABILITY: 'True'
