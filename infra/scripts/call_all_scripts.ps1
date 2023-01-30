872f9baf9efa14289a51c809d4718b1a6bf56374
Harneet Virk <harnvir@microsoft.com>
2023-01-30 14:34:24 -0800

    Fix the path

--- infra/scripts/call_all_scripts.ps1
+++ infra/scripts/call_all_scripts.ps1
@@ -2,4 +2,4 @@
 # All the post cleanup scripts are called from this file                                           #
 ####################################################################################################
 
-./remove_role_assignments.ps1 -ResourceScope "/subscriptions/$env:SUBSCRIPTION_ID/resourceGroups/$env:RESOURCE_GROUP_NAME"
+./scripts/remove_role_assignments.ps1 -ResourceScope "/subscriptions/$env:SUBSCRIPTION_ID/resourceGroups/$env:RESOURCE_GROUP_NAME"
