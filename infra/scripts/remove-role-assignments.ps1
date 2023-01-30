4863a0c35e075974bced5b8569bf81dfa2e56070
Harneet Virk <harnvir@microsoft.com>
2023-01-30 11:46:22 -0800

    Add role assignment cleanup

---
+++ infra/scripts/remove-role-assignments.ps1
@@ -0,0 +1,33 @@
+####################################################################################################
+# This script allows you to remove stale role assignments listed as 'Identity not found'           #
+####################################################################################################
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ResourceScope
+)
+function Get-RoleAssignmentCount {
+    Write-Output "Determing unknown Role Assignment count..."
+    $stale = Get-AzRoleAssignment -Scope "$ResourceScope" | Where-Object { $_.ObjectType -eq 'Unknown'}
+    $unknownRoleAssignmentCount = $stale.Count
+    Write-Output "Total Unknown Role Assignment Count: $unknownRoleAssignmentCount..."
+
+    return $unknownRoleAssignmentCount
+}
+
+try
+{
+    Write-Output "Pre-checking the RoleAssignment count..."
+    Get-RoleAssignmentCount
+    Get-AzRoleAssignment -Scope "$ResourceScope" | Where-Object { $_.ObjectType -eq 'Unknown'} | Remove-AzRoleAssignment
+    Write-Output "Check the Role Assignment count after cleanup"
+    Get-RoleAssignmentCount
+    Write-Output "Role Assignment clean-up complete."
+}
+catch
+{
+    Write-Error "There was an issue cleaning the . See details: $($_.ErrorDetails.Message)"
+    # Write-Error $_.Exception.Message
+    # Exit 1
+}
