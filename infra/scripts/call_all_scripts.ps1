3aebd48b780d192419639cb3d8b23636f4b4dd19
Harneet Virk <harnvir@microsoft.com>
2023-01-30 21:09:49 -0800

    Update the path

--- infra/scripts/call_all_scripts.ps1
+++ infra/scripts/call_all_scripts.ps1
@@ -2,4 +2,4 @@
 # All the post cleanup scripts are called from this file                                           #
 ####################################################################################################
 
-scripts/remove_role_assignments.ps1 "/subscriptions/$env:SUBSCRIPTION_ID/resourceGroups/$env:RESOURCE_GROUP_NAME"
+&"$PSScriptroot\remove_role_assignments.ps1" "/subscriptions/$env:SUBSCRIPTION_ID/resourceGroups/$env:RESOURCE_GROUP_NAME"
