45c4bbf8240894517a25fe6b9800e898a9c09e6b
Harneet Virk <harnvir@microsoft.com>
2023-01-31 21:39:20 -0800

    Validate azure-ai-ml SDK-1.4.0 and ML-2.14.0 wheels

--- sdk/python/setup.sh
+++ sdk/python/setup.sh
@@ -16,8 +16,8 @@ pip install pandas
 
 # <az_ml_sdk_test_install>
 # pip install azure-ai-ml==0.1.0.b8
-pip install azure-ai-ml
-# https://docsupport.blob.core.windows.net/ml-sample-submissions/1905732/azure_ai_ml-1.0.0-py3-none-any.whl
+# pip install azure-ai-ml
+pip install https://docsupport.blob.core.windows.net/ml-sample-submissions/2153284/azure_ai_ml-1.4.0-py3-none-any.whl
 # </az_ml_sdk_test_install>
 
 pip list
