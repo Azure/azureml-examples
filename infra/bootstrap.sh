87400c9b82cdf02bf6db9e2babbd8ea7ade8cb03
HrishikeshGeedMS <hgeed@microsoft.com>
2023-02-02 18:08:23 +0530

    code updated for reg/dep

--- infra/bootstrap.sh
+++ infra/bootstrap.sh
@@ -103,12 +103,18 @@ if [[ ! -z "${RUN_BOOTSTRAP:-}" ]]; then
     # Larger CPU cluster for Dask and Spark examples
     "$SCRIPT_DIR"/sdk_helpers.sh ensure_aml_compute "cpu-cluster-lg" 0 4 "Standard_DS15_v2"
     
+    echo_title "Ensuring CPU compute with MSI"
+    "$SCRIPT_DIR"/sdk_helpers.sh ensure_aml_compute_msi "cpu-cluster-msi" 0 8 "Standard_DS3_v2" "uaimevnet"
+
     echo_title "Ensuring GPU compute"
     "$SCRIPT_DIR"/sdk_helpers.sh ensure_aml_compute "gpu-cluster" 0 20 "Standard_NC6"
     "$SCRIPT_DIR"/sdk_helpers.sh ensure_aml_compute "automl-gpu-cluster" 0 4 "STANDARD_NC6"
     # v100 GPU cluster for deepspeed cli examples
     "$SCRIPT_DIR"/sdk_helpers.sh ensure_aml_compute "gpu-v100-cluster" 0 2 "Standard_ND40rs_v2"
-    
+  
+    echo_title "Ensuring GPU compute with MSI"
+    "$SCRIPT_DIR"/sdk_helpers.sh ensure_aml_compute_msi "gpu-cluster-msi" 0 8 "Standard_NC6" "uaimevnet"
+
     echo_title "Running prerequisites"
     "$SCRIPT_DIR"/sdk_helpers.sh ensure_prerequisites_in_workspace
     "$SCRIPT_DIR"/sdk_helpers.sh update_dataset
