6501f8f0e3c79763080df8e9e85eea9a4d315cf1
Ayush Mishra <ayushmishra@microsoft.com>
2023-05-10 03:00:39 +0530

    update

--- sdk/python/foundation-models/system/import/code/predict.py
+++ sdk/python/foundation-models/system/import/code/predict.py
@@ -1,16 +1,13 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
+
 import json
-import logging
 import os
+import time
 import torch
-import numpy as np
-import pandas as pd
 from transformers import LlamaTokenizer, LlamaForCausalLM
 
-
-logger = logging.getLogger(__name__)
 
 model = None
 tokenizer = None
@@ -20,42 +17,72 @@ def init():
     global tokenizer
 
     model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), "INPUT_model_path")
-    # subfolder = "open_llama_7b_preview_200bt_transformers_weights"
     try:
-        logger.info("Loading model from path.")
-        # tokenizer = LlamaTokenizer.from_pretrained(model_path, subfolder=subfolder, use_fast=False, local_files_only=True)
-        # model = LlamaForCausalLM.from_pretrained(model_path, subfolder=subfolder, local_files_only=True)
-        tokenizer = LlamaTokenizer.from_pretrained(model_path, use_fast=False, local_files_only=True)
-        model = LlamaForCausalLM.from_pretrained(model_path, local_files_only=True)
-        logger.info("Loading successful.")
+        print("Loading model from path.")
+        # Use this for open_llama models
+        subfolder = "open_llama_7b_preview_200bt_transformers_weights"
+        tokenizer = LlamaTokenizer.from_pretrained(model_path, subfolder=subfolder, use_fast=False, local_files_only=True)
+        model = LlamaForCausalLM.from_pretrained(model_path, subfolder=subfolder, local_files_only=True)
+        # uncomment for others
+        # tokenizer = LlamaTokenizer.from_pretrained(model_path, use_fast=False, local_files_only=True)
+        # model = LlamaForCausalLM.from_pretrained(model_path, local_files_only=True)
+        print("Loading successful.")
     except Exception as e:
         return json.dumps({"error": str(e)})
 
 
-def run(data, **kwargs):
+def get_input_string(input_str):
+    data = json.loads(input_str)
+    input_data = data["inputs"]["input_str"]
+    params = data["inputs"]["params"]
+    return input_data, params
+
+
+def log_execution_time(func, logger=None):
+    """Decorate method to log execution time."""
+    def wrap_func(*args, **kwargs):
+        t1 = time.time()
+        result = func(*args, **kwargs)
+        t2 = time.time()
+        print(f"{func.__name__!r} executed in {(t2-t1):.4f}s")
+        return result
+    return wrap_func
+
+
+@log_execution_time
+def run(data):
+    global model
+    global tokenizer
+
     if not model or not tokenizer:
         return json.dumps({"error": "Model or tokenizer was not initialized correctly. Could not infer"})
 
-    device = kwargs.get("device", -1)
+    try:
+        print(data)
+        inputs, params = get_input_string(data)
+    except Exception as e:
+        return json.dumps({"error": f'Error: {e}.' + 'Input request should be in: {"inputs": {"input_str": ["text"], "params": {"k": "v"}}}'})
+
+    device = params.get("device", -1)
     if device == -1 and torch.cuda.is_available():
-        logging.warning('CUDA available. To switch to GPU device pass `"parameters": {"device" : 0}` in the input.')
+        print('WARNING: CUDA available. To switch to GPU device pass `"params": {"device" : 0}` in the input.')
     if device == 0 and not torch.cuda.is_available():
         device = -1
-        logging.warning("CUDA unavailable. Defaulting to CPU device.")
+        print("WARNING: CUDA unavailable. Defaulting to CPU device.")
 
     device = "cuda" if device == 0 else "cpu"
 
-    logging.info(f"Using device: {device} for the inference")
+    print(f"Using device: {device} for the inference")
 
     try:
-        max_new_tokens = kwargs.get("max_new_tokens", 512)
-        print(type(data))
-        print(data)
-        # inputs = tokenizer("Hello, my dog is cute", return_tensors="pt").input_features.to(device)
-        inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
-        # model = model.to(device)
-        preds = model.generate(**inputs, max_new_tokens=max_new_tokens, do_sample=True)
-        result = tokenizer.batch_decode(preds, skip_special_tokens=True)[0]
+        max_new_tokens = params.get("max_new_tokens", 512)
+        result = []
+        for inp in inputs:
+            # inputs = tokenizer(inp, return_tensors="pt").input_features.to(device)
+            inputs = tokenizer(inp, return_tensors="pt")
+            model = model.to(device)
+            preds = model.generate(**inputs, max_new_tokens=max_new_tokens, do_sample=True)
+            result.append(tokenizer.batch_decode(preds, skip_special_tokens=True)[0])
         return json.dumps({
             "result": result
         })
@@ -63,6 +90,6 @@ def run(data, **kwargs):
         return json.dumps({"error": str(e)})
 
 
-if __name__ == "__main__":
-    print(init())
-    print(run("data"))
+# if __name__ == "__main__":
+#     print(init())
+#     print(run('{"inputs": {"input_str": ["rocco noticed the almost defeated look on her lovely face and did not like it.", "Hello, my dog is cute."], "params": {"max_new_tokens": 512}}}'))
