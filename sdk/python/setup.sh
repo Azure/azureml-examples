09593a3c5d0e693679ab3bea2407ea84d6ad6b69
fkriti <kritifaujdar@gmail.com>
2023-05-09 14:22:20 -0700

    update setup.sh with updated SDK

--- sdk/python/setup.sh
+++ sdk/python/setup.sh
@@ -16,7 +16,7 @@ pip install pandas
 
 # <az_ml_sdk_test_install>
 # pip install azure-ai-ml==0.1.0.b8
-pip install azure-ai-ml
+pip install azure-ai-ml==1.7.0a20230505008 --extra-index-url https://pkgs.dev.azure.com/azure-sdk/public/_packaging/azure-sdk-for-python/pypi/simple/
 # https://docsupport.blob.core.windows.net/ml-sample-submissions/1905732/azure_ai_ml-1.0.0-py3-none-any.whl
 # </az_ml_sdk_test_install>
 
