83107ba0f94971c25aecf8795e958748578f1595
SitaRam Chaitanya Kanakamedala <skanakamedal@microsoft.com>
2023-05-18 17:54:53 +0530

    Updating FT notebooks

--- sdk/python/foundation-models/system/finetune/translation/wmt16-en-ro-dataset/download-dataset.py
+++ sdk/python/foundation-models/system/finetune/translation/wmt16-en-ro-dataset/download-dataset.py
@@ -3,10 +3,10 @@ import argparse, os
 
 parser = argparse.ArgumentParser()
 # add an argument to specify a dataset name to download
-parser.add_argument("--dataset", type=str, default="cnn_dailymail", help="dataset name")
-# add an argument to specify the config name of the dataset
+parser.add_argument("--dataset", type=str, default="wmt16", help="dataset name")
+# add an argument to specify a dataset name to download
 parser.add_argument(
-    "--config_name", type=str, default="3.0.0", help="config name of the dataset"
+    "--dataset_subset", type=str, default="ro-en", help="dataset subset name"
 )
 # argument to save a fraction of the dataset
 parser.add_argument(
@@ -16,7 +16,7 @@ parser.add_argument(
 parser.add_argument(
     "--download_dir",
     type=str,
-    default="./news-summary-dataset",
+    default="data",
     help="directory to download the dataset to",
 )
 args = parser.parse_args()
@@ -25,13 +25,20 @@ args = parser.parse_args()
 if not os.path.exists(args.download_dir):
     os.makedirs(args.download_dir)
 
+
+def format_translation(example):
+    for key in example["translation"]:
+        example[key] = example["translation"][key]
+    return example
+
+
 # import hugging face datasets library
 from datasets import load_dataset, get_dataset_split_names
 
-for split in get_dataset_split_names(args.dataset, config_name=args.config_name):
-    print(f"Loading {split} split of {args.dataset} dataset...")
+for split in get_dataset_split_names(args.dataset, args.dataset_subset):
     # load the split of the dataset
-    dataset = load_dataset(args.dataset, args.config_name, split=split)
+    dataset = load_dataset(args.dataset, args.dataset_subset, split=split)
+    dataset = dataset.map(format_translation, remove_columns=["translation"])
     # save the split of the dataset to the download directory as json lines file
     dataset.select(range(int(dataset.num_rows * args.fraction))).to_json(
         os.path.join(args.download_dir, f"{split}.jsonl")
