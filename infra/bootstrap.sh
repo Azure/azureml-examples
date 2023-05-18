be1a7a8517958e637048e4efb5cdb8932406b040
Gaurav Rajguru <rajgurugaurav77@gmail.com>
2023-05-18 13:10:51 +0530

    NebulaML pytoch notebook failure due to k80 gpu (#2278)
    Co-authored-by: grajguru <grajguru@microsoft.com>

--- infra/bootstrap.sh
+++ infra/bootstrap.sh
@@ -106,6 +106,8 @@ if [[ ! -z "${RUN_BOOTSTRAP:-}" ]]; then
     echo_title "Ensuring GPU compute"
     "$SCRIPT_DIR"/sdk_helpers.sh ensure_aml_compute "gpu-cluster" 0 20 "Standard_NC6"
     "$SCRIPT_DIR"/sdk_helpers.sh ensure_aml_compute "automl-gpu-cluster" 0 4 "STANDARD_NC6"
+    # v100 single GPU cluster for pytorch 2.0 based notebooks
+    "$SCRIPT_DIR"/sdk_helpers.sh ensure_aml_compute "gpu-v100-1GPU-cluster" 0 4 "Standard_NC6s_v3"
     # v100 GPU cluster for deepspeed cli examples
     "$SCRIPT_DIR"/sdk_helpers.sh ensure_aml_compute "gpu-v100-cluster" 0 2 "Standard_ND40rs_v2"
     
