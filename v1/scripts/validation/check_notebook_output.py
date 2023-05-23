f841e77dc50068de4be12e9ad89ed69f6f99e967
nick863 <30440255+nick863@users.noreply.github.com>
2023-05-22 18:18:12 -0700

    Exclude downloading message from warnings

--- v1/scripts/validation/check_notebook_output.py
+++ v1/scripts/validation/check_notebook_output.py
@@ -52,9 +52,8 @@ allowed_list = [
     "Readonly attribute primary_metric will be ignored",
     "Downloading artifact ",
     "Warnings:",
-    "Class FeatureStoreOperations: This is an experimental class",
-    "Class FeatureSetOperations: This is an experimental class",
-    "Class FeatureStoreEntityOperations: This is an experimental class",
+    "Downloading builder script",
+    "Downloading extra modules",
 ]
 
 with open(full_name, "r") as notebook_file:
