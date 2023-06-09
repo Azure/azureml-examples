091c4ad94089b5a49d2c0913596d95663e02202e
learndikshadutt08 <135646287+learndikshadutt08@users.noreply.github.com>
2023-06-09 18:41:39 +0530

    Update setup.sh

--- cli/setup.sh
+++ cli/setup.sh
@@ -26,9 +26,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="Sample"
-LOCATION="eastus2"
-WORKSPACE="MLWorkflow"
+GROUP="NewAMLRG"
+LOCATION="eastus"
+WORKSPACE="NewWorkspace"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
