75e217f6a1d65542adb6fd31e0640d39c37c8e9e
Fred Li <feli1@microsoft.com>
2023-06-19 13:34:13 -0700

    Format the script

--- cli/jobs/spark/setup-identities.sh
+++ cli/jobs/spark/setup-identities.sh
@@ -6,15 +6,15 @@ AML_WORKSPACE_NAME=$(az configure -l --query "[?name=='workspace'].value" -o tsv
 API_VERSION="2022-05-01"
 TOKEN=$(az account get-access-token --query accessToken -o tsv)
 
-AML_USER_MANAGED_ID = "${RESOURCE_GROUP}-uai"
-AML_RESOURCE_ID = "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.ManagedIdentity/userAssignedIdentities/$AML_USER_MANAGED_ID"
+AML_USER_MANAGED_ID=${RESOURCE_GROUP}-uai
+AML_RESOURCE_ID="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.ManagedIdentity/userAssignedIdentities/$AML_USER_MANAGED_ID"
 #</create_variables>
 
 #<create_uai>
 az identity create --name $AML_USER_MANAGED_ID --resource-group $RESOURCE_GROUP --location $LOCATION
 #</create_uai>
 
-TEMP_UAI_FILE = "temp-user-assigned-identity.yml"
+TEMP_UAI_FILE="temp-user-assigned-identity.yml"
 cp user-assigned-identity.yml $TEMP_UAI_FILE
 sed -i "s/{{AML_RESOURCE_ID}}/$AML_RESOURCE_ID/g;" $TEMP_UAI_FILE
 
