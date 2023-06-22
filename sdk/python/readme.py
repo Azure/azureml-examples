817da847c8151f48eb5bd8223277ec5068b6fd5a
Fred Li <feli1@microsoft.com>
2023-06-22 12:06:36 -0700

    Initial update to enable sdk spark samples automation test

--- sdk/python/readme.py
+++ sdk/python/readme.py
@@ -18,12 +18,7 @@ NOT_TESTED_NOTEBOOKS = [
     "train-hyperparameter-tune-with-sklearn",
     "train-hyperparameter-tune-deploy-with-keras",
     "train-hyperparameter-tune-deploy-with-tensorflow",
-    "pipeline_with_spark_nodes",
     "interactive_data_wrangling",
-    "attach_manage_spark_pools",
-    "submit_spark_pipeline_jobs",
-    "submit_spark_standalone_jobs",
-    "submit_spark_standalone_jobs_managed_vnet",
     # mlflow SDK samples notebooks
     "mlflow_sdk_online_endpoints_progresive",
     "mlflow_sdk_online_endpoints",
