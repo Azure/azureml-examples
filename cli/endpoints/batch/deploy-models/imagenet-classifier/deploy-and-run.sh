dfd5760f409924f05f36c25b210d1bf4fbaf0515
Facundo Santiago <fasantia@microsoft.com>
2023-06-29 19:23:41 -0400

    Update deploy-and-run.sh

--- cli/endpoints/batch/deploy-models/imagenet-classifier/deploy-and-run.sh
+++ cli/endpoints/batch/deploy-models/imagenet-classifier/deploy-and-run.sh
@@ -14,7 +14,7 @@ ENDPOINT_NAME="$ENDPOINT_NAME-$ENDPOINT_SUFIX"
 
 echo "Download model from Azure Storage"
 # <download_model>
-wget https://azuremlexampledata.blob.core.windows.net/data/imagenet/imagenet-1000.zip
+wget https://azuremlexampledata.blob.core.windows.net/data/imagenet/model.zip
 unzip model.zip -d .
 # </download_model>
 
@@ -51,7 +51,7 @@ az ml batch-deployment show --name $DEPLOYMENT_NAME --endpoint-name $ENDPOINT_NA
 # </query_deployment>
 
 # <download_sample_data>
-wget https://azuremlexampledata.blob.core.windows.net/data/imagenet-1000.zip
+wget https://azuremlexampledata.blob.core.windows.net/data/imagenet/imagenet-1000.zip
 unzip imagenet-1000.zip -d data
 # </download_sample_data>
 
