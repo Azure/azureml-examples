2d456b29fb5e64c4ab7a7e217da7a6431ff9a2c1
Gaurav Rajguru <rajgurugaurav77@gmail.com>
2023-07-10 14:05:32 +0530

    vision batch deployment cli and sdk notebook (#2433)
    * vision batch deployment cli and sdk notebook
    
    * black formatting
    
    * Comments
    
    * comments
    
    * updated to take latest version in FT notebooks
    
    ---------
    
    Co-authored-by: grajguru <grajguru@microsoft.com>

--- cli/foundation-models/system/finetune/image-classification/multiclass-classification/prepare_data.py
+++ cli/foundation-models/system/finetune/image-classification/multiclass-classification/prepare_data.py
@@ -40,9 +40,7 @@ def create_jsonl_and_mltable_files(uri_folder_data_path, dataset_dir):
 
     # We'll copy each JSONL file within its related MLTable folder
     training_mltable_path = os.path.join(dataset_parent_dir, "training-mltable-folder")
-    validation_mltable_path = os.path.join(
-        dataset_parent_dir, "validation-mltable-folder"
-    )
+    validation_mltable_path = os.path.join(dataset_parent_dir, "validation-mltable-folder")
 
     # Create MLTable folders, if they don't exist
     os.makedirs(training_mltable_path, exist_ok=True)
@@ -51,12 +49,8 @@ def create_jsonl_and_mltable_files(uri_folder_data_path, dataset_dir):
     train_validation_ratio = 5
 
     # Path to the training and validation files
-    train_annotations_file = os.path.join(
-        training_mltable_path, "train_annotations.jsonl"
-    )
-    validation_annotations_file = os.path.join(
-        validation_mltable_path, "validation_annotations.jsonl"
-    )
+    train_annotations_file = os.path.join(training_mltable_path, "train_annotations.jsonl")
+    validation_annotations_file = os.path.join(validation_mltable_path, "validation_annotations.jsonl")
 
     # Baseline of json line dictionary
     json_line_sample = {"image_url": uri_folder_data_path, "label": ""}
@@ -87,20 +81,15 @@ def create_jsonl_and_mltable_files(uri_folder_data_path, dataset_dir):
     print("done")
 
     # Create and save train mltable
-    train_mltable_file_contents = create_ml_table_file(
-        os.path.basename(train_annotations_file)
-    )
+    train_mltable_file_contents = create_ml_table_file(os.path.basename(train_annotations_file))
     save_ml_table_file(training_mltable_path, train_mltable_file_contents)
 
     # Create and save validation mltable
-    validation_mltable_file_contents = create_ml_table_file(
-        os.path.basename(validation_annotations_file)
-    )
+    validation_mltable_file_contents = create_ml_table_file(os.path.basename(validation_annotations_file))
     save_ml_table_file(validation_mltable_path, validation_mltable_file_contents)
 
 
 def upload_data_and_create_jsonl_mltable_files(ml_client, dataset_parent_dir):
-
     # Create directory, if it does not exist
     os.makedirs(dataset_parent_dir, exist_ok=True)
 
@@ -142,9 +131,7 @@ def upload_data_and_create_jsonl_mltable_files(ml_client, dataset_parent_dir):
     print("")
     print("Path to folder in Blob Storage:")
     print(uri_folder_data_asset.path)
-    create_jsonl_and_mltable_files(
-        uri_folder_data_path=uri_folder_data_asset.path, dataset_dir=dataset_dir
-    )
+    create_jsonl_and_mltable_files(uri_folder_data_path=uri_folder_data_asset.path, dataset_dir=dataset_dir)
 
 
 def read_image(image_path):
@@ -153,16 +140,12 @@ def read_image(image_path):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Prepare data for image classification"
-    )
+    parser = argparse.ArgumentParser(description="Prepare data for image classification")
 
     parser.add_argument("--subscription", type=str, help="Subscription ID")
     parser.add_argument("--resource_group", type=str, help="Resource group name")
     parser.add_argument("--workspace", type=str, help="Workspace name")
-    parser.add_argument(
-        "--data_path", type=str, default="./data", help="Dataset location"
-    )
+    parser.add_argument("--data_path", type=str, default="./data", help="Dataset location")
 
     args, unknown = parser.parse_known_args()
     args_dict = vars(args)
@@ -178,9 +161,7 @@ if __name__ == "__main__":
         workspace = args.workspace
         ml_client = MLClient(credential, subscription_id, resource_group, workspace)
 
-    upload_data_and_create_jsonl_mltable_files(
-        ml_client=ml_client, dataset_parent_dir=args.data_path
-    )
+    upload_data_and_create_jsonl_mltable_files(ml_client=ml_client, dataset_parent_dir=args.data_path)
 
     sample_image = os.path.join(args.data_path, "fridgeObjects", "milk_bottle", "99.jpg")
     huggingface_request_json = {
