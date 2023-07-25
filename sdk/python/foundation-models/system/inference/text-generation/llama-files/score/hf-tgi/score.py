79e4836d64b43d1def21e62812e2d883c5cb04d3
Ayush Mishra <ayushmishra@microsoft.com>
2023-07-25 13:25:03 +0530

    black + minor fixes

--- sdk/python/foundation-models/system/inference/text-generation/llama-files/score/hf-tgi/score.py
+++ sdk/python/foundation-models/system/inference/text-generation/llama-files/score/hf-tgi/score.py
@@ -62,38 +62,44 @@ aacs_threshold = int(os.environ.get("CONTENT_SAFETY_THRESHOLD", 2))
 
 class SupportedTask:
     """Supported tasks by text-generation-inference"""
+
     TEXT_GENERATION = "text-generation"
     CHAT_COMPLETION = "chat-completion"
 
 
 # default values
 MLMODEL_PATH = "mlflow_model_folder/MLmodel"
-DEFAULT_MODEL_ID_PATH  = "mlflow_model_folder/data/model"
+DEFAULT_MODEL_ID_PATH = "mlflow_model_folder/data/model"
 client = None
 task_type = SupportedTask.TEXT_GENERATION
 
 
 def is_server_healthy():
     """Periodically checks if server is up and running."""
-    # use psutil to go through active process 
+    # use psutil to go through active process
     WAIT_TIME = 20
     RETRY_COUNT = 5
     count = 0
-    while count < RETRY_COUNT and TEXT_GEN_LAUNCHER_PROCESS_NAME not in [p.name() for p in psutil.process_iter()]:
-        logger.warning(f"Process {TEXT_GEN_LAUNCHER_PROCESS_NAME} is not running. Sleeping for {WAIT_TIME}s and retrying")
+    while count < RETRY_COUNT and TEXT_GEN_LAUNCHER_PROCESS_NAME not in [
+        p.name() for p in psutil.process_iter()
+    ]:
+        logger.warning(
+            f"Process {TEXT_GEN_LAUNCHER_PROCESS_NAME} is not running. Sleeping for {WAIT_TIME}s and retrying"
+        )
         time.sleep(WAIT_TIME)
         count += 1
     if count >= RETRY_COUNT:
         total_dur = RETRY_COUNT * WAIT_TIME
-        raise Exception(f"Sever process not running after waiting for {total_dur}. Terminating")
+        raise Exception(
+            f"Sever process not running after waiting for {total_dur}. Terminating"
+        )
 
-    logger.info(f"Server process {TEXT_GEN_LAUNCHER_PROCESS_NAME} running. Hitting endpoint with 5s delay")
+    logger.info(
+        f"Server process {TEXT_GEN_LAUNCHER_PROCESS_NAME} running. Hitting endpoint with 5s delay"
+    )
     time.sleep(5)
 
-    payload_dict = {
-        "inputs": "Meaning of life is",
-        "parameters":{"max_new_tokens":2}
-    }
+    payload_dict = {"inputs": "Meaning of life is", "parameters": {"max_new_tokens": 2}}
 
     json_str = json.dumps(payload_dict)
 
@@ -101,9 +107,7 @@ def is_server_healthy():
         response = requests.post(
             url=LOCAL_HOST_URI,
             data=json_str,
-            headers={
-                "Content-Type": "application/json"
-            }
+            headers={"Content-Type": "application/json"},
         )
         logger.info(f"response status code: {response.status_code}")
         if response.status_code == 200 or response.status_code == 201:
@@ -111,6 +115,7 @@ def is_server_healthy():
     except Exception as e:
         logger.warning(f"Test request failed. Error {e}")
     return False
+
 
 # ACS START
 
@@ -173,6 +178,7 @@ class CsChunkingUtils:
         if len(buffer) > 0:
             ret.append(buffer)
         return ret
+
 
 async def async_analyze_text_task(client, request):
     loop = asyncio.get_event_loop()
@@ -287,6 +293,7 @@ def get_safe_input(input_data):
     print(f"Input analyzed, severity {severity}")
     return result, severity
 
+
 # ACS END
 
 
@@ -323,7 +330,9 @@ def init():
             logger.info(f"env: {k} = {v}")
 
         model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR", ""), model_id)
-        abs_mlmodel_path = os.path.join(os.getenv("AZUREML_MODEL_DIR", ""), MLMODEL_PATH)
+        abs_mlmodel_path = os.path.join(
+            os.getenv("AZUREML_MODEL_DIR", ""), MLMODEL_PATH
+        )
         mlmodel = {}
         if abs_mlmodel_path and os.path.exists(abs_mlmodel_path):
             with open(abs_mlmodel_path) as f:
@@ -333,7 +342,10 @@ def init():
             flavors = mlmodel.get("flavors", {})
             if "hftransformersv2" in flavors:
                 task_type = flavors["hftransformersv2"]["task_type"]
-                if task_type not in (SupportedTask.TEXT_GENERATION, SupportedTask.CHAT_COMPLETION):
+                if task_type not in (
+                    SupportedTask.TEXT_GENERATION,
+                    SupportedTask.CHAT_COMPLETION,
+                ):
                     raise Exception(f"Unsupported task_type {task_type}")
 
         logger.info(f"Loading model from path {model_path} for task_type: {task_type}")
@@ -346,7 +358,9 @@ def init():
 
         WAIT_TIME = 60
         while not is_server_healthy():
-            logger.info(f"Server not up. Waiting for {WAIT_TIME}s, before querying again.")
+            logger.info(
+                f"Server not up. Waiting for {WAIT_TIME}s, before querying again."
+            )
             time.sleep(WAIT_TIME)
         logger.info("Server Started")
 
@@ -355,14 +369,19 @@ def init():
         logger.info(os.system("nvidia-smi"))
         logger.info("###### GPU INFO ######")
 
-        client = Client(LOCAL_HOST_URI, timeout=client_timeout)  # use deployment settings
+        client = Client(
+            LOCAL_HOST_URI, timeout=client_timeout
+        )  # use deployment settings
         logger.info(f"Created Client: {client}")
     except Exception as e:
         raise Exception(f"Error in creating client or server: {e}")
 
 
 def get_processed_input_data_for_chat_completion(data: List[str]) -> str:
-    """
+    """Process chat-completion input request.
+
+    Taken from: https://github.com/facebookresearch/llama/blob/6c7fe276574e78057f917549435a2554000a876d/llama/generation.py#L213
+
     example input:
     [
         {"role": "user", "content": "What is the tallest building in the world?"},
@@ -372,6 +391,7 @@ def get_processed_input_data_for_chat_completion(data: List[str]) -> str:
     example output:
     "[INST]What is the tallest building in the world?[\INST]As of 2021, the Burj Khalifa in Dubai\n[INST]and in Africa?[/INST]"
     """
+    # TODO: user ConversationalPipeline
     B_INST, E_INST = "[INST]", "[/INST]"
     B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
     DEFAULT_SYSTEM_PROMPT = """\
@@ -417,7 +437,8 @@ def get_processed_input_data_for_chat_completion(data: List[str]) -> str:
 
 
 def get_request_data(request_string) -> Tuple[Union[str, List[str]], Dict[str, Any]]:
-    """
+    """Process and validate inference request.
+
     return type for chat-completion: str, dict
     return type for text-generation: list, dict
     """
@@ -427,8 +448,8 @@ def get_request_data(request_string) -> Tuple[Union[str, List[str]], Dict[str, A
         logger.info(f"data: {data}")
         inputs = data.get("input_data", None)
 
-        input_data = []   # type: Union[str, List[str]]
-        params = {} # type: Dict[str, Any]
+        input_data = []  # type: Union[str, List[str]]
+        params = {}  # type: Dict[str, Any]
 
         if not isinstance(inputs, dict):
             raise Exception("Invalid input data")
@@ -448,16 +469,20 @@ def get_request_data(request_string) -> Tuple[Union[str, List[str]], Dict[str, A
 
         return input_data, params
     except Exception as e:
-        raise Exception(json.dumps({
-            "error": (
-                'Expected input format: \n'
-                '{"input_data": {"input_string": "<query>", "parameters": {"k1":"v1", "k2":"v2"}}}.\n '
-                '<query> should be in below format:\n '
-                'For text-generation: ["str1", "str2", ...]\n'
-                'For chat-completion : [{"role": "user", "content": "str1"}, {"role": "assistant", "content": "str2"} ....]'
-            ),
-            "exception": str(e)
-        }))
+        raise Exception(
+            json.dumps(
+                {
+                    "error": (
+                        "Expected input format: \n"
+                        '{"input_data": {"input_string": "<query>", "parameters": {"k1":"v1", "k2":"v2"}}}.\n '
+                        "<query> should be in below format:\n "
+                        'For text-generation: ["str1", "str2", ...]\n'
+                        'For chat-completion : [{"role": "user", "content": "str1"}, {"role": "assistant", "content": "str2"} ....]'
+                    ),
+                    "exception": str(e),
+                }
+            )
+        )
 
 
 def run(data):
@@ -468,25 +493,31 @@ def run(data):
     try:
         data, severity = get_safe_input(data)
         if severity > aacs_threshold:
-            logger.warning(f"Input severity ({severity}) greater than aacs threshold ({aacs_threshold}).")
+            logger.warning(
+                f"Input severity ({severity}) greater than aacs threshold ({aacs_threshold})."
+            )
             return {}
 
         if client is None:
             raise Exception("Client is not initialized")
 
         query, params = get_request_data(data)
-        logger.info(f"generating response for input_string: {query}, parameters: {params}")
+        logger.info(
+            f"generating response for input_string: {query}, parameters: {params}"
+        )
 
         if task_type == SupportedTask.CHAT_COMPLETION:
             time_start = time.time()
             response_str = client.generate(query, **params).generated_text
             time_taken = time.time() - time_start
             logger.info(f"time_taken: {time_taken}")
-            result_dict = {'output': f'{response_str}'}
+            result_dict = {"output": f"{response_str}"}
             resp = pd.DataFrame([result_dict])
             return get_safe_response(resp)
 
-        assert task_type == SupportedTask.TEXT_GENERATION and isinstance(query, list), "query should be a list for text-generation"
+        assert task_type == SupportedTask.TEXT_GENERATION and isinstance(
+            query, list
+        ), "query should be a list for text-generation"
 
         results = {}
         for i, q in enumerate(query):
@@ -494,16 +525,13 @@ def run(data):
             response_str = client.generate(q, **params).generated_text
             time_taken = time.time() - time_start
             logger.info(f"query {i} - time_taken: {time_taken}")
-            results[str(i)] = [f'{response_str}']
+            results[str(i)] = [f"{response_str}"]
 
         resp = pd.DataFrame(results)
         return get_safe_response(resp)
 
     except Exception as e:
-        return json.dumps({
-            "error": "Error in processing request",
-            "exception": str(e)
-        })
+        return json.dumps({"error": "Error in processing request", "exception": str(e)})
 
 
 if __name__ == "__main__":
@@ -513,46 +541,40 @@ if __name__ == "__main__":
     valid_inputs = {
         "text-generation": [
             {
-                "input_data":{
+                "input_data": {
                     "input_string": ["the meaning of life is"],
-                    "parameters":{"max_new_tokens": 100, "do_sample": True}
+                    "parameters": {"max_new_tokens": 100, "do_sample": True},
                 }
             }
         ],
         "chat-completion": [
-            { 
-                "input_data": { 
+            {
+                "input_data": {
                     "input_string": [
-                        { 
-                            "role": "user", 
-                            "content": "What is the tallest building in the world?" 
-                        }, 
-                        { 
-                            "role": "assistant", 
-                            "content": "As of 2021, the Burj Khalifa in Dubai, United Arab Emirates is the tallest building in the world, standing at a height of 828 meters (2,722 feet). It was completed in 2010 and has 163 floors. The Burj Khalifa is not only the tallest building in the world but also holds several other records, such as the highest occupied floor, highest outdoor observation deck, elevator with the longest travel distance, and the tallest freestanding structure in the world." 
-                        }, 
-                        { 
-                            "role": "user", 
-                            "content": "and in Africa?" 
-                        }, 
-                        { 
-                            "role": "assistant", 
-                            "content": "In Africa, the tallest building is the Carlton Centre, located in Johannesburg, South Africa. It stands at a height of 50 floors and 223 meters (730 feet). The CarltonDefault Centre was completed in 1973 and was the tallest building in Africa for many years until the construction of the Leonardo, a 55-story skyscraper in Sandton, Johannesburg, which was completed in 2019 and stands at a height of 230 meters (755 feet). Other notable tall buildings in Africa include the Ponte City Apartments in Johannesburg, the John Hancock Center in Lagos, Nigeria, and the Alpha II Building in Abidjan, Ivory Coast" 
-                        }, 
-                        { 
-                            "role": "user", 
-                            "content": "and in Europe?" 
-                        } 
-                    ], 
-                    "parameters":{ 
+                        {
+                            "role": "user",
+                            "content": "What is the tallest building in the world?",
+                        },
+                        {
+                            "role": "assistant",
+                            "content": "As of 2021, the Burj Khalifa in Dubai, United Arab Emirates is the tallest building in the world, standing at a height of 828 meters (2,722 feet). It was completed in 2010 and has 163 floors. The Burj Khalifa is not only the tallest building in the world but also holds several other records, such as the highest occupied floor, highest outdoor observation deck, elevator with the longest travel distance, and the tallest freestanding structure in the world.",
+                        },
+                        {"role": "user", "content": "and in Africa?"},
+                        {
+                            "role": "assistant",
+                            "content": "In Africa, the tallest building is the Carlton Centre, located in Johannesburg, South Africa. It stands at a height of 50 floors and 223 meters (730 feet). The CarltonDefault Centre was completed in 1973 and was the tallest building in Africa for many years until the construction of the Leonardo, a 55-story skyscraper in Sandton, Johannesburg, which was completed in 2019 and stands at a height of 230 meters (755 feet). Other notable tall buildings in Africa include the Ponte City Apartments in Johannesburg, the John Hancock Center in Lagos, Nigeria, and the Alpha II Building in Abidjan, Ivory Coast",
+                        },
+                        {"role": "user", "content": "and in Europe?"},
+                    ],
+                    "parameters": {
                         "temperature": 0.9,
                         "top_p": 0.6,
                         "do_sample": True,
-                        "max_new_tokens":100 
-                    }
-                } 
+                        "max_new_tokens": 100,
+                    },
+                }
             }
-        ]
+        ],
     }
 
     for sample_ip in valid_inputs[task_type]:
