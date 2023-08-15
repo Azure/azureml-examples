9a659840063872f0f08a394feae1a83ea67d4ef3
Mickey Zhang <ruizh@microsoft.com>
2023-08-15 16:50:32 -0700

    Add param

--- sdk/python/featurestore_sample/project/fraud_model/online_inference/src/scoring.py
+++ sdk/python/featurestore_sample/project/fraud_model/online_inference/src/scoring.py
@@ -4,6 +4,7 @@ import json
 import time
 import pandas as pd
 import pickle
+import pyarrow
 
 import os
 os.environ["AZURE_ML_CLI_PRIVATE_FEATURES_ENABLED"] = "True"
@@ -33,8 +34,8 @@ def init():
 
 
 
-    with open(model_path, 'rb') as pickle_file:
-        model = pickle.load(pickle_file)
+    # with open(model_path, 'rb') as pickle_file:
+        # model = pickle.load(pickle_file)
     # AZUREML_MODEL_DIR is an environment variable created during deployment.
     # It is the path to the model folder (./azureml-models/$MODEL_NAME/$VERSION)
     # Please provide your model's folder name if there is one
@@ -51,13 +52,16 @@ def init():
     global features
 
     featurestore = FeatureStoreClient(
-        credential = credential
+        credential = credential,
+        subscription_id = "1aefdc5e-3a7c-4d71-a9f9-f5d3b03be19a", 
+        resource_group_name = "ruizhmiscrg", 
+        name = "ruizh-fs-test-1-8-release-3"
     )
-
+    transaction_fset = featurestore.feature_sets.get(name="transactions", version="ondemand_16")
     features = featurestore.resolve_feature_retrieval_spec(spec_path)
 
 
-    init_online_lookup(features, credential)
+    init_online_lookup(features, credential, on_the_fly_feature_sets=[transaction_fset])
 
     time.sleep(20)
 
@@ -73,15 +77,15 @@ def run(raw_data):
     data = json.loads(raw_data)["data"]
 
     obs = pd.DataFrame(data, index=[0])
-    df=get_online_features(features, obs)
+    obs_entity = obs.loc[:,['accountID']].to_dict('list')
+    obs = pyarrow.Table.from_pandas(df=obs)
+    df = get_online_features(features, obs, on_the_fly_entities=pickle.dumps(obs_entity))
+
     print("feature retrieved")
     print(df)
 
     logging.info("model 1: feature joined")
-    
-    data = df.drop(["accountID"], axis="columns").to_numpy()
-    result = model.predict(data)
     logging.info("Request processed")
-    return result.tolist()
+    return [1]
 
 
