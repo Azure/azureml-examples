977e91671f3b9266e6f9b4a49b1b54b758f237f9
Fred Li <feli1@microsoft.com>
2023-08-23 12:06:49 -0700

    Add python package to cluster

--- sdk/python/featurestore_sample/setup-resources.sh
+++ sdk/python/featurestore_sample/setup-resources.sh
@@ -13,11 +13,10 @@ jupytext --to py "notebooks/sdk_only/2. Enable materialization and backfill feat
 jupytext --to py "notebooks/sdk_only/3. Experiment and train models using features.ipynb"
 jupytext --to py "notebooks/sdk_only/4. Enable recurrent materialization and run batch inference.ipynb"
 
-
-#juipytext --to py "notebooks/sdk_and_cli/1. Develop a feature set and register with managed feature store.ipynb"
-#juipytext --to py "notebooks/sdk_and_cli/2. Enable materialization and backfill feature data.ipynb"
-#juipytext --to py "notebooks/sdk_and_cli/3. Experiment and train models using features.ipynb"
-#juipytext --to py "notebooks/sdk_and_cli/4. Enable recurrent materialization and run batch inference.ipynb"
+#jupytext --to py "notebooks/sdk_and_cli/1. Develop a feature set and register with managed feature store.ipynb"
+#jupytext --to py "notebooks/sdk_and_cli/2. Enable materialization and backfill feature data.ipynb"
+#jupytext --to py "notebooks/sdk_and_cli/3. Experiment and train models using features.ipynb"
+#jupytext --to py "notebooks/sdk_and_cli/4. Enable recurrent materialization and run batch inference.ipynb"
 # <convert_notebook_to_py>
 
 #<replace_template_values>
