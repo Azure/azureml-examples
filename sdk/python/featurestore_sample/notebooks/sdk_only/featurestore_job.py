6376959a3aff58f2f12fe34bec74bf99b8afddf7
Fred Li <feli1@microsoft.com>
2023-08-23 16:09:53 -0700

    Put script run in the same process

--- sdk/python/featurestore_sample/notebooks/sdk_only/featurestore_job.py
+++ sdk/python/featurestore_sample/notebooks/sdk_only/featurestore_job.py
@@ -1,13 +1,15 @@
 from pyspark.sql import SparkSession
+
 spark = SparkSession.builder.appName("AccessData").getOrCreate()
 
+with open("1. Develop a feature set and register with managed feature store.py") as f:
+    exec(f.read())
 
-import subprocess
+with open("2. Enable materialization and backfill feature data.py") as f:
+    exec(f.read())
 
-subprocess.run(["python", "1. Develop a feature set and register with managed feature store.py"])
+with open("3. Experiment and train models using features.py") as f:
+    exec(f.read())
 
-subprocess.run(["python", "2. Enable materialization and backfill feature data.py"])
-
-subprocess.run(["python", "3. Experiment and train models using features.py"])
-
-subprocess.run(["python", "4. Enable recurrent materialization and run batch inference.py"])
+with open("4. Enable recurrent materialization and run batch inference.py") as f:
+    exec(f.read())
