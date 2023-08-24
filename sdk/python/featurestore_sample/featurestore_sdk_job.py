9e84ea3f393de5fac905ef30754f7e9345ddba58
Fred Li <feli1@microsoft.com>
2023-08-24 13:45:28 -0700

    List folder in current path

--- sdk/python/featurestore_sample/featurestore_sdk_job.py
+++ sdk/python/featurestore_sample/featurestore_sdk_job.py
@@ -2,6 +2,13 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("AccessData").getOrCreate()
 
+# Test folder structure
+import os
+
+for path, subdirs, files in os.walk("./"):
+    for name in files:
+        print(os.path.join(path, name))
+
 with open(
     "notebooks/sdk_only/1. Develop a feature set and register with managed feature store.py"
 ) as f:
