f48dc22bed48368ac126ceee9e1a13b6b6ab2597
Vivek Dani <110168656+vivek-dani@users.noreply.github.com>
2023-08-31 18:58:47 +0530

    CLI code for deploying stable diffusion on endpoint

---
+++ cli/generative-ai/inference/utils/base64_to_jpeg.py
@@ -0,0 +1,30 @@
+# Read base64 encoded image from txt file and convert it to image file
+import argparse
+import re
+import json
+import io
+import base64
+from PIL import Image
+
+
+def base64_str_to_image(response_file: str) -> bytes:
+    with open(response_file) as f:
+        serialized_image_json = f.read().strip()
+
+    serialized_image_json=serialized_image_json.replace("\\\"","\"")[1:-1] 
+
+    json_obj = json.loads(serialized_image_json)
+    for obj in json_obj:
+        text_prompt = obj["text_prompt"].strip()
+        generated_image = obj["generated_image"]
+        img = Image.open(io.BytesIO(base64.b64decode(generated_image)))
+        text_prompt=re.sub(r"[^a-zA-Z0-9 ]+", "", text_prompt)
+        img.save(text_prompt + ".jpg", "JPEG")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Prepare data for image classification")
+    parser.add_argument("--response_file", type=str, default="generated_image.txt", help="File having image response from endpoint.")
+    args, unknown = parser.parse_known_args()
+    
+    base64_str_to_image(args.response_file)
