fcf5ac79943740534abbe5c14492db6f5091382d
Gaurav Rajguru <rajgurugaurav77@gmail.com>
2023-09-15 14:44:32 +0530

    Apply suggestions from code review
    remove message

--- cli/foundation-models/system/inference/text-to-image/text-to-image-batch-endpoint.sh
+++ cli/foundation-models/system/inference/text-to-image/text-to-image-batch-endpoint.sh
@@ -69,7 +69,7 @@ az ml batch-deployment create --file batch-deploy.yml --set-default $workspace_i
 # 5. Invoke a job on the batch endpoint
 invoke_output=$(az ml batch-endpoint invoke --name $endpoint_name --input $base_dir $workspace_info 2>&1) || {
     echo "endpoint invoke failed"; exit 1;
-    100 MB, then try splitting input csv file into multiple csv files each of size less than 100MB."; exit 1;
+    
 }
 invoke_temp=${invoke_output#*\"name\": \"}
 job_name=${invoke_temp%%\"*}
