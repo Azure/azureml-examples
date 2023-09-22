ece0a3d7a1c6f88319d69c90605b60c32be666c7
Fred Li <feli1@microsoft.com>
2023-09-21 21:00:13 -0700

    Check mounted folder

--- sdk/python/featurestore_sample/featurestore_sdk_job.py
+++ sdk/python/featurestore_sample/featurestore_sdk_job.py
@@ -2,6 +2,12 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("AccessData").getOrCreate()
 
+import os
+
+for path, subdirs, files in os.walk("./"):
+    for name in files:
+        print(os.path.join(path, name))
+
 print("=======Test Notebook 1============")
 with open(
     "notebooks/sdk_only/1. Develop a feature set and register with managed feature store.py"
