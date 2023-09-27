b19a056cf3a431fc37b2498bb14159bc44c7035b
Fred Li <51424245+fredms@users.noreply.github.com>
2023-09-27 12:28:12 -0700

    Enable automation test for all feature store notebooks (#2657)
    * Enable automation test for all feature store notebooks
    
    * Update feature store uai
    
    * Update uai name
    
    * Add test for 5th notebook
    
    * Convert 5th notebook to py
    
    * Fix the syntax error
    
    * Update random generator
    
    * Update redis name
    
    * Change conda file used for test
    
    * Update to use dynamic version
    
    * Fix the format
    
    * Use the same conda for all notebooks
    
    * Check mounted folder
    
    * Exclude 5th notebook
    
    * Update feature_version to version
    
    * Sample updates for AML Spark managed VNet GA. (#2656)
    
    * Sample updates for AML Spark managed VNet GA.
    
    * Updated notebook title. Triggering build to test.
    
    * Updated workspace name to include timestamp. Triggering build to test.
    
    * Shortened VNet workspace name to meet validation.
    
    * README.md clean-up and fixes for AML Spark samples.
    
    * README.md clean-up and fixes for AML Spark samples.
    
    * chore: Remove 'Send IcM on Failure' step from tutorial workflows (#2670)
    
    * chore: Remove 'Send IcM on Failure' step that hasn't been in use for a while
    
    * chore: Run tutorials/readme.py
    
    * chore: Remove unused local 'generate-icm' action
    
    * update cron schedules to avoid endpoint deletion from automated resource cleanup (#2672)
    
    * change starting hour for workflow
    
    * update cron for text mc mlflow
    
    * use python 3.8.* (#2678)
    
    * use python 3.8.18
    
    * Update cleanup.yml
    
    * update usepython v4
    
    * Update workstation_env.yml (#2680)
    
    * Examples for online inference for LLaVA (#2671)
    
    * Rough version of ITT notebook.
    
    * Working version of the notebook.
    
    * Minor improvements.
    
    * Rough version of the CLI demo.
    
    * Reformat with black.
    
    * Remove mode parameter in call to prepare_data.py.
    
    * Make code cells easier to read.
    
    * Minor readability improvement.
    
    * Format with black.
    
    * Minor fixes and reformatting.
    
    * Reformat with black.
    
    * Update workstation_env.yml (#2681)
    
    delete extraneous space
    
    * update vision FT examples corresponding to recent component update (#2650)
    
    * updating changes with ME component
    
    * updating MMOD as well
    
    * update MMOD cli
    
    * separate section for model evaluation component
    
    * remove ds ort true for classification
    
    * black check
    
    * Remove v1 references (#2675)
    
    * Remove v1 samples from v2 repo
    
    * Remove v1 from table of contents
    
    * Remove v1 test files
    
    * Remove v1 test files
    
    * Remove v1 folder from README
    
    * Remove v1 folder
    
    * Remove v1 tests
    
    * Delete v1 files
    
    * Restore custom-container directory
    
    * Restore custom-container/triton directory
    
    * Update the notebook format
    
    ---------
    
    Co-authored-by: Yogi Pandey <20666257+ynpandey@users.noreply.github.com>
    Co-authored-by: kdestin <101366538+kdestin@users.noreply.github.com>
    Co-authored-by: Rehaan Bhimani <rbhimani@microsoft.com>
    Co-authored-by: Ada <61294872+adrosa@users.noreply.github.com>
    Co-authored-by: Sheri Gilley <sgilley@microsoft.com>
    Co-authored-by: rdondera-microsoft <98922913+rdondera-microsoft@users.noreply.github.com>
    Co-authored-by: shubhamiit <41925087+shubhamiit@users.noreply.github.com>
    Co-authored-by: Diondra <16376603+diondrapeck@users.noreply.github.com>

--- sdk/python/featurestore_sample/setup-resources.sh
+++ sdk/python/featurestore_sample/setup-resources.sh
@@ -8,7 +8,10 @@ AML_WORKSPACE_NAME=$(az configure -l --query "[?name=='workspace'].value" -o tsv
 OUTPUT_COMMAND="print"
 FEATURE_STORAGE_ACCOUNT_NAME=${RESOURCE_GROUP}fs
 USER_ID="36b5b70a-a2b2-45e6-a496-df3c2ffde085"
-
+RAND_NUM=$RANDOM
+UAI_NAME=fstoreuai${RAND_NUM}
+REDIS_NAME=${RESOURCE_GROUP}rds
+VERSION=$(((RANDOM%10)+1))
 # </create_variables>
 
 # <convert_notebook_to_py>
@@ -16,10 +19,12 @@ NOTEBOOK_1="notebooks/sdk_only/1. Develop a feature set and register with manage
 NOTEBOOK_2="notebooks/sdk_only/2. Enable materialization and backfill feature data"
 NOTEBOOK_3="notebooks/sdk_only/3. Experiment and train models using features"
 NOTEBOOK_4="notebooks/sdk_only/4. Enable recurrent materialization and run batch inference"
+NOTEBOOK_5="notebooks/sdk_only/5. Enable online store and run online inference"
 jupytext --to py "${NOTEBOOK_1}.ipynb"
 jupytext --to py "${NOTEBOOK_2}.ipynb"
 jupytext --to py "${NOTEBOOK_3}.ipynb"
 jupytext --to py "${NOTEBOOK_4}.ipynb"
+jupytext --to py "${NOTEBOOK_5}.ipynb"
 # <convert_notebook_to_py>
 
 #<replace_template_values>
@@ -28,9 +33,17 @@ sed -i "s/<SUBSCRIPTION_ID>/$SUBSCRIPTION_ID/g;
     s/<AML_WORKSPACE_NAME>/$AML_WORKSPACE_NAME/g;" $1
 
 #<replace_template_values>
-sed -i "s/display/$OUTPUT_COMMAND/g;s/.\/Users\/<your_user_alias>\/featurestore_sample/.\//g;" "${NOTEBOOK_1}.py"
+sed -i "s/display/$OUTPUT_COMMAND/g;s/.\/Users\/<your_user_alias>\/featurestore_sample/.\//g;
+    s/<VERSION>/$VERSION/g;" "${NOTEBOOK_1}.py"
 sed -i "s/display/$OUTPUT_COMMAND/g;s/.\/Users\/<your_user_alias>\/featurestore_sample/.\//g;
     s/<FEATURE_STORAGE_ACCOUNT_NAME>/$FEATURE_STORAGE_ACCOUNT_NAME/g;
-    s/<USER_AAD_OBJECTID>/$USER_ID/g;" "${NOTEBOOK_2}.py"
-sed -i "s/display/$OUTPUT_COMMAND/g;s/.\/Users\/<your_user_alias>\/featurestore_sample/.\//g;" "${NOTEBOOK_3}.py"
-sed -i "s/display/$OUTPUT_COMMAND/g;s/.\/Users\/<your_user_alias>\/featurestore_sample/.\//g;" "${NOTEBOOK_4}.py"
+    s/<USER_AAD_OBJECTID>/$USER_ID/g
+    s/<VERSION>/$VERSION/g;;
+    s/<FEATURE_STORE_UAI_NAME>/$UAI_NAME/g;" "${NOTEBOOK_2}.py"
+sed -i "s/display/$OUTPUT_COMMAND/g;s/.\/Users\/<your_user_alias>\/featurestore_sample/.\//g;
+    s/<VERSION>/$VERSION/g;" "${NOTEBOOK_3}.py"
+sed -i "s/display/$OUTPUT_COMMAND/g;s/.\/Users\/<your_user_alias>\/featurestore_sample/.\//g;
+    s/<VERSION>/$VERSION/g;" "${NOTEBOOK_4}.py"
+sed -i "s/display/$OUTPUT_COMMAND/g;s/.\/Users\/<your_user_alias>\/featurestore_sample/.\//g;
+    s/<REDIS_NAME>/$REDIS_NAME/g;
+    s/<VERSION>/$VERSION/g;" "${NOTEBOOK_5}.py"
