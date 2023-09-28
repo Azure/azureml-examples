371d79fc6cb6f7e7d31e9a79ce81384111f0c012
risha <risha@microsoft.com>
2023-09-28 15:15:12 -0700

    add batch scoring for other od models

--- sdk/python/jobs/automl-standalone-jobs/automl-image-object-detection-task-fridge-items-batch-scoring/scripts/batch_scoring_yolo.py
+++ sdk/python/jobs/automl-standalone-jobs/automl-image-object-detection-task-fridge-items-batch-scoring/scripts/batch_scoring_yolo.py
@@ -13,7 +13,7 @@ from azureml.automl.dnn.vision.common.model_export_utils import (
     load_model,
     run_inference_batch,
 )
-from azureml.automl.dnn.vision.object_detection.writers.score import (
+from azureml.automl.dnn.vision.object_detection_yolo.writers.score import (
     _score_with_model,
 )
 from azureml.automl.dnn.vision.common.utils import _set_logging_parameters
