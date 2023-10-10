1539efb9a10e5a1dc4abbaed02b5925419f8dba6
Aswin Nagarajan <anagarajan@microsoft.com>
2023-10-09 22:27:28 -0700

    adding install reqs for sdk

--- sdk/python/setup_oai_v2.sh
+++ sdk/python/setup_oai_v2.sh
@@ -18,11 +18,6 @@
 #      unset wheel_url
 #  }
 
-
-# <az_ml_install>
-az extension add -n ml -y
-# </az_ml_install>
-
 ## For backward compatibility - running on old subscription
 # <set_variables>
 GROUP="azureml-examples"
@@ -45,3 +40,25 @@ then
 else
     echo "Workflows are using the new subscription."
 fi
+
+# <az_ml_sdk_install>
+# pip install --pre azure-ai-ml
+# </az_ml_sdk_install>
+
+# <mldesigner_install>
+pip install mldesigner
+# </mldesigner_install>
+
+# <mltable_install>
+pip install mltable
+pip install pandas
+# </mltable_install>
+
+
+# <az_ml_sdk_test_install>
+# pip install azure-ai-ml==0.1.0.b8
+pip install azure-ai-ml
+# https://docsupport.blob.core.windows.net/ml-sample-submissions/1905732/azure_ai_ml-1.0.0-py3-none-any.whl
+# </az_ml_sdk_test_install>
+
+pip list
