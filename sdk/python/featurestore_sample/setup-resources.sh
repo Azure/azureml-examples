dbd844c480606a4e5dc5571a0a83fcbee1e01f5b
Qianjun Xu <qiax@microsoft.com>
2023-10-10 15:16:05 -0700

    update resource setup

--- sdk/python/featurestore_sample/setup-resources.sh
+++ sdk/python/featurestore_sample/setup-resources.sh
@@ -11,20 +11,18 @@ USER_ID="36b5b70a-a2b2-45e6-a496-df3c2ffde085"
 RAND_NUM=$RANDOM
 UAI_NAME=fstoreuai${RAND_NUM}
 REDIS_NAME=${RESOURCE_GROUP}rds
-VERSION=$(((RANDOM%10)+1))
+VERSION=$(((RANDOM%100)+1))
 # </create_variables>
 
 # <convert_notebook_to_py>
 NOTEBOOK_1="notebooks/sdk_only/1. Develop a feature set and register with managed feature store"
-NOTEBOOK_2="notebooks/sdk_only/2. Enable materialization and backfill feature data"
-NOTEBOOK_3="notebooks/sdk_only/3. Experiment and train models using features"
-NOTEBOOK_4="notebooks/sdk_only/4. Enable recurrent materialization and run batch inference"
-NOTEBOOK_5="notebooks/sdk_only/5. Enable online store and run online inference"
+NOTEBOOK_2="notebooks/sdk_only/2. Experiment and train models using features"
+NOTEBOOK_3="notebooks/sdk_only/3. Enable recurrent materialization and run batch inference"
+NOTEBOOK_4="notebooks/sdk_only/4. Enable online store and run online inference"
 jupytext --to py "${NOTEBOOK_1}.ipynb"
 jupytext --to py "${NOTEBOOK_2}.ipynb"
 jupytext --to py "${NOTEBOOK_3}.ipynb"
 jupytext --to py "${NOTEBOOK_4}.ipynb"
-jupytext --to py "${NOTEBOOK_5}.ipynb"
 # <convert_notebook_to_py>
 
 #<replace_template_values>
@@ -36,14 +34,9 @@ sed -i "s/<SUBSCRIPTION_ID>/$SUBSCRIPTION_ID/g;
 sed -i "s/display/$OUTPUT_COMMAND/g;s/.\/Users\/<your_user_alias>\/featurestore_sample/.\//g;
     s/<VERSION>/$VERSION/g;" "${NOTEBOOK_1}.py"
 sed -i "s/display/$OUTPUT_COMMAND/g;s/.\/Users\/<your_user_alias>\/featurestore_sample/.\//g;
-    s/<FEATURE_STORAGE_ACCOUNT_NAME>/$FEATURE_STORAGE_ACCOUNT_NAME/g;
-    s/<USER_AAD_OBJECTID>/$USER_ID/g
-    s/<VERSION>/$VERSION/g;;
-    s/<FEATURE_STORE_UAI_NAME>/$UAI_NAME/g;" "${NOTEBOOK_2}.py"
+    s/<VERSION>/$VERSION/g;" "${NOTEBOOK_2}.py"
 sed -i "s/display/$OUTPUT_COMMAND/g;s/.\/Users\/<your_user_alias>\/featurestore_sample/.\//g;
     s/<VERSION>/$VERSION/g;" "${NOTEBOOK_3}.py"
 sed -i "s/display/$OUTPUT_COMMAND/g;s/.\/Users\/<your_user_alias>\/featurestore_sample/.\//g;
-    s/<VERSION>/$VERSION/g;" "${NOTEBOOK_4}.py"
-sed -i "s/display/$OUTPUT_COMMAND/g;s/.\/Users\/<your_user_alias>\/featurestore_sample/.\//g;
     s/<REDIS_NAME>/$REDIS_NAME/g;
-    s/<VERSION>/$VERSION/g;" "${NOTEBOOK_5}.py"
+    s/<VERSION>/$VERSION/g;" "${NOTEBOOK_4}.py"
