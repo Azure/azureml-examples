7d88ec809af0c5b291452a1e0c5ab2eff7d19bb6
Qianjun Xu <qiax@microsoft.com>
2023-10-10 10:45:11 -0700

    fix test failure

--- sdk/python/featurestore_sample/featurestore_sdk_job.py
+++ sdk/python/featurestore_sample/featurestore_sdk_job.py
@@ -10,21 +10,21 @@ for path, subdirs, files in os.walk("./"):
 
 print("=======Test Notebook 1============")
 with open(
-    "notebooks/sdk_only/Develop a feature set and register with managed feature store.py"
+    "notebooks/sdk_only/1. Develop a feature set and register with managed feature store.py"
 ) as f:
     exec(f.read())
 
 print("=======Test Notebook 2============")
-with open("notebooks/sdk_only/Experiment and train models using features.py") as f:
+with open("notebooks/sdk_only/2. Experiment and train models using features.py") as f:
     exec(f.read())
 
 print("=======Test Notebook 3============")
 with open(
-    "notebooks/sdk_only/Enable recurrent materialization and run batch inference.py"
+    "notebooks/sdk_only/3. Enable recurrent materialization and run batch inference.py"
 ) as f:
     exec(f.read())
 
 # # exclude 5th notebook for now
 # print("=======Test Notebook 4============")
-# with open("notebooks/sdk_only/Enable online store and run online inference.py") as f:
+# with open("notebooks/sdk_only/4. Enable online store and run online inference.py") as f:
 #     exec(f.read())
