e38df6882c1a470d1ba355147ba67c041b06afcc
Fred Li <feli1@microsoft.com>
2023-10-24 19:30:56 -0700

    Restructure automation test files

--- sdk/python/featurestore_sample/automation-test/featurestore_sdk_job.py
+++ sdk/python/featurestore_sample/automation-test/featurestore_sdk_job.py
@@ -10,17 +10,17 @@ for path, subdirs, files in os.walk("./"):
 
 print("=======Test Notebook 1============")
 with open(
-    "notebooks/sdk_only/1. Develop a feature set and register with managed feature store.py"
+    "../notebooks/sdk_only/1. Develop a feature set and register with managed feature store.py"
 ) as f:
     exec(f.read())
 
 print("=======Test Notebook 2============")
-with open("notebooks/sdk_only/2. Experiment and train models using features.py") as f:
+with open("../notebooks/sdk_only/2. Experiment and train models using features.py") as f:
     exec(f.read())
 
 print("=======Test Notebook 3============")
 with open(
-    "notebooks/sdk_only/3. Enable recurrent materialization and run batch inference.py"
+    "../notebooks/sdk_only/3. Enable recurrent materialization and run batch inference.py"
 ) as f:
     exec(f.read())
 
--- sdk/python/featurestore_sample/automation-test/setup-resources.sh
+++ sdk/python/featurestore_sample/automation-test/setup-resources.sh
@@ -15,10 +15,10 @@ VERSION=$(((RANDOM%1000)+1))
 # </create_variables>
 
 # <convert_notebook_to_py>
-NOTEBOOK_1="notebooks/sdk_only/1. Develop a feature set and register with managed feature store"
-NOTEBOOK_2="notebooks/sdk_only/2. Experiment and train models using features"
-NOTEBOOK_3="notebooks/sdk_only/3. Enable recurrent materialization and run batch inference"
-NOTEBOOK_4="notebooks/sdk_only/4. Enable online store and run online inference"
+NOTEBOOK_1="../notebooks/sdk_only/1. Develop a feature set and register with managed feature store"
+NOTEBOOK_2="../notebooks/sdk_only/2. Experiment and train models using features"
+NOTEBOOK_3="../notebooks/sdk_only/3. Enable recurrent materialization and run batch inference"
+NOTEBOOK_4="../notebooks/sdk_only/4. Enable online store and run online inference"
 jupytext --to py "${NOTEBOOK_1}.ipynb"
 jupytext --to py "${NOTEBOOK_2}.ipynb"
 jupytext --to py "${NOTEBOOK_3}.ipynb"
