422449137df5a0a78f8b2bdf8400e2549b847333
Diondra Peck <dipeck@microsoft.com>
2023-10-26 16:08:59 -0700

    Update check-readme.py with Kevin's suggestions

--- infra/bootstrapping/check-readme.py
+++ infra/bootstrapping/check-readme.py
@@ -1,19 +1,19 @@
 import argparse
-import os
+import sys
+
+from pathlib import Path
 
 
 argParser = argparse.ArgumentParser()
-argParser.add_argument("working_directory", help="The working directory for this script.")
-argParser.add_argument("sample_path", help="The absolute path to the sample directory.")
+argParser.add_argument("sample_path", help="The absolute path to the sample directory.", type=Path)
 args = argParser.parse_args()
-working_directory = args.working_directory.replace("\\", "/")
-sample_path = args.sample_path.replace("\\", "/")
+sample_path = args.sample_path
 
 
 def main():
     INVALID_README_MSG = f"{sample_path} does not contain a README.md file with all required words. See the Discoverability section of CONTRIBUTING.md."
-    EXCLUSIONS_FILE_PATH = f"{working_directory}/infra/bootstrapping/readme_validation_exclusions.txt"
-    required_sections = ["overview", "objective", "programming languages", "estimated runtime"]
+    EXCLUSIONS_FILE_PATH = "/home/runner/work/azureml-examples/azureml-examples/infra/bootstrapping/readme_validation_exclusions.txt"
+    required_sections = ["overview", "objective", "programming languages", "estimated runtime", "page_type: sample", "languages:", "products:", "description:"]
 
     print(f"Checking if {sample_path} contains a README.md file with all required words...")
 
@@ -22,21 +22,21 @@ def main():
         exclusions = exclusions_file.read().splitlines()
         if sample_path in exclusions:
             print(f"Skipping {sample_path} since it is excluded from README validation.")
-            return 0
+            sys.exit(0)
 
     # Check if sample contains a valid README.md file
     try:
         with open(f"{sample_path}/README.md", encoding="utf-8") as readme_file:
             readme_content = readme_file.read()
 
-            if all([section in readme_content for section in required_sections]):
+            if all(section in readme_content for section in required_sections):
                 print(f"{sample_path} contains a README.md file with all required sections.")
-                return 0
+                sys.exit(0)
     except FileNotFoundError:
         pass
 
     print(INVALID_README_MSG)
-    return 1
+    sys.exit(1)
 
 
 if __name__ == "__main__":
