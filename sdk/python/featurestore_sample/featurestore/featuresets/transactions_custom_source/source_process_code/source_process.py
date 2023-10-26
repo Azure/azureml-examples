68feb348519c5660cef1ab3152ef93afea9db35d
Mickey Zhang <ruizh@microsoft.com>
2023-10-26 16:14:52 -0700

    Update sample

--- sdk/python/featurestore_sample/featurestore/featuresets/transactions_custom_source/source_process_code/source_process.py
+++ sdk/python/featurestore_sample/featurestore/featuresets/transactions_custom_source/source_process_code/source_process.py
@@ -6,16 +6,26 @@ from datetime import datetime
 
 class CustomSourceTransformer:
     def __init__(self, **kwargs):
-        self.path = kwargs.get(
-            "path",
-            "wasbs://data@azuremlexampledata.blob.core.windows.net/feature-store-prp/datasources/transactions-source/*.parquet",
-        )
+        self.path = kwargs.get("source_path")
+        self.timestamp_column_name = kwargs.get("timestamp_column_name")
+        if not self.path:
+            raise Exception("`source_path` is not provided")
+        if not self.timestamp_column_name:
+            raise Exception("`timestamp_column_name` is not provided")
 
     def process(
         self, start_time: datetime, end_time: datetime, **kwargs
     ) -> "pyspark.sql.DataFrame":
         from pyspark.sql import SparkSession
+        from pyspark.sql.functions import col, lit, to_timestamp
 
         spark = SparkSession.builder.getOrCreate()
-        df = spark.read.parquet(self.path)
+        df = spark.read.json(self.path)
+
+        if start_time:
+            df = df.filter(col(self.timestamp_column_name) >= to_timestamp(lit(start_time)))
+
+        if end_time:
+            df = df.filter(col(self.timestamp_column_name) < to_timestamp(lit(end_time)))
+
         return df
