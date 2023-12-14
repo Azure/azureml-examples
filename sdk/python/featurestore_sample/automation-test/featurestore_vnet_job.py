f09d62fbf1f9a95b4f8896fa81ea665d0546ea95
Fred Li <feli1@microsoft.com>
2023-12-13 20:51:56 -0800

    Add comment

--- sdk/python/featurestore_sample/automation-test/featurestore_vnet_job.py
+++ sdk/python/featurestore_sample/automation-test/featurestore_vnet_job.py
@@ -8,6 +8,7 @@ for path, subdirs, files in os.walk("./"):
     for name in files:
         print(os.path.join(path, name))
 
+print("======Test Vnet scenario======")
 from azure.ai.ml import MLClient
 from azure.ai.ml.identity import AzureMLOnBehalfOfCredential
 
