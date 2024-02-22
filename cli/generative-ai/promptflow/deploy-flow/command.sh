f0fffad2c860080479aeec8b46644bccb7be4064
Blanca Li <39058063+likebupt@users.noreply.github.com>
2024-02-22 18:47:15 +0800

    add promptflow code deploy samples (#2619)
    * add promptflow code deploy samples
    
    * add streaming sample
    
    * format ipynb
    
    * black
    
    * update pf deployment sample
    
    * resolve comments

---
+++ cli/generative-ai/promptflow/deploy-flow/command.sh
@@ -0,0 +1,20 @@
+# Prerequiste
+# make sure you have created the connection used in the flow in your AzureML workspace
+
+# register flow as a model
+az ml model create --file model.yaml
+
+# create online endpoint
+az ml online-endpoint create --file endpoint.yaml
+
+# create online deployment with 0 traffic
+az ml online-deployment create --file deployment.yaml
+
+# create online deployment with 100% traffic
+az ml online-deployment create --file deployment.yaml --all-traffic
+
+# set traffic of the deployment
+az ml online-endpoint update --name basic-chat-endpoint --traffic "blue=100"
+
+# invoke the endpoint
+az ml online-endpoint invoke --name basic-chat-endpoint --request-file sample-request.json
