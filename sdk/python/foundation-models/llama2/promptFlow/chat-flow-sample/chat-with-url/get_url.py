f9002b84e69463f20077a3d3e7536fe985042f84
p9523 <141737115+p9523@users.noreply.github.com>
2024-04-17 10:50:56 +0530

    Delete sdk/python/foundation-models/llama2/promptFlow/chat-flow-sample/chat-with-url directory

--- sdk/python/foundation-models/llama2/promptFlow/chat-flow-sample/chat-with-url/get_url.py
+++
@@ -1,9 +0,0 @@
-from promptflow import tool
-
-# The inputs section will change based on the arguments of the tool function, after you save the code
-# Adding type hints to arguments and return value will help the system show the types properly
-# Please update the function name/signature per need
-@tool
-def get_url(input: str) -> str:
-  """Returns the input URL."""
-  return input
