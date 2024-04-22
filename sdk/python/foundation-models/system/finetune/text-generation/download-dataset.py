1c3a1321bcb3dfbe8f62ad25267a81982c087212
Anubha98 <anubhajain@microsoft.com>
2024-04-22 15:17:57 +0530

    Chat completion notebook with ultrachat_200k dataset

--- sdk/python/foundation-models/system/finetune/text-generation/download-dataset.py
+++ sdk/python/foundation-models/system/finetune/text-generation/download-dataset.py
@@ -8,6 +8,9 @@ parser.add_argument("--dataset", type=str, default="samsum", help="dataset name"
 parser.add_argument(
     "--dataset_subset", type=str, default=None, help="dataset subset name"
 )
+parser.add_argument(
+    "--dataset_split_pc", type=str, default=None, help="percentage of each split to be downloaded"
+)
 # add an argument to specify the directory to download the dataset to
 parser.add_argument(
     "--download_dir",
@@ -27,7 +30,8 @@ from datasets import load_dataset, get_dataset_split_names
 
 for split in get_dataset_split_names(args.dataset, args.dataset_subset):
     # load the split of the dataset
-    dataset = load_dataset(args.dataset, args.dataset_subset, split=split)
+    split_pc = split + f"[0%:{args.dataset_split_pc}%]"
+    dataset = load_dataset(args.dataset, args.dataset_subset, split=split_pc)
     # save the split of the dataset to the download directory as json lines file
     dataset.to_json(os.path.join(args.download_dir, f"{split}.jsonl"))
     # print dataset features
