4f297c52ae3995184d353c05e7248e4027ac4cd2
AshishC17 <ashishchoudhary1717@gmail.com>
2024-05-14 20:17:59 +0530

    modified changes

--- cli/setup.sh
+++ cli/setup.sh
@@ -25,9 +25,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="azureml-examples"
-LOCATION="eastus"
-WORKSPACE="main"
+GROUP="ashishchoudhary_17-rg"
+LOCATION="Central India"
+WORKSPACE="MLOps"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
