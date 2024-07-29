ad56282684ddae1f00af5c4690d9b82cf54e3134
lucasfijen <lucasfijen@users.noreply.github.com>
2024-07-29 17:21:33 +0200

    Update setup-custom-conda-env.sh
    The creation of the conda environment needs to be ran outside of the azureuser block

--- setup/setup-ci/setup-custom-conda-env.sh
+++ setup/setup-ci/setup-custom-conda-env.sh
@@ -4,13 +4,14 @@ set -e
 
 # This script creates a custom conda environment and kernel based on a sample yml file.
 
-sudo -u azureuser -i <<'EOF'
 source /anaconda/etc/profile.d/conda.sh
 conda env create -f env.yml
 echo "Activating new conda environment"
 conda activate envname
 conda install -y ipykernel
+sudo -u azureuser -i <<'EOF'
 echo "Installing kernel"
+source /anaconda/etc/profile.d/conda.sh
 conda activate envname
 python -m ipykernel install --user --name envname --display-name "mykernel"
 echo "Conda environment setup successfully."
