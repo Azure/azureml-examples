ab2093797b3a61fa1dacf961fc52b2bc8bce5cd6
Prashant Dhote <pdhote@microsoft.com>
2024-09-08 16:02:00 +0530

    add file

--- cli/deploy-managed-online-endpoint.sh
+++ cli/deploy-managed-online-endpoint.sh
@@ -28,7 +28,7 @@ else
   echo "Endpoint creation failed"
   exit 1
 fi
-
+echo "####Hi"
 deploy_status=`az ml online-deployment show --name blue --endpoint $ENDPOINT_NAME --query "provisioning_state" -o tsv`
 echo $deploy_status
 if [[ $deploy_status == "Succeeded" ]]
