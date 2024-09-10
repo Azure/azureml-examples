52eda6e1d5ed00c860ae92c064e7fe216b422c27
Amit Chauhan <70937115+achauhan-scc@users.noreply.github.com>
2024-09-10 13:58:41 +0530

    black reformatting

--- infra/validation/schema_file_validation.py
+++ infra/validation/schema_file_validation.py
@@ -1,31 +1,28 @@
 from urllib.request import urlopen
 import json
 
-#url = 'pipelineComponent.schema.json'
+# url = 'pipelineComponent.schema.json'
 #'#/definitions/PipelineComponentSchema'
-validation_required ={
-    'pipelineComponent':'PipelineComponentSchema',
-    'workspace':'WorkspaceSchema',
-    'environment':'EnvironmentSchema',
-    'dataset':'DatasetSchema',
-    'model':'ModelSchema'
+validation_required = {
+    "pipelineComponent": "PipelineComponentSchema",
+    "workspace": "WorkspaceSchema",
+    "environment": "EnvironmentSchema",
+    "dataset": "DatasetSchema",
+    "model": "ModelSchema",
 }
 
-def validation(entity_name: str, validation_value : str ) -> None:
-    response = urlopen(f'https://azuremlschemas.azureedge.net/latest/{entity_name}.schema.json')
-    string = response.read().decode('utf-8')
+
+def validation(entity_name: str, validation_value: str) -> None:
+    response = urlopen(f"https://azuremlschemas.azureedge.net/latest/{entity_name}.schema.json")
+    string = response.read().decode("utf-8")
     json_obj = json.loads(string)
-    actual_val = json_obj['$ref']
-    if (validation_value in actual_val) :
+    actual_val = json_obj["$ref"]
+    if validation_value in actual_val:
         print(f"successfully validated schema : {entity_name}")
-    else :
+    else:
         print(f"unable to validate schema for : {entity_name}")
-        raise Exception (f"unable to validate schema for : {entity_name}")
+        raise Exception(f"unable to validate schema for : {entity_name}")
 
 
 for validate in validation_required:
-    validation(validate,validation_required[validate])
-
-
-    
-    
+    validation(validate, validation_required[validate])
