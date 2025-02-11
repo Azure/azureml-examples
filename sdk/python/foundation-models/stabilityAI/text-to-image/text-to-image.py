74b907c28ffa3f666b6675a9aa7890a3cbe6a582
deepanshMS <>
2025-02-11 23:19:07 +0530

    sd3 text to image examples

--- sdk/python/foundation-models/stabilityAI/text-to-image/text-to-image.py
+++ sdk/python/foundation-models/stabilityAI/text-to-image/text-to-image.py
@@ -8,7 +8,7 @@ import base64
 AZURE_HOST = ""
 STABILITY_KEY = ""
 
-def save_key_and_generate_image(prompt, output_format, negative_prompt, seed, size, progress=gr.Progress(track_tqdm=True)):
+def save_and_generate_image(prompt, output_format, negative_prompt, seed, size, progress=gr.Progress(track_tqdm=True)):
     print(f"Image Prompt is : {prompt}")
 
     image = generate_image(prompt, output_format, negative_prompt, seed, size)
@@ -24,9 +24,11 @@ def generate_image(prompt, output_format, negative_prompt, seed, size):
         "prompt": prompt, 
         "output_format": output_format,
         "size": size,
-        "negative_prompt": negative_prompt or None,
         "seed": seed
     }
+
+    if negative_prompt:
+        params["negative_prompt"] = negative_prompt
 
     print(f"Sending request with params: {json.dumps(params, indent=2)}")
 
@@ -51,19 +53,17 @@ def generate_image(prompt, output_format, negative_prompt, seed, size):
     return output_image
 
 demo = gr.Interface(
-    fn=save_key_and_generate_image,
+    fn=save_and_generate_image,
     inputs=[
-        gr.Image(type='pil', label='Initial Image'),
         gr.Textbox(label="Enter your Image Prompt", placeholder="Describe your image..."),
         gr.Radio(choices=["jpeg", "png"], label="Output Format", value="jpeg"),
         gr.Textbox(label="Negative Prompt (optional)", placeholder="What to avoid in the image"),
-        gr.Slider(minimum=0, maximum=10000, step=1, label="Seed (optional)", value=0),
-        gr.Radio(choices=["672x1566", "832x1216", "768x1366", "836x1254", "916x1145", "1024x1024", "1145x916", "1254x836", "1366x768", "1566x672"], label="Image Size", value="1024x1024")
+        gr.Slider(minimum=0, maximum=1000, step=1, label="Seed (optional)", value=0),
+        gr.Radio(choices=["672x1566", "768x1366", "836x1254", "916x1145", "1024x1024", "1145x916", "1254x836", "1366x768", "1566x672"], label="Image Size", value="1024x1024")
     ],
     outputs=[gr.Image(label="Generated Image")],
-    title="Stability AI | Text to Image"
+    title="Stability AI on Azure AI | Text to Image"
 )
-
 
 if __name__ == "__main__":
     demo.launch()
