2c07d21d0b97e34fadcefcd26a4113330511aa7f
Sharvin Jondhale <shjondhale@microsoft.com>
2025-05-21 16:57:40 +0000

    Add missing copyright

--- sdk/python/jobs/grpo/aml_setup.py
+++ sdk/python/jobs/grpo/aml_setup.py
@@ -1,3 +1,6 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
 """
 Install necessary libraries and packages required for running AML training jobs.
 """
