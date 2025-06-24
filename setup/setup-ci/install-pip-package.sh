9905b8c352730924aca644de3fcd0bdd2b061928
Sheri Gilley <sgilley@microsoft.com>
2025-06-24 11:28:03 -0700

    Update install-pip-package.sh
    DO NOT MERGE.  DO NOT APPROVE.  
    PR is being created to demo our process.

--- setup/setup-ci/install-pip-package.sh
+++ setup/setup-ci/install-pip-package.sh
@@ -3,6 +3,7 @@
 set -e
 
 # This script installs a pip package in compute instance azureml_py38 environment.
+# adding a comment DO NOT APPROVE.  Using it for a demo.
 
 sudo -u azureuser -i <<'EOF'
 
