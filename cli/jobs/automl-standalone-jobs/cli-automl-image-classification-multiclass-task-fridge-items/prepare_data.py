b172f948adbcfbe198d1e4a2aec9fb56a9a8710e
visahan-24 <visahanmohan@microsoft.com>
2025-09-13 12:22:42 +0530

    Removing storage download for image-classification

--- cli/jobs/automl-standalone-jobs/cli-automl-image-classification-multiclass-task-fridge-items/prepare_data.py
+++ cli/jobs/automl-standalone-jobs/cli-automl-image-classification-multiclass-task-fridge-items/prepare_data.py
@@ -1,7 +1,6 @@
 import argparse
 import json
 import os
-import urllib
 from zipfile import ZipFile
 
 from azure.identity import InteractiveBrowserCredential
@@ -103,28 +102,17 @@ def upload_data_and_create_jsonl_mltable_files(ml_client, dataset_parent_dir):
     # Create directory, if it does not exist
     os.makedirs(dataset_parent_dir, exist_ok=True)
 
-    # download data
-    print("Downloading data.")
-    download_url = "https://automlsamplenotebookdata-adcuc7f7bqhhh8a4.b02.azurefd.net/image-classification/fridgeObjects.zip"
-
-    # Extract current dataset name from dataset url
-    dataset_name = os.path.basename(download_url).split(".")[0]
-    # Get dataset path for later use
+    # Use local fridgeObjects.zip inside the data folder
+    data_file = os.path.join(dataset_parent_dir, "fridgeObjects.zip")
+    dataset_name = "fridgeObjects"
     dataset_dir = os.path.join(dataset_parent_dir, dataset_name)
 
-    # Get the name of zip file
-    data_file = os.path.join(dataset_parent_dir, f"{dataset_name}.zip")
-
-    # Download data from public url
-    urllib.request.urlretrieve(download_url, filename=data_file)
 
     # extract files
     with ZipFile(data_file, "r") as zip:
         print("extracting files...")
         zip.extractall(path=dataset_parent_dir)
         print("done")
-    # delete zip file
-    os.remove(data_file)
 
     # Upload data and create a data asset URI folder
     print("Uploading data to blob storage")
