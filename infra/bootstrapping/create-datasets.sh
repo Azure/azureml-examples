f50e4b983fdc629f6804743814ec16baa0c5dff2
Rahul Kumar <iamrk04@gmail.com>
2025-12-16 23:11:37 +0530

    Revert "attempt to download and then register data"
    This reverts commit ed8e3b958b83a9557386489890ff2e9a31560277.

--- infra/bootstrapping/create-datasets.sh
+++ infra/bootstrapping/create-datasets.sh
@@ -11,12 +11,12 @@ pushd "$ROOT_DIR" > /dev/null
 az ml data create -f "./cli/jobs/pipelines-with-components/basics/4d_data_input/data.yml" > /dev/null 2>&1
 
 # This step times out. May need to use az copy to upload the dataset - TODO
-mkdir data
-wget "https://azuremlexamples.blob.core.windows.net/datasets/cifar-10-python.tar.gz"
-tar -xvzf cifar-10-python.tar.gz -C data
-az ml data create --name cifar-10-example --version 1 --set path=data
+# mkdir data
+# wget "https://azuremlexamples.blob.core.windows.net/datasets/cifar-10-python.tar.gz"
+# tar -xvzf cifar-10-python.tar.gz -C data
+# az ml data create --name cifar-10-example --version 1 --set path=data
 
-# az ml data create --name cifar-10-example --version 1 --path "wasbs://cifar-10-batches@azuremlexampledata.blob.core.windows.net/" > /dev/null 2>&1
+az ml data create --name cifar-10-example --version 1 --path "wasbs://cifar-10-batches@azuremlexampledata.blob.core.windows.net/" > /dev/null 2>&1
 
 
 
