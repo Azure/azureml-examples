711aa4aec10a927cb220da166507635e78e76754
Chakradhar886 <v-cchakradha@microsoft.com>
2026-03-10 16:41:38 +0530

    Update predict.py

--- cli/jobs/pipelines-with-components/pipeline_with_hyperparameter_sweep/predict-src/predict.py
+++ cli/jobs/pipelines-with-components/pipeline_with_hyperparameter_sweep/predict-src/predict.py
@@ -5,21 +5,25 @@ import argparse
 from pathlib import Path
 import pandas as pd
 
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
-
 parser = argparse.ArgumentParser()
-
 parser.add_argument("--model", type=str)
 parser.add_argument("--test_data", type=str)
 parser.add_argument("--predict_result", type=str)
 
 args = parser.parse_args()
 
+# ✅ REQUIRED FOR HYPERPARAMETER (SWEEP) JOBS
+os.makedirs(args.predict_result, exist_ok=True)
+
+# load data
 X_test = pd.read_csv(Path(args.test_data) / "X_test.csv")
-# model = mlflow.sklearn.load_model(Path(args.model)/ 'model')
-model = mlflow.sklearn.load_model(args.model)
 y_test = pd.read_csv(Path(args.test_data) / "y_test.csv")
+
+# load model
+model = mlflow.sklearn.load_model(args.model)
+
+# predict
 y_test["predict"] = model.predict(X_test)
 
-y_test.to_csv(Path(args.predict_result) / "predict_result.csv")
+# save results
+y_test.to_csv(Path(args.predict_result) / "predict_result.csv", index=False)
