1860fcc88645988583f24ba4ac329cf1fa801096
lavakumarrepala <v-rlava@microsoft.com>
2026-07-07 00:04:29 -0700

    Update Azure AI ML SDK installation command

--- sdk/python/setup.sh
+++ sdk/python/setup.sh
@@ -13,7 +13,7 @@ pip install pandas
 # </mltable_install>
 
 
-# https://docsupport.blob.core.windows.net/ml-sample-submissions/1905732/azure_ai_ml-1.0.0-py3-none-any.whl
+# <az_ml_sdk_test_install>
 pip install https://azuresdkartifacts.z5.web.core.windows.net/python/distributions/ml-sample/6521821/azure_ai_ml-1.35.0-py3-none-any.whl || pip install azure-ai-ml
 # </az_ml_sdk_test_install>
 
